a7e185cce1dad23935792d58a0417cebcbcd603a
Mathieu Delestre <ldbglobe@gmail.com>
2017-06-21 19:03:24 +0200

    Implémentation d'une base de travail pour le reste du dev. Le lanceur lance les services Admin / PlaylistControler / Player (et dbInterface bien que non utilisé) Il gère les échange entre les services via un système de capture d'évènement
    Afin de pouvoir travailler sans dépendre d'une véritable base de kara, j'ai ajouté un dossier Sample contenant 2 kara et les vidéos et ass associé
    
    Dans son état actuel, le script ouvre un panneau d'admin qui permet de changer l'état privé/public du système et de lancer ou couper le gestionnaire de playlist
    Il charge 4 fois le même morceau en playlist au démarrage ce qui permet de vérifier que la communication avec le player fonctionne.

---
+++ src/_player/index.js
@@ -0,0 +1,96 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+	playing:false,
+	_playing:false, // internal delay flag
+	_player:null,
+	_ref:null,
+	BINPATH:null,
+	init:function(){
+		if(!fs.existsSync(module.exports.BINPATH)){
+			console.log('Unable to found mpv.exe');
+			console.log('Received path was : '+module.exports.BINPATH);
+			process.exit();
+		}
+
+		var mpvAPI = require('node-mpv');
+		module.exports._player = new mpvAPI({
+			audio_only: false,
+			binary: path.join(module.exports.BINPATH,'mpv.exe'),
+			socket: '\\\\.\\pipe\\mpvsocket',
+			time_update: 1,
+			verbose: false,
+			debug: false,
+		},
+		[
+			//"--fullscreen",
+			"--no-border",
+			"--keep-open=yes",
+			"--idle=yes",
+			"--fps=60",
+			"--screen=1",
+		]);
+
+		module.exports._player.on('statuschange',function(status){
+			if(module.exports._playing && status && status.filename && status.filename.match(/__blank__/))
+			{
+				// immediate switch to Playing = False to avoid multiple trigger
+				module.exports.playing = false;
+				module.exports._playing = false;
+				module.exports._player.pause();
+				module.exports.onEnd(module.exports._ref);
+				module.exports._ref = null;
+			}
+		});
+	},
+	play: function(video,subtitle,reference){
+		module.exports.playing = true;
+		if(fs.existsSync(video)){
+			console.log('playing : '+video);
+			module.exports._ref = reference;
+			module.exports._player.loadFile(video);
+			module.exports._player.volume(70);
+			module.exports._player.play();
+			// video may need some delay to play
+			setTimeout(function(){
+				module.exports._playing = true;
+				if(subtitle)
+				{
+					if(fs.existsSync(subtitle)){
+						console.log('subtitle : '+subtitle);
+						module.exports._player.addSubtitles(subtitle);//, flag, title, lang)
+					}
+					else
+					{
+						console.log('Can not found subtitle '+subtitle)
+					}
+				}
+				else
+				{
+					console.log('No subtitle');
+				}
+				module.exports._player.loadFile(path.join(__dirname,'assets/__blank__.png'),'append');
+			},500);
+		}
+		else {
+			module.exports.playing = false;
+			console.log('Can not found video '+video)
+		}
+	},
+	stop:function()
+	{
+		module.exports._player.loadFile(path.join(__dirname,'assets/__blank__.png'));
+	},
+	pause: function(){
+		console.log(module.exports._player);
+		module.exports._player.pause();
+	},
+	resume: function(){
+		module.exports._player.play();
+	},
+	onEnd:function(){
+		// événement émis pour quitter l'application
+		console.log('_player/index.js :: onEnd not set')
+	},
+};
--- src/_player/mpv.js
+++
@@ -1,5 +0,0 @@
-// Fonctions relatives à l'utilisation de mpv
-// - Démarrer MPV
-// - Stopper MPV
-// - Pause/unpause
-// - Ajouter un fichier kara à la suite du kara en cours
