6b9e220a2b115e42632f3e2b6f3326937bdd5419
Axel Terizaki <axel@teri-chan.net>
2017-06-23 19:04:47 +0200

    Ajout de champs normalisés pour les champs susceptibles de servir à la recherche.

--- src/_common/db/karas.sqlite3.sql
+++ src/_common/db/karas.sqlite3.sql
@@ -1,40 +1,42 @@
-BEGIN TRANSACTION;
 CREATE TABLE `tag` (
-	`PK_id_tag`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`tagtype`	INTEGER,
-	`name`	TEXT COLLATE NOCASE
+    `PK_id_tag` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `tagtype`   INTEGER,
+    `name`  TEXT,
+    `NORM_name` TEXT COLLATE NOCASE
 );
 CREATE TABLE `series` (
-	`PK_id_series`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`name`	TEXT COLLATE NOCASE,
-	`altname`	TEXT COLLATE NOCASE
+    `PK_id_series`  INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `name`  TEXT,
+    `NORM_name` TEXT COLLATE NOCASE,
+    `altname`   TEXT,
+    `NORM_altname`  TEXT COLLATE NOCASE
 );
 CREATE TABLE `kara_tag` (
-	`PK_id_kara_tag`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`FK_id_kara`	INTEGER,
-	`FK_id_tag`	INTEGER
+    `PK_id_kara_tag`    INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `FK_id_kara`    INTEGER,
+    `FK_id_tag` INTEGER
 );
 CREATE TABLE `kara_series` (
-	`PK_id_kara_series`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`FK_id_kara`	INTEGER,
-	`FK_id_series`	INTEGER
+    `PK_id_kara_series` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `FK_id_kara`    INTEGER,
+    `FK_id_series`  INTEGER
 );
 CREATE TABLE `kara` (
-	`PK_id_kara` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`kid`	TEXT UNIQUE,
-	`title`	TEXT COLLATE NOCASE,
-	`year`	TEXT,
-	`songorder`	INTEGER,
-	`videofile`	TEXT,
-	`subfile`	TEXT,
-	`videolength`	INTEGER,
-	`date_added`	INTEGER,
-	`date_last_modified`	INTEGER,
-	`rating`	REAL,
-	`viewcount`	INTEGER
+    `PK_id_kara` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `kid`   TEXT UNIQUE,
+    `title` TEXT,
+    `NORM_title`    TEXT COLLATE NOCASE,
+    `year`  TEXT,
+    `songorder` INTEGER,
+    `videofile` TEXT,
+    `subfile`   TEXT,
+    `videolength`   INTEGER,
+    `date_added`    INTEGER,
+    `date_last_modified`    INTEGER,
+    `rating`    REAL,
+    `viewcount` INTEGER
 );
 CREATE INDEX `index_kara_tag_id_tag` ON `kara_tag` (`FK_id_tag` );
 CREATE INDEX `index_kara_tag_id_kara` ON `kara_tag` (`FK_id_kara` );
 CREATE INDEX `index_kara_series_id_series` ON `kara_series` (`FK_id_series` );
 CREATE INDEX `index_kara_series_id_kara` ON `kara_series` (`FK_id_kara` );
-COMMIT;
