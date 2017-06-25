15d156875a84877e51d603aec281864175af51ba
Axel Terizaki <axel@teri-chan.net>
2017-06-25 10:28:28 +0200

    Playlist.js devenu inutile
    Y'a playlistcontroler pour ça

--- src/_common/playlist.js
+++
@@ -1,7 +0,0 @@
-// Code de gestion des playlist :
-// - Lecture de la playlist demandée
-// - Interrogation de la playlist pour connaître la prochaine chanson à jouer
-// - Interrogation de la playlist pour connaître la chanson en cours ainsi que ses infos
-// - Création d'une nouvelle playlist
-// - Export de playlist
-// - Import de playlist
