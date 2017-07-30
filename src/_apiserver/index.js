a10b07589daba530c5d91ca698d51d922fdeb7a6
Ziassan <coc5@free.fr>
2017-07-30 23:21:48 +0200

    #39 gestion des requetes preflight OPTIONS ne pouvant pas avoir d'identifiant

--- src/_apiserver/index.js
+++ src/_apiserver/index.js
@@ -314,11 +314,9 @@ module.exports = {
                 res.json(err);
             })
         })
-        
-				
-		// Add headers
+        // Add headers
 		app.use(function (req, res, next) {
-
+		
 			// Website you wish to allow to connect
 			res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1337');
 
@@ -326,15 +324,20 @@ module.exports = {
 			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
 			// Request headers you wish to allow
-			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+			res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept, Key');
 
 			// Set to true if you need the website to include cookies in the requests sent
 			// to the API (e.g. in case you use sessions)
 			// res.setHeader('Access-Control-Allow-Credentials', true);
-
-			// Pass to next layer of middleware
-			next();
+			
+			if (req.method === 'OPTIONS') {
+				res.send(200);
+			} else { 
+				// Pass to next layer of middleware
+				next();
+			}
 		});
+		
         app.use('/api/v1/public', routerPublic);
         app.use('/api/v1/admin', routerAdmin);
         logger.info(__('API_SERVER_READY'));
