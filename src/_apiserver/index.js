c888d74869faad657e07127e957634ae9f7de9b9
Ziassan <coc5@free.fr>
2017-07-30 02:24:45 +0200

    modif de l'api pour ajout de headers notamment cross domaine

--- src/_apiserver/index.js
+++ src/_apiserver/index.js
@@ -315,8 +315,26 @@ module.exports = {
             })
         })
         
-        
+				
+		// Add headers
+		app.use(function (req, res, next) {
 
+			// Website you wish to allow to connect
+			res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1337');
+
+			// Request methods you wish to allow
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+			// Request headers you wish to allow
+			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+			// Set to true if you need the website to include cookies in the requests sent
+			// to the API (e.g. in case you use sessions)
+			// res.setHeader('Access-Control-Allow-Credentials', true);
+
+			// Pass to next layer of middleware
+			next();
+		});
         app.use('/api/v1/public', routerPublic);
         app.use('/api/v1/admin', routerAdmin);
         logger.info(__('API_SERVER_READY'));
