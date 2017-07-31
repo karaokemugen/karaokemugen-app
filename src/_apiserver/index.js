9729880d1614f87974692a2fefa79a7403da9731
Ziassan <coc5@free.fr>
2017-07-31 20:10:30 +0200

    #39 ouverture de l'API à toute origine

--- src/_apiserver/index.js
+++ src/_apiserver/index.js
@@ -606,7 +606,7 @@ module.exports = {
 		app.use(function (req, res, next) {
 		
 			// Website you wish to allow to connect
-			res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1337');
+			res.setHeader('Access-Control-Allow-Origin', '*');
 
 			// Request methods you wish to allow
 			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
