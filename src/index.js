edfe2ef757cc9ab98ca8910fde5bc2726e123340
Florent Berthelot <florentius.b@gmail.com>
2017-08-04 23:31:43 +0200

    Correction eslint src//index.js

--- src/index.js
+++ src/index.js
@@ -18,18 +18,18 @@ const osLocale = require('os-locale');
  * Clear console - and welcome message
  * Node does not like the octal clear screen sequence.
  * So we wrote it in hexa (1B)
- */ 
+ */
 process.stdout.write('\x1Bc');
 console.log(clc.greenBright('+------------------------------------------------------------------+'));
 console.log(clc.greenBright('| Project Toyunda Mugen                                            |'));
 console.log(clc.greenBright('+------------------------------------------------------------------+'));
-console.log("\n");
+console.log('\n');
 
-var detectedLocale = osLocale.sync().substring(0,2);	
+var detectedLocale = osLocale.sync().substring(0,2);
 
 
-i18n.configure({    
-    directory: path.resolve(__dirname,'_common/locales'),
+i18n.configure({
+	directory: path.resolve(__dirname,'_common/locales'),
 	defaultLocale: 'en',
 	register: global
 });
@@ -38,32 +38,30 @@ i18n.setLocale(detectedLocale);
 logger.info(__('LOCALE_DETECTED')+detectedLocale);
 
 
-if (argv.help) { 
-	
+if (argv.help) {
+
 	console.log(__('HELP_MSG'));
 	process.exit(0);
 }
 
 if (argv.version) {
-	console.log("Toyunda Mugen v2.0 - Finé Fantastique");
-	console.log("Database version : xxx")
+	console.log('Toyunda Mugen v2.0 - Finé Fantastique');
+	console.log('Database version : xxx');
 	process.exit(0);
 }
 
 /** Call to resolveSyspath to get the app's path in all OS configurations */
 const SYSPATH = require('./_common/utils/resolveSyspath.js')('config.ini.default',__dirname,['./','../']);
-if(SYSPATH)
-{
+if(SYSPATH) {
 	logger.debug(__('SYSPATH_DETECTED')+SYSPATH);
 	// Lecture de la configuration par défault
 	/**
 	 * Reading config.ini.default, then override it with config.ini if it exists.
 	 */
 	var SETTINGS = ini.parse(fs.readFileSync(path.join(SYSPATH,'config.ini.default'), 'utf-8'));
-	if(fs.existsSync(path.join(SYSPATH,'config.ini')))
-	{
+	if(fs.existsSync(path.join(SYSPATH,'config.ini'))) {
 		// et surcharge via le contenu du fichier personnalisé si présent
-		var configCustom = ini.parse(fs.readFileSync(path.join(SYSPATH,'config.ini'), 'utf-8'))
+		var configCustom = ini.parse(fs.readFileSync(path.join(SYSPATH,'config.ini'), 'utf-8'));
 		extend(true,SETTINGS,configCustom);
 	}
 	SETTINGS.os = process.platform;
@@ -90,7 +88,7 @@ if(SYSPATH)
 			logger.error(__('CREATING_FOLDER_FAILED'));
 			process.exit();
 		}
-	}	
+	}
 	if(!fs.existsSync(path.join(SYSPATH,SETTINGS.PathSubs))) {
 		logger.warn(__('CREATING_FOLDER',path.join(SYSPATH,SETTINGS.PathSubs)));
 		ret = mkdirp.sync(path.join(SYSPATH,SETTINGS.PathSubs));
@@ -103,7 +101,7 @@ if(SYSPATH)
 		logger.warn(__('CREATING_FOLDER',path.join(SYSPATH,SETTINGS.PathVideos)));
 		ret = mkdirp.sync(path.join(SYSPATH,SETTINGS.PathVideos));
 		if (!ret) {
-			logger.error(__('CREATING_FOLDER_FAILED'))
+			logger.error(__('CREATING_FOLDER_FAILED'));
 			process.exit();
 		}
 	}
@@ -143,8 +141,6 @@ if(SYSPATH)
 		engine.test_playlist_controller();
 	else
 		engine.run();
-}
-else
-{
+} else {
 	logger.error('DETECTED_SYSPATH_FAILED');
 }
