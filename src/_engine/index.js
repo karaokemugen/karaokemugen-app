7facde720638d142ebfa7c5c5bb4a52efc57e6b6
Guillaume Lebigot <axel@teri-chan.net>
2017-08-05 18:14:24 +0200

    Oubli de retirer le test de l'avant dernier commit

--- src/_engine/index.js
+++ src/_engine/index.js
@@ -831,7 +831,7 @@ module.exports = {
 				console.log(err);
 			});
 		*/
-		
+		/*
 		module.exports._services.playlist_controller.toggleDisplayNickname(true)
 			.then(function(){
 				console.log('Success toggling all karas');
