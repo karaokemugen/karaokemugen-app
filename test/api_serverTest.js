ddf6b944a870cad9b815d69ff11152443a41262e
Guillaume Lebigot <axel@teri-chan.net>
2017-08-15 00:43:57 +0200

    Fix des premiers tests + ajout de quelques tests avec différents flags (#44)
    Ca commence à doucement prendre forme. Par contre ça a donné naissance à
    l'issue #77, comme quoi les tests unitaires ça sert à quelque chose.
    
    Techniquement on pourrait lancer un npm test dans le gitlab-ci.yml mais
    j'ai viré le lancement de l'app depuis le js de tests car ça foutait la
    merde, l'app n'était pas prête avant les tests.
    
    Le mieux sera de faire en sorte que le runner lance l'app, attende un
    peu, puis lance les tests. Un script shell devra faire l'affaire. Ou
    alors un retry sur le premier test. A moins que le runner soit très
    occupé, ça ne devrait pas échouer.
    
    Bref il va falloir tester les tests lol

--- test/api_serverTest.js
+++ test/api_serverTest.js
@@ -1,10 +1,14 @@
-var assert = require('assert');
+const assert = require('assert');
 const supertest = require('supertest');
-require('../src/index.js');
-var request = supertest('http://localhost:1339');
+const request = supertest('http://localhost:1339');
+const fs = require('fs');
+const path = require('path');
+const password = 'gurdil';
+
+
 
 describe('GET /api/v1/public/', function() {
-	it('should return Hello Word', function(done) {
+	it('should return Hello World', function(done) {
 		request
 			.get('/api/v1/public/')
 			.set('Accept', 'application/json')
@@ -13,63 +17,139 @@ describe('GET /api/v1/public/', function() {
 	});
 });
 
-describe('GET /api/v1/public/karas?filter=Inspecteur', function() {
-	it('get a kara should return inspecteur gadget', function() {
+describe('GET /api/v1/public/karas?filter=Dragon', function() {
+	it('should return Dragon Ball as first karaoke song', function() {
 		return request
-			.get('/api/v1/public/karas?filter=Inspecteur')
+			.get('/api/v1/public/karas?filter=Dragon')
 			.set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200)
 			.then(function(response) {
-				assert.equal(response.body[0].NORM_serie, 'Inspecteur Gadget');
+				assert.equal(response.body[0].NORM_serie, 'Dragon Ball');
 			});
 	});
 });
 
 describe('GET /api/v1/admin/playlists', function() {
-	it('should return 2 playlists', function() {
+	it('should return 2 or more playlists', function() {
 		return request
 			.get('/api/v1/admin/playlists')
 			.set('Accept', 'application/json')
-			.auth('admin', 'shami')
+			.auth('admin', password)
 			.expect('Content-Type', /json/)
 			.expect(200)
 			.then(function(response) {
-				assert.equal(response.body.length, 2);
-				assert.equal(response.body[0].name, 'Liste de lecture publique');
+				var result = false;
+				if (response.body.length >= 2) result = true;
+				assert.equal(result, true);								
 			});
 	});
 });
 
-describe('POST / DELETE /api/v1/admin/playlists ', function() {
+
+describe('POST / DELETE / PUT /api/v1/admin/playlists ', function() {
 	var playlist = {
 		'name':'new_playlist',
 		'flag_visible':true,
 		'flag_public':false,
 		'flag_current':false
 	};
+	var playlist_current = {
+		'name':'new_playlist',
+		'flag_visible':true,
+		'flag_public':false,
+		'flag_current':true
+	};
+	var playlist_public = {
+		'name':'new_playlist',
+		'flag_visible':true,
+		'flag_public':true,
+		'flag_current':false
+	};
 	var new_playlist_id;
+	var new_playlist_current_id;
+	var new_playlist_public_id;
 	it('POST a new playlist', function() {
 		return request
 			.post('/api/v1/admin/playlists')
 			.set('Accept', 'application/json')
-			.auth('admin', 'shami')
+			.auth('admin', password)
 			.send(playlist)
 			.expect('Content-Type', /json/)
 			.expect(201)
 			.then(function(response) {
-				new_playlist_id = response.body.new_playlist_id;
+				new_playlist_id = response.body;
 			});
 	});
-
-	it('DELETE the new playlist ', function() {
+	it('POST a new CURRENT playlist', function() {
 		return request
-			.delete('/api/v1/admin/playlists/'+new_playlist_id)
+			.post('/api/v1/admin/playlists')
 			.set('Accept', 'application/json')
-			.auth('admin', 'shami')
+			.auth('admin', password)
+			.send(playlist_current)
 			.expect('Content-Type', /json/)
 			.expect(201)
 			.then(function(response) {
+				new_playlist_current_id = response.body;
+			});
+	});
+	it('POST a new PUBLIC playlist', function() {
+		return request
+			.post('/api/v1/admin/playlists')
+			.set('Accept', 'application/json')
+			.auth('admin', password)
+			.send(playlist_public)
+			.expect('Content-Type', /json/)
+			.expect(201)
+			.then(function(response) {
+				new_playlist_public_id = response.body;
+			});
+	});
+	
+	it('PUT (edit) a playlist', function() {
+		return request
+			.put('/api/v1/admin/playlists/'+new_playlist_id)
+			.set('Accept', 'application/json')
+			.auth('admin', password)
+			.send(playlist)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then(function(response) {
+				assert.equal(response.body,'Playlist '+new_playlist_id+' updated')
+			});
+	});
+	it('PUT (edit) a CURRENT playlist', function() {
+		return request
+			.put('/api/v1/admin/playlists/'+new_playlist_current_id)
+			.set('Accept', 'application/json')
+			.auth('admin', password)
+			.send(playlist)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then(function(response) {
+				assert.equal(response.body,'Playlist '+new_playlist_current_id+' updated')
+			});
+	});
+	it('PUT (edit) a PUBLIC playlist', function() {
+		return request
+			.put('/api/v1/admin/playlists/'+new_playlist_public_id)
+			.set('Accept', 'application/json')
+			.auth('admin', password)
+			.send(playlist)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then(function(response) {
+				assert.equal(response.body,'Playlist '+new_playlist_public_id+' updated')
+			});
+	});
+	it('DELETE a playlist ', function() {
+		return request
+			.delete('/api/v1/admin/playlists/'+new_playlist_id)
+			.set('Accept', 'application/json')
+			.auth('admin', password)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.then(function(response) {				
 				assert.equal(response.body, 'Deleted '+new_playlist_id)
 			});
 	});
