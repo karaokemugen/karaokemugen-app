8b7293a29781c38bfde9d7c49dc831a86b24eb8d
Ziassan <coc5@free.fr>
2017-08-15 17:44:59 +0200

    #2 update webapp, addition début partie public, création de fichiers js/css communs

--- src/_webapp/js/admin.js
+++ src/_webapp/js/admin.js
@@ -1,12 +1,7 @@
 $(document).ready(function () {
     /*** INITIALISATION ***/
     /* variables & ajax setup */
-    var stopUpdate = false;
     var mouseDown = false;
-    var oldState = {};
-    $.ajaxPrefilter(function (options) {
-        options.url = window.location.protocol + "//" + window.location.hostname + ":1339/api/v1/" + options.url
-    });
 
     setupAjax = function (passwordAdmin) {
 
@@ -36,65 +31,6 @@ $(document).ready(function () {
 
     /* init functions */
 
-    // refresh screen depending on player infos
-    refreshCommandStates = function (callback, param1) {
-        $.ajax({ url: 'public/player' }).done(function (data) {
-            if (oldState != data) {
-                if (oldState.status != data.status || oldState.playerstatus != data.playerstatus) {
-                    var status = data.status === "stop" ? "stop" : data.playerstatus;
-                    //console.log("status : " + status + " enginestatus : " + data.status  + " playerstatus : " + data.playerstatus );
-                    switch (status) {
-                        case "play":
-                            $('#play').find('i').attr('class', 'glyphicon glyphicon-pause');
-                            $('#play').val('pause');
-                            break;
-                        case "pause":
-                            $('#play').find('i').attr('class', 'glyphicon glyphicon-play');
-                            $('#play').val('play');
-                            break;
-                        case "stop":
-                            $('#play').find('i').attr('class', 'glyphicon glyphicon-play');
-                            $('#play').val('play');
-                            break;
-                        default:
-                            console.log("ERR : Kara status unknown : " + status);
-                    }
-                }
-                if (data.currentlyplaying !== oldState.currentlyplaying && data.currentlyplaying > 0) {
-                    $.ajax({ url: 'public/karas/' + data.currentlyplaying }).done(function (dataKara) {
-                        $('#karaInfo').attr('idKara', dataKara[0].id_kara);
-                        $('#karaInfo').text([dataKara[0].language.toUpperCase(), dataKara[0].serie, dataKara[0].songtype_i18n_short, dataKara[0].title].join(" - "));
-                        $('#karaInfo').attr('length', dataKara[0].duration);
-                    });
-                }
-                if (data.timeposition != oldState.timeposition && $('#karaInfo').attr('length') != 0) {
-                    console.log(data.timeposition);
-                    $('#progressBarColor').width(100 * data.timeposition / $('#karaInfo').attr('length') + '%');
-                }
-                if (data.mutestatus != oldState.mutestatus) {
-                    if (data.mutestatus) {
-                        $('#volume').find('i').attr('class', 'glyphicon glyphicon-volume-off');
-                        $('#volume').val('unmute');
-                    } else {
-                        $('#volume').find('i').attr('class', 'glyphicon glyphicon-volume-up');
-                        $('#volume').val('mute');
-                    }
-                }
-                if (data.ontop != oldState.ontop) {
-                    $('input[name="toggleAlwaysOnTop"]').bootstrapSwitch('state', data.ontop, true);
-                }
-                if (data.fullscreen != oldState.fullscreen) {
-                    $('input[name="toggleFullscreen"]').bootstrapSwitch('state', data.fullscreen, true);
-                }
-
-                oldState = data;
-                if (callback && typeof callback === "function" && typeof param1 != "undefined") {
-                    callback(param1);
-                }
-            }
-        });
-    };
-
     fillPlaylistSelects = function () {
         var playlistList = {};
         $.ajax({ url: 'admin/playlists', }).done(function (data) {
@@ -114,22 +50,6 @@ $(document).ready(function () {
                 $('#consolelog').html($('#consolelog').html() + index + ': ' + value + '<br/>');
             });
 
-        });
-    };
-
-    initSwitchs = function () {
-        $("input[switch='onoff'],[name='EnginePrivateMode'],[name='kara_panel']").bootstrapSwitch('destroy', true);
-
-        $("input[switch='onoff']").bootstrapSwitch({
-            wrapperClass: "btn btn-default",
-            "data-size": "normal"
-        });
-        $("[name='EnginePrivateMode'],[name='kara_panel']").bootstrapSwitch({
-            "wrapperClass": "btn btn-default",
-            "data-size": "large",
-            "labelWidth": "0",
-            "handleWidth": "65",
-            "data-inverse": "false"
         });
     };
 
@@ -171,8 +91,7 @@ $(document).ready(function () {
                 });
                 settingsArray['EnginePrivateMode'] = $('input[name="EnginePrivateMode"]').val();
                 settingsArray['AdminPassword'] = changeAdminPass ? $('button[name="AdminPassword"]').val() : $('button[name="AdminPassword"]').attr('oldValue');
-                console.log("SETTINGS", settingsArray);
-
+          
                 $.ajax({
                     type: 'PUT',
                     url: 'admin/settings',
@@ -180,7 +99,6 @@ $(document).ready(function () {
                 }).done(function (data) {
                     if (changeAdminPass) {
                         mdpAdmin = $('button[name="AdminPassword"]').val();
-                        console.log(mdpAdmin);
                         setupAjax(mdpAdmin);
                     }
                     getSettings();
@@ -191,19 +109,9 @@ $(document).ready(function () {
     /* init selects & switchs */
 
     fillPlaylistSelects();
-    initSwitchs();
-    refreshCommandStates();
-    setInterval(function () {
-        if (!stopUpdate) {
-            refreshCommandStates("refresh");
-        }
-    }, 1000);
-
     getSettings();
 
-
     $("[name='kara_panel']").on('switchChange.bootstrapSwitch', function (event, state) {
-        console.log(this, event, state);
         if (state) {
             $('#playlist').hide();
             $('#manage').show();
@@ -217,7 +125,23 @@ $(document).ready(function () {
         // FCT send playlist state (1=private 0=public)
 
     });
+    
+    // handling small touchscreen screens with big virtual keyboard
+    
+    $("select[type='playlist_select']").on('select2:open', function(){
+        //$('#header').hide();
+         $(".select2-dropdown").css('z-index','9999');
+    });
+    
+    $("select[type='playlist_select']").on('select2:close', function(){
+       // $('#header').show();
+        $(".select2-dropdown").css('z-index','1051');
+        document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+        document.documentElement.scrollTop = 0; // For IE and Firefox
+    })
+    
     // get & build kara list on screen
+
     $("select[type='playlist_select']").change(function () {
         var val = $(this).val();
         var num = $(this).attr('num');
@@ -239,22 +163,7 @@ $(document).ready(function () {
         } else if (val == -1) {
             urlKaras = 'public/karas';
         }
-
-        $.ajax({ url: urlKaras }).done(function (data) {
-            console.time('profile');
-            htmlList = "";
-            for (var key in data) {
-                if (data.hasOwnProperty(key)) {
-                    if (data[key].language === null) data[key].language = "";
-                    htmlList += "<li idKara='" + data[key].id_kara + "' class='list-group-item'>"
-                        + [data[key].language.toUpperCase(), data[key].serie, data[key].songtype_i18n_short, data[key].title].join(" - ")
-                        + '<span class="badge">' + data[key].language.toUpperCase() + '</span>' + buttonHtml + "</li>";
-
-                }
-            }
-            document.getElementById('playlist' + num).innerHTML = htmlList;
-            var time = console.timeEnd('profile');
-        });
+        fillPlaylist('playlist' + num, urlKaras, 'list', buttonHtml);
     });
 
     // TODO change everything, global PUT followed by playlist refresh showing right client infos
@@ -283,11 +192,14 @@ $(document).ready(function () {
     };
 
     $('button[action="command"]').click(function () {
-        val = $(this).val();
+        var val = $(this).val();
+        var dataAjax = { command : val };
+        if ($(this).attr('options') != undefined) dataAjax['options'] = $(this).attr('options');
+
         $.ajax({
             url: 'admin/player',
             type: 'PUT',
-            data: { command: val }
+            data: dataAjax
         }).done(function (data) {
             refreshCommandStates();
         });
@@ -324,18 +236,17 @@ $(document).ready(function () {
 
     goToPosition = function (e) {
         var karaInfo = $('#karaInfo');
-         var songLength = karaInfo.attr('length');
+        var songLength = karaInfo.attr('length');
         var barInnerwidth = karaInfo.innerWidth();
         var futurTimeX = e.pageX - karaInfo.offset().left;
         var presentTimeX = $(progressBarColor).width();
-        var difference = futurTimeX - presentTimeX;
-        var differenceRatio = difference / barInnerwidth;
-        var differenceTime = Math.round(differenceRatio * songLength);
-        $(progressBarColor).width(100 * (presentTimeX / barInnerwidth + differenceTime / songLength) + "%");
+        var futurTimeSec = Math.round(songLength * futurTimeX / barInnerwidth);
+        $(progressBarColor).width(100 * futurTimeSec / songLength + "%");
+        
         $.ajax({
             url: 'admin/player',
             type: 'PUT',
-            data: { command: 'seek', options: differenceTime }
+            data: { command: 'goTo', options: futurTimeSec }
         })
             .done(function (data) {
                 refreshCommandStates(setStopUpdate, false);
@@ -345,17 +256,17 @@ $(document).ready(function () {
         refreshCommandStates(goToPosition, e);
     });
 
-    $('#karaInfo').mousedown(function (e) {
+    $('#karaInfo').on('mousedown touchstart', function (e) {
         stopUpdate = true;
         mouseDown = true;
-        $(progressBarColor).width(e.pageX + "px");
+        $(progressBarColor).width( e.pageX + "px");
     });
     $('#karaInfo').mouseup(function (e) {
         mouseDown = false;
     });
     $('#karaInfo').mousemove(function (e) {
         if (mouseDown) {
-            $(progressBarColor).width(e.pageX + "px");
+            $(progressBarColor).width( e.pageX + "px");
         }
     });
     $('#karaInfo').mouseout(function (e) {
@@ -365,7 +276,6 @@ $(document).ready(function () {
             refreshCommandStates();
         }
     });
-
 
     /* password case handlers */
 
