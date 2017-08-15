750aab3f862deb2d8f0fa43e9f1b183810c4dac3
Ziassan <coc5@free.fr>
2017-08-15 17:44:59 +0200

    #2 update webapp, addition début partie public, création de fichiers js/css communs

---
+++ src/_webapp/js/public.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    fillPlaylist('playlist2', 'public/playlists/public/karas', 'list', '');
+    fillPlaylist('playlist1', 'public/karas', 'list', '');
+    
+   
+  //  alert($(window).width() + "-" + $(window).height());
+});
