6f36ae5e517c399a702f7f0bd940f88d174302c1
Unknown <coc5@free.fr>
2017-08-21 00:40:18 +0200

    #2 fonctions de playlists, details kara + lyrics kara, revue css/html

--- src/_webapp/js/admin.js
+++ src/_webapp/js/admin.js
@@ -1,8 +1,13 @@
-$(document).ready(function () {
+  (function(yourcode) {
+    // The global jQuery object is passed as a parameter
+  	yourcode(window.jQuery, window, document);
+  }(function($, window, document) {
+    // The $ is now locally scoped 
+   // Listen for the jQuery ready event on the document
+   $(function() {
 
     /* init selects & switchs */
 
-    
     $("[name='kara_panel']").on('switchChange.bootstrapSwitch', function (event, state) {
         if (state) {
             $('#playlist').show();
@@ -116,147 +121,144 @@ $(document).ready(function () {
     fillPlaylistSelects("admin");
     getSettings();
 
-    $(window).trigger('resize');
-
     pseudo = "Administrateur";
-});
+   });
 
-/*** INITIALISATION ***/
-/* variables & ajax setup */
+    /*** INITIALISATION ***/
+    /* variables & ajax setup */
 
-var mouseDown = false;
-var scope = 'admin';
+    mouseDown = false;
+    scope = 'admin';
 
-setupAjax = function (passwordAdmin) {
+    setupAjax = function (passwordAdmin) {
 
-    $.ajaxSetup({
-        headers: { "Authorization": "Basic " + btoa("truc:" + passwordAdmin) },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(jqXHR.status + "  - " + textStatus + "  - " + errorThrown);
+        $.ajaxSetup({
+            headers: { "Authorization": "Basic " + btoa("truc:" + passwordAdmin) },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR.status + "  - " + textStatus + "  - " + errorThrown);
+            }
+        });
+    }
+
+    setupAjax(mdpAdmin);
+
+    // dynamic creation of switchable settings 
+    var settingsOnOff = ["PlayerPIP", "EngineAllowNicknameChange", "EngineAllowViewBlacklist", "EngineAllowViewBlacklistCriterias"
+        , "EngineAllowViewWhitelist", "EngineDisplayNickname", "PlayerFullscreen", "PlayerStayOnTop"
+        , "PlayerNoBar", "PlayerNoHud"];
+    var htmlSettings = ""
+    $.each(settingsOnOff, function (e, val) {
+        html = '<div class="form-group"><label for="' + val + '" class="col-xs-4 control-label">' + val + '</label>'
+            + '<div class="col-xs-6"> <input switch="onoff" type="checkbox" name="' + val + '"></div></div>';
+        if (val === "PlayerPIP") {
+            $(html).insertBefore('#pipSettings')
+        } else {
+            htmlSettings += html;
         }
     });
-}
+    $('#settings').append(htmlSettings);
 
-setupAjax(mdpAdmin);
-
-// dynamic creation of switchable settings 
-var settingsOnOff = ["PlayerPIP", "EngineAllowNicknameChange", "EngineAllowViewBlacklist", "EngineAllowViewBlacklistCriterias"
-    , "EngineAllowViewWhitelist", "EngineDisplayNickname", "PlayerFullscreen", "PlayerStayOnTop"
-    , "PlayerNoBar", "PlayerNoHud"];
-$.each(settingsOnOff, function (e, val) {
-    html = $('<div class="form-group"><label for="' + val + '" class="col-xs-4 control-label">' + val + '</label>'
-        + '<div class="col-xs-6"> <input switch="onoff" type="checkbox" name="' + val + '"></div></div>');
-    if (val === "PlayerPIP") {
-        html.insertBefore('#pipSettings')
-    } else {
-        $('#settings').append(html);
-    }
-});
-
-
-// nameExclude = input not being updated (most likely user is on it)
-getSettings = function (nameExclude) {
-    var playlistList = {};
-    $.ajax({ url: 'admin/settings' }).done(function (data) {
-        $.each(data, function (i, val) {
-            input = $('[name="' + i + '"]');
-            // console.log(i, val);
-            if (input.length == 1 && i != nameExclude) {
-                if (input.attr('type') !== "checkbox") {
-                    input.val(val);
-                } else {
-                    input.bootstrapSwitch('state', val, true);
-                    input.val(val);
-                    if (input.attr('name') === "PlayerPIP") {
-                        val ? $('#pipSettings').show() : $('#pipSettings').hide();
+    // nameExclude = input not being updated (most likely user is on it)
+    getSettings = function (nameExclude) {
+        var playlistList = {};
+        $.ajax({ url: 'admin/settings' }).done(function (data) {
+            $.each(data, function (i, val) {
+                input = $('[name="' + i + '"]');
+                // console.log(i, val);
+                if (input.length == 1 && i != nameExclude) {
+                    if (input.attr('type') !== "checkbox") {
+                        input.val(val);
+                    } else {
+                        input.bootstrapSwitch('state', val, true);
+                        input.val(val);
+                        if (input.attr('name') === "PlayerPIP") {
+                            val ? $('#pipSettings').show('500') : $('#pipSettings').hide('500');
+                        }
                     }
                 }
-            }
-        });
-    });
-}
-
-setSettings = function (e, changeAdminPass) {
-//    console.log( $(e).attr('name'), $(e).val(), $(e));
-    if (e.attr('oldValue') !== e.val() || e.attr('type') === "checkbox") {
-        getSettings(e.attr('name'));
-
-        $('#settings').promise().then(function () {
-            settingsArray = {};
-            formArray = $('#settings').serializeArray()
-                .concat($('#settings input[type=checkbox]:not(:checked)').map(function () { return { name: this.name, value: "0" }; }).get());
-
-            $(formArray).each(function (index, obj) {
-                settingsArray[obj.name] = obj.value;
             });
-            settingsArray['EnginePrivateMode'] = $('input[name="EnginePrivateMode"]').val();
-            settingsArray['AdminPassword'] = changeAdminPass ? $('button[name="AdminPassword"]').val() : $('button[name="AdminPassword"]').attr('oldValue');
+        });
+    }
 
-            console.log("setSettings : ", settingsArray);
+    setSettings = function (e, changeAdminPass) {
+    //    console.log( $(e).attr('name'), $(e).val(), $(e));
+        if (e.attr('oldValue') !== e.val() || e.attr('type') === "checkbox") {
+            getSettings(e.attr('name'));
 
+            $('#settings').promise().then(function () {
+                settingsArray = {};
+                formArray = $('#settings').serializeArray()
+                    .concat($('#settings input[type=checkbox]:not(:checked)').map(function () { return { name: this.name, value: "0" }; }).get());
+
+                $(formArray).each(function (index, obj) {
+                    settingsArray[obj.name] = obj.value;
+                });
+                settingsArray['EnginePrivateMode'] = $('input[name="EnginePrivateMode"]').val();
+                settingsArray['AdminPassword'] = changeAdminPass ? $('button[name="AdminPassword"]').val() : $('button[name="AdminPassword"]').attr('oldValue');
+
+                console.log("setSettings : ", settingsArray);
+
+                $.ajax({
+                    type: 'PUT',
+                    url: 'admin/settings',
+                    data: settingsArray
+                }).done(function (data) {
+                    if (changeAdminPass) {
+                        mdpAdmin = $('button[name="AdminPassword"]').val();
+                        setupAjax(mdpAdmin);
+                    }
+                    getSettings();
+                });
+            });
+        }
+    }
+    
+    // TODO change everything, global PUT followed by playlist refresh showing right client infos
+    transfer = function (e) {
+        var num = $(e).attr('num');
+        var newNum = 3 - num;
+        var idPlaylistFrom = $("#selectPlaylist2"+ num).val();
+        var idPlaylistTo = $("#selectPlaylist2" + newNum).val();
+
+        if (idPlaylistFrom == -1) {
             $.ajax({
-                type: 'PUT',
-                url: 'admin/settings',
-                data: settingsArray
-            }).done(function (data) {
-                if (changeAdminPass) {
-                    mdpAdmin = $('button[name="AdminPassword"]').val();
-                    setupAjax(mdpAdmin);
+                type: 'POST',
+                url: 'admin/playlists/' + idPlaylistTo + '/karas',
+                data: {
+                    requestedby: 'admin',
+                    kara_id: $(e).parent().attr('idKara')
                 }
-                getSettings();
+            }).done(function (data) {
+                console.log(data);
+                $(e).parent().clone().appendTo('#playlist' + newNum);
             });
-        });
-    }
-}
+        } else {
+            $(e).attr('num', newNum);
+            $(e).parent().detach().appendTo('#playlist' + newNum);
+        }
+    };
 
-$()    
-// TODO change everything, global PUT followed by playlist refresh showing right client infos
-transfer = function (e) {
-    var num = $(e).attr('num');
-    var newNum = 3 - num;
-    var idPlaylistFrom = $("#selectPlaylist2"+ num).val();
-    var idPlaylistTo = $("#selectPlaylist2" + newNum).val();
+    /* progression bar handlers part */
 
-    if (idPlaylistFrom == -1) {
+    goToPosition = function (e) {
+        var karaInfo = $('#karaInfo');
+        var songLength = karaInfo.attr('length');
+        var barInnerwidth = karaInfo.innerWidth();
+        var futurTimeX = e.pageX - karaInfo.offset().left;
+        var presentTimeX = $(progressBarColor).width();
+        var futurTimeSec = Math.round(songLength * futurTimeX / barInnerwidth);
+        $(progressBarColor).stop().css('width', 100 * futurTimeSec / songLength + "%");
+
         $.ajax({
-            type: 'POST',
-            url: 'admin/playlists/' + idPlaylistTo + '/karas',
-            data: {
-                requestedby: 'admin',
-                kara_id: $(e).parent().attr('idKara')
-            }
-        }).done(function (data) {
-            console.log(data);
-            $(e).parent().clone().appendTo('#playlist' + newNum);
-        });
-    } else {
-        $(e).attr('num', newNum);
-        $(e).parent().detach().appendTo('#playlist' + newNum);
+            url: 'admin/player',
+            type: 'PUT',
+            data: { command: 'goTo', options: futurTimeSec }
+        })
+            .done(function (data) {
+                refreshCommandStates(setStopUpdate, false);
+            });
     }
-};
 
-/* progression bar handlers part */
+}));
+	
 
-goToPosition = function (e) {
-    var karaInfo = $('#karaInfo');
-    var songLength = karaInfo.attr('length');
-    var barInnerwidth = karaInfo.innerWidth();
-    var futurTimeX = e.pageX - karaInfo.offset().left;
-    var presentTimeX = $(progressBarColor).width();
-    var futurTimeSec = Math.round(songLength * futurTimeX / barInnerwidth);
-    $(progressBarColor).stop().css('width', 100 * futurTimeSec / songLength + "%");
-
-    $.ajax({
-        url: 'admin/player',
-        type: 'PUT',
-        data: { command: 'goTo', options: futurTimeSec }
-    })
-        .done(function (data) {
-            refreshCommandStates(setStopUpdate, false);
-        });
-}
-$(window).resize(function () {
-    //  initSwitchs();$
-    var topHeight = $('.panel-heading.container-fluid').outerHeight();
-    $('#playlist1,#playlist2').css('height','calc(100% - ' + topHeight + 'px  ');
-});
