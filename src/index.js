5d4ad62aee371ff34d05d41a8b96bd4651bee5bc
Guillaume Lebigot <axel@teri-chan.net>
2017-09-27 22:03:28 +0200

    Retrait du catcheur d'exception en double (pourquoi, mystère)

--- src/index.js
+++ src/index.js
@@ -22,12 +22,6 @@ process.on('uncaughtException', function (exception) {
 	// email as well ?
 });
 
-process.on('uncaughtException', function (exception) {
-	console.log(exception); // to see your exception details in the console
-	// if you are on production, maybe you can send the exception details to your
-	// email as well ?
-});
-
 /**
  * Clear console - and welcome message
  * Node does not like the octal clear screen sequence.
