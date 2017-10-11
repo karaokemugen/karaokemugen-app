0c12cce732969bccaae7bb347ef540d431511fad
Guillaume Lebigot <axel@teri-chan.net>
2017-10-11 14:52:59 +0200

    Ajout time_before_play sur les infos kara d'une PL (#184)

--- src/_common/db/select_plcontent_info.sql
+++ src/_common/db/select_plcontent_info.sql
@@ -38,8 +38,13 @@ SELECT ak.kara_id AS kara_id,
       (CASE WHEN bl.fk_id_kara = ak.kara_id
 	      THEN 1
         ELSE 0
-      END) AS flag_blacklisted
-FROM karasdb.all_karas AS ak 
+      END) AS flag_blacklisted,
+	  (SELECT ifnull(SUM(all_karas.videolength),0)
+    	FROM karasdb.all_karas AS all_karas
+    	INNER JOIN playlist_content ON all_karas.kara_id = playlist_content.fk_id_kara
+    	WHERE playlist_content.fk_id_playlist = pc.fk_id_playlist
+    	AND playlist_content.pos BETWEEN (SELECT ifnull(pos,0) FROM playlist_content WHERE flag_playing = 1) AND pc.pos) AS time_before_play
+FROM karasdb.all_karas AS ak
 INNER JOIN playlist_content AS pc ON pc.fk_id_kara = ak.kara_id
 LEFT OUTER JOIN blacklist AS bl ON ak.kara_id = bl.fk_id_kara
 LEFT OUTER JOIN playlist AS p ON pc.fk_id_playlist = p.pk_id_playlist
