e7d928f1d74c3cc5bee00d7c9f3d63fce8c34b5f
Benoît Courtine <benoit.courtine@gmail.com>
2017-10-19 14:18:59 +0200

    Résolution du problème de parsing des arguments passés à babel-node.
    Cette résolution passe par une méthode parseArgs (dans index.js et logger.js), nécessaire pour contourner
    un bug non résolu de babel-node : https://github.com/babel/babel/issues/5542.

--- src/_common/utils/logger.js
+++ src/_common/utils/logger.js
@@ -1,9 +1,11 @@
-const argv = require('minimist')(process.argv.slice(2));
+import minimist from 'minimist';
+
 const path = require('path');
 const tsFormat = () => (new Date()).toLocaleTimeString();
 const winston = require('winston');
 require('winston-daily-rotate-file');
 const SYSPATH = require('./resolveSyspath.js')('config.ini.default',__dirname,['./','../','../../','../../../']);
+
 const logger = new (winston.Logger)({
 	transports: [
 		new (winston.transports.Console)({ timestamp: tsFormat, level: 'info', colorize: true }),
@@ -11,7 +13,9 @@ const logger = new (winston.Logger)({
 	]
 });
 
-if (argv.dbg) {
+const argv = parseArgs();
+
+if (argv.debug) {
 	logger.configure({
 		transports: [
 			new (winston.transports.Console)({ timestamp: tsFormat, level: 'debug', colorize: true }),
@@ -21,3 +25,15 @@ if (argv.dbg) {
 }
 
 module.exports = logger;
+
+/**
+ * Fonction de contournement du bug https://github.com/babel/babel/issues/5542
+ * A supprimer une fois que celui-ci sera résolu.
+ */
+function parseArgs() {
+	if (process.argv.indexOf('--') >= 0) {
+		return minimist(process.argv.slice(3));
+	} else {
+		return minimist(process.argv.slice(2));
+	}
+}
