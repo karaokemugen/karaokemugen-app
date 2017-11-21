f5c43d88ee8d6f08eb284944bef26a3c4f47687a
Guillaume Lebigot <axel@teri-chan.net>
2017-11-21 11:55:15 +0100

    Fix du commit précédent (#115)

--- src/_common/db/migrations/userdata/007-add_admin_user.sql
+++ src/_common/db/migrations/userdata/007-add_admin_user.sql
@@ -1,6 +1,6 @@
 -- Up 
 
-INSERT INTO user(login,password,nickname,NORM_nickname,flag_admin,guest_expires) VALUES ('admin','5e8742c987dbc38d0d0b5139926c64cd5c552989f71cc8e73652b36f161ff942','Administrator','Administrator',1,0);
+INSERT INTO user(login,password,nickname,NORM_nickname,flag_admin,last_login) VALUES ('admin','5e8742c987dbc38d0d0b5139926c64cd5c552989f71cc8e73652b36f161ff942','Administrator','Administrator',0,0);
 
 -- Down
 
