b11245839efe817b63a0054e608e3c98b526a24d
Guillaume Lebigot <axel@teri-chan.net>
2017-12-19 14:45:34 +0100

    Arrow functions = best (#226)

--- src/_common/utils/ass.js
+++ src/_common/utils/ass.js
@@ -4,10 +4,10 @@ export function ASSToLyrics(ass) {
 	let lyrics = [];			
 	let script = assParser(ass, { comments: true });
 	let DialogueSection;
-	script.forEach(function(ASSSection,index){
+	script.forEach((ASSSection,index) => {
 		if (ASSSection.section == 'Events') DialogueSection = index;
 	});
-	script[DialogueSection].body.forEach(function(param){
+	script[DialogueSection].body.forEach((param) => {
 		if (param.key == 'Dialogue') lyrics.push(param.value.Text.replace(/\{(?:.|\n)*?\}/gm, ''));
 	});		
 	return lyrics;	
