d22c2da045b38283b714509bd2c975d84b326ab2
Axel Terizaki <axel@teri-chan.net>
2018-02-22 15:39:01 +0100

    Made upvote table refencing others for delete cascade (#167)

--- src/_common/db/migrations/userdata/012-add_upvote_table.sql
+++ src/_common/db/migrations/userdata/012-add_upvote_table.sql
@@ -1,8 +1,8 @@
 -- Up
 
 CREATE TABLE upvote (
-	fk_id_plcontent	INTEGER NOT NULL,
-	fk_id_user	INTEGER NOT NULL
+	fk_id_plcontent	INTEGER NOT NULL REFERENCES playlist_content ON DELETE CASCADE,
+	fk_id_user	INTEGER NOT NULL REFERENCES user ON DELETE CASCADE
 );
 
 CREATE INDEX IF NOT EXISTS index_upvote ON upvote (fk_id_plcontent,fk_id_user);
