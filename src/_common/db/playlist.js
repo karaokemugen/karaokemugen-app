b0e2fca95388a7fd72641ba456cd5fa57290b90d
Axel Terizaki <axel@teri-chan.net>
2018-02-25 18:33:38 +0100

    Fix getPlaylistContent from automerging #151

--- src/_common/db/playlist.js
+++ src/_common/db/playlist.js
@@ -135,6 +135,7 @@ export const getPlaylistContents = `SELECT ak.kara_id AS kara_id,
 										(SELECT max(vc.modified_at) FROM viewcount AS vc WHERE vc.fk_id_kara = ak.kara_id) AS lastplayed_at
 									FROM karasdb.all_karas AS ak 
 									INNER JOIN playlist_content AS pc ON pc.fk_id_kara = ak.kara_id
+									LEFT OUTER JOIN user AS u ON u.pk_id_user = pc.fk_id_user
 									LEFT OUTER JOIN blacklist AS bl ON ak.kara_id = bl.fk_id_kara
 									LEFT OUTER JOIN whitelist AS wl ON ak.kara_id = wl.fk_id_kara
 									WHERE pc.fk_id_playlist = $playlist_id
@@ -209,7 +210,7 @@ export const getPLCInfo = `SELECT ak.kara_id AS kara_id,
         							ELSE 0
       							END) AS flag_blacklisted,
 	  							(SELECT ifnull(SUM(all_karas.videolength) - ak.videolength,0)
-    							FROM karasdb.all_karas AS all_karas
+								FROM karasdb.all_karas AS all_karas
     							INNER JOIN playlist_content ON all_karas.kara_id = playlist_content.fk_id_kara
     							WHERE playlist_content.fk_id_playlist = pc.fk_id_playlist
     							AND playlist_content.pos BETWEEN (SELECT ifnull(pos,0) FROM playlist_content WHERE flag_playing = 1) AND pc.pos) AS time_before_play,
@@ -220,6 +221,7 @@ export const getPLCInfo = `SELECT ak.kara_id AS kara_id,
 								(SELECT max(vc.modified_at) FROM viewcount AS vc WHERE vc.fk_id_kara = ak.kara_id) AS lastplayed_at
 						FROM karasdb.all_karas AS ak
 						INNER JOIN playlist_content AS pc ON pc.fk_id_kara = ak.kara_id
+						LEFT OUTER JOIN user AS u ON u.pk_id_user = pc.fk_id_user    	
 						LEFT OUTER JOIN blacklist AS bl ON ak.kara_id = bl.fk_id_kara
 						LEFT OUTER JOIN playlist AS p ON pc.fk_id_playlist = p.pk_id_playlist
 						LEFT OUTER JOIN whitelist AS wl ON ak.kara_id = wl.fk_id_kara
