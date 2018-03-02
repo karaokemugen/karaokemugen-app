e3c1d982fdb5114b696a8c54ca9e3aab46704648
Guillaume Lebigot <axel@teri-chan.net>
2018-03-02 09:12:18 +0100

    Forgot a ; (#199)

--- src/_services/favorites.js
+++ src/_services/favorites.js
@@ -115,7 +115,7 @@ export async function initFavoritesSystem() {
 		const isFavoritePLExists = playlists.some(pl => {
 			if (pl.fk_user_id == user.user_id && pl.flag_favorites == 1 && user.type == 1) return true;
 			return false;
-		})
+		});
 		if (!isFavoritePLExists) await createPlaylist('Faves : '+user.login,0,0,0,1,user.login);
 	}
 }
