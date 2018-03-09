1e307184a9066a1303ae134e901d11d79b0fce5c
Axel Terizaki <axel@teri-chan.net>
2018-03-09 23:14:13 +0100

    Allow admin to use webapp even if mode is restricted or closed (#202)

--- src/_controllers/webapp_mode.js
+++ src/_controllers/webapp_mode.js
@@ -1,7 +1,9 @@
 import {getConfig} from '../_common/utils/config';
+import {decode} from 'jwt-simple';
 
-export const requireWebappLimited = (req, res, next) => {	
-	if (getConfig().WebappMode > 0) {
+export const requireWebappLimited = (req, res, next) => {
+	const token = decode(req.get('authorization'), getConfig().JwtSecret);		
+	if (getConfig().WebappMode > 0 || token.role == 'admin') {
 		next();
 	} else {
 		res.status(503).send('API is in restricted mode');
@@ -9,7 +11,7 @@ export const requireWebappLimited = (req, res, next) => {
 };
 
 export const requireWebappOpen = (req, res, next) => {	
-	if (getConfig().WebappMode > 1) {
+	if (getConfig().WebappMode > 1 || token.role == 'admin') {
 		next();
 	} else {
 		res.status(503).send('API is in restricted mode');
