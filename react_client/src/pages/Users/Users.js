fcff2988f7b1d660b8c800ab3e2bc6ff4848ca06
Benoît Courtine <benoit.courtine@gmail.com>
2018-03-30 12:21:11 +0200

    Amélioration de la liste des utilisateurs : tris/filtres.

--- react_client/src/pages/Users/Users.js
+++ react_client/src/pages/Users/Users.js
@@ -10,35 +10,62 @@ const columns = [{
 	title: 'ID',
 	dataIndex: 'user_id',
 	key: 'user_id',
-	render: userId => <Link to={`/users/${userId}`}>{userId}</Link>
+	render: user_id => <Link to={`/users/${user_id}`}>{user_id}</Link>,
+	defaultSortOrder: 'ascend',
+	sorter: (a, b) => a.user_id - b.user_id
 }, {
 	title: 'Type',
 	dataIndex: 'type',
 	key: 'type',
+	filters: [
+		{ text: 'User', value: '1' },
+		{ text: 'Guest', value: '2' },
+	],
+	filterMultiple: false,
+	onFilter: (value, record) => `${record.type}` === value,
 }, {
 	title: 'Avatar',
 	dataIndex: 'avatar_file',
 	key: 'avatar_file',
+	sorter: (a, b) => a.avatar_file.localeCompare(b.avatar_file)
 }, {
 	title: 'Username',
 	dataIndex: 'login',
 	key: 'login',
+	render: (text, record) => <Link to={`/users/${record.user_id}`}>{text}</Link>,
+	sorter: (a, b) => a.login.localeCompare(b.login)
 }, {
 	title: 'Nickname',
 	dataIndex: 'nickname',
 	key: 'nickname',
+	render: (text, record) => <Link to={`/users/${record.user_id}`}>{text}</Link>,
+	sorter: (a, b) => a.nickname.localeCompare(b.nickname)
 }, {
 	title: 'Last seen on',
 	dataIndex: 'last_login',
 	key: 'last_login',
+	render: (date) => (new Date(+date)).toString(),
+	sorter: (a,b) => a.last_login - b.last_login
 }, {
 	title: 'Logged in?',
 	dataIndex: 'flag_online',
 	key: 'flag_online',
+	filters: [
+		{ text: 'Online', value: '1' },
+		{ text: 'Offline', value: '0' },
+	],
+	filterMultiple: false,
+	onFilter: (value, record) => `${record.flag_online}` === value,
 }, {
 	title: 'Admin?',
 	dataIndex: 'flag_admin',
 	key: 'flag_admin',
+	filters: [
+		{ text: 'Admin', value: '1' },
+		{ text: 'Standard', value: '0' },
+	],
+	filterMultiple: false,
+	onFilter: (value, record) => `${record.flag_admin}` === value,
 }];
 
 class Users extends Component {
