aa55c2d8919dee5fb98ca161c97c86b2985ffff4
Guillaume Lebigot <axel@teri-chan.net>
2018-04-03 12:04:30 +0200

    Fix creating new users

--- src/_controllers/api.js
+++ src/_controllers/api.js
@@ -527,7 +527,7 @@ export function APIControllerAdmin(router) {
 						req.sanitize('password').unescape();
 						req.sanitize('role').trim();
 						req.sanitize('role').unescape();
-						if (req.body.role = 'admin') req.body.flag_admin = 1;
+						if (req.body.role === 'admin') req.body.flag_admin = 1;
 						user.createUser(req.body)
 							.then(() => {
 								res.json(OKMessage(true,'USER_CREATED'));
