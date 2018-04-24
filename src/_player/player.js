548d2d10ecc91c32f5c40d50ae7c258c171fb24e
Guillaume Lebigot <axel@teri-chan.net>
2018-04-24 10:00:12 +0200

    Removed debug & trace from mpv

--- src/_player/player.js
+++ src/_player/player.js
@@ -244,8 +244,8 @@ async function startmpv() {
 			binary: conf.BinmpvPath,
 			socket: socket,
 			time_update: 1,
-			verbose: true,
-			debug: true,
+			verbose: false,
+			debug: false,
 		},
 		mpvOptions
 	);	
