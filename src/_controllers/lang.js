016388d06c8c5ddb4fab3540470671a255603d33
Guillaume Lebigot <axel@teri-chan.net>
2018-05-16 16:41:37 +0200

    What? Accept-Language can actually be EMPTY? (#288)

--- src/_controllers/lang.js
+++ src/_controllers/lang.js
@@ -1,5 +1,7 @@
 export const getLang = (req, res, next) => {
-	const langs = req.get('accept-language').split(',');
-	req.lang = langs[0].substring(0,2);	
+	const langs = req.get('accept-language')
+	if (langs) {
+		req.lang = langs.split(',')[0].substring(0,2);	
+	}	
 	next();
 };
