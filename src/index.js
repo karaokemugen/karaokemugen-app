7cdd215a4a842de8c859e0dbe88f168396c22662
Guillaume Lebigot <axel@teri-chan.net>
2018-05-23 12:40:02 +0200

    Moved backgroundImage to VERSION

--- src/index.js
+++ src/index.js
@@ -13,9 +13,6 @@ import {logo} from './logo';
 import chalk from 'chalk';
 import {createInterface} from 'readline';
 
-// Background image for this version
-const backgroundImage = 'HaruhiHagiographique.jpg';
-
 process.on('uncaughtException', function (exception) {
 	console.log(exception);
 });
@@ -87,7 +84,7 @@ async function main() {
 	);
 	logger.debug('[Launcher] Copying default background to to ' + resolve(appPath, config.PathTemp));
 	await copy(
-		join(__dirname, `/_player/assets/${backgroundImage}`),
+		join(__dirname, `/_player/assets/${config.VersionImage}`),
 		resolve(appPath, config.PathTemp, 'default.jpg'),
 		{ overwrite: true }
 	);
