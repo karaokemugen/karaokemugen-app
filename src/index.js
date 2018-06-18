386605af53ddd0a705498ff8ea2614fd7652d67f
Guillaume Lebigot <axel@teri-chan.net>
2018-06-18 13:54:23 +0200

    Clean up app/temp on startup. Closes #321

--- src/index.js
+++ src/index.js
@@ -145,6 +145,7 @@ async function checkPaths(config) {
 			);
 		}
 	}	
+	if (await asyncExists(resolve(appPath, config.PathTemp))) await asyncRemove(resolve(appPath, config.PathTemp));
 	let checks = [];
 	config.PathKaras.split('|').forEach(dir => checks.push(asyncCheckOrMkdir(appPath, dir)));
 	config.PathSubs.split('|').forEach(dir => checks.push(asyncCheckOrMkdir(appPath, dir)));
