251cd02e6890c78e98e4d6e65f385d978d1535ab
Axel Terizaki <axel@teri-chan.net>
2018-07-28 14:06:15 +0200

    Fixed series form when updating a series

--- react_client/src/pages/Series/SeriesForm.js
+++ react_client/src/pages/Series/SeriesForm.js
@@ -21,9 +21,20 @@ class SerieForm extends Component {
 				this.state[`lang_${lang}`] = this.props.serie.i18n[lang];
 			});
 		}
+		console.log(this.state);
+		console.log(this.props.serie);
 	}
 
 	componentDidMount() {
+		// For some stupid reason the list of languages won't be filled up, even with initialValue.
+		// So we're filling the form here.
+		console.log('Fields');
+		console.log(this.props.form.getFieldsValue());
+		for (const lang of this.state.i18n) {
+			const obj = {};
+			obj[`lang_${lang}`] = this.props.serie.i18n[lang];
+			this.props.form.setFieldsValue(obj);
+		}
 		this.props.form.validateFields();
 	}
 
@@ -141,6 +152,7 @@ class SerieForm extends Component {
 
 									<Input
 										placeholder='Name in that language'
+										defaultValue={this.state[`lang_${langKey}`]}
 										label={langKey}
 									/></Col>
 							)}
