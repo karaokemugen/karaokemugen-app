01903331ec0c18f6f17253c7d7fd44934a50de28
Bibah <thebibah14@gmail.com>
2018-07-31 00:00:50 +0200

    Add smart shuffle on playlist (except current)

--- src/_services/playlist.js
+++ src/_services/playlist.js
@@ -959,23 +959,108 @@ export async function shufflePlaylist(playlist_id, smartShuffleBoolean) {
 
 		}
 
-		var userShuffleBoolean = false;
-		//	if(smartShuffleBoolean){
-		let userTest = 1;
-		let userTestArray = [playlist[0].pseudo_add];
-		for(const playlistItem of playlist){
-			if(!userTestArray.includes(playlistItem.pseudo_add)){
-				userTestArray.push(playlistItem.pseudo_add);
-				userTest++;
+		let smartShuffleBoolean = true; // The boolean who determine if we will use the smart shuffle
+		let userShuffleBoolean = false; // The boolean to add a shuffle condition if the number of user is high enough
+
+		if (smartShuffleBoolean) {
+			let verificator = 0;
+			if (playlist.length - 6 > 0) {      // We do nothing if the playlist length is too low
+
+
+				let userTest = 1;
+				let userTestArray = [playlist[0].pseudo_add];
+
+				for (const playlistItem of playlist) {
+					if (!userTestArray.includes(playlistItem.pseudo_add)) {
+						userTestArray.push(playlistItem.pseudo_add);
+						userTest++;
+					}
+				}
+
+				if (userTest > 5) {
+					userShuffleBoolean = true;
+				}
+
+				let user_iterator = 0;
+
+				if (userShuffleBoolean) {
+					while (playlist.length - user_iterator > 0) {
+
+
+						if ((playlist.length - user_iterator) > 6) {
+							let playlist_temp = playlist.slice(user_iterator, user_iterator + 6);
+							for (let i = 0; i < 5; i++) {
+								if (playlist_temp[i].pseudo_add === playlist_temp[i + 1].pseudo_add) {
+									if(playlist[i + 4 + user_iterator]){
+										let a = playlist_temp[i + 1];
+										playlist[i + 1 + user_iterator] = playlist[i + 4 + user_iterator];
+										playlist[i + 4 + user_iterator] = a;
+									}else{
+										let a = playlist_temp[i + 1];
+										playlist[i + 1 + user_iterator] = playlist[i - 5 + user_iterator];
+										playlist[i - 5 + user_iterator] = a;
+
+									}
+
+								}
+
+							}
+
+						}
+						user_iterator += 5;
+					}
+
+					let playlist_temp = playlist.slice(user_iterator - 1, playlist.length);
+
+					for (let i = user_iterator; i < playlist_temp.length - 1; i++) {
+
+						if (playlist_temp[i].pseudo_add === playlist_temp[i + 1].pseudo_add) verificator = i;
+
+					}
+
+					if (verificator !== 0) {
+
+						let a = playlist_temp[verificator + 1];
+						playlist[verificator + 1 + user_iterator] = playlist[2];
+						playlist[2] = a;
+
+					}
+
+				}
+
+				let duration_iterator = 0;
+
+				while (playlist.length - duration_iterator > 0) {
+
+					if (playlist.length - duration_iterator > 6) {
+						let playlist_temp = playlist.slice(duration_iterator, duration_iterator + 6);
+
+						for (let i = 0; i < 4; i++) {
+							if (playlist_temp[i].duration > 150 && playlist_temp[i + 1].duration > 150) {
+								if (playlist[i + 4 + duration_iterator]) {
+									let a = playlist_temp[i + 1];
+									playlist[i + 1 + duration_iterator] = playlist[i + 4 + duration_iterator];
+									playlist[i + 4 + duration_iterator] = a;
+								} else {
+
+									let a = playlist_temp[i + 1];
+									playlist[i + 1 + duration_iterator] = playlist[i - 5 + duration_iterator];
+									playlist[i - 5 + duration_iterator] = a;
+
+								}
+
+							}
+						}
+
+					}
+					duration_iterator += 6;
+				}
+
 			}
+
 		}
-		if(userTest > 5) {
-			userShuffleBoolean = true;
-		}
-		console.log(userTest);
-		console.log(userTestArray);
-		console.log(userShuffleBoolean);
-		//	}
+
+
 
 		if (!pl.flag_current) {
 			playlist = shuffle(playlist);
