f2ec372c4f84b47f89e2dab244334058f7361135
Jaerdoster <poppen.gilles@gmail.com>
2018-08-20 16:57:49 +0200

    Fix on getAllKaras
    A tester - GOTTA GO FAST

--- src/_common/db/kara.js
+++ src/_common/db/kara.js
@@ -64,14 +64,8 @@ export const getAllKaras = (filterClauses, lang, orderClauses, typeClauses) => `
       							ak.title AS title,
 								ak.NORM_title AS NORM_title,
       							ak.songorder AS songorder,
-      							COALESCE(
-									  (SELECT sl.name FROM serie_lang sl, kara_serie ks WHERE sl.fk_id_serie = ks.fk_id_serie AND ks.fk_id_kara = kara_id AND sl.lang = ${lang.main}),
-									  (SELECT sl.name FROM serie_lang sl, kara_serie ks WHERE sl.fk_id_serie = ks.fk_id_serie AND ks.fk_id_kara = kara_id AND sl.lang = ${lang.fallback}),
-									  ak.serie) AS serie,
-								COALESCE(
-									  (SELECT sl.NORM_name FROM serie_lang sl, kara_serie ks WHERE sl.fk_id_serie = ks.fk_id_serie AND ks.fk_id_kara = kara_id AND sl.lang = ${lang.main}),
-									  (SELECT sl.NORM_name FROM serie_lang sl, kara_serie ks WHERE sl.fk_id_serie = ks.fk_id_serie AND ks.fk_id_kara = kara_id AND sl.lang = ${lang.fallback}),
-									  ak.NORM_serie) AS NORM_serie,
+      							COALESCE(sl_main.name, sl_fall.name, ak.serie) AS serie,
+								COALESCE(sl_main.NORM_name, sl_fall.NORM_name, ak.NORM_serie) AS NORM_serie,
       							ak.serie_altname AS serie_altname,
       							ak.NORM_serie_altname AS NORM_serie_altname,
 								ak.serie_i18n AS serie_i18n,
@@ -90,29 +84,31 @@ export const getAllKaras = (filterClauses, lang, orderClauses, typeClauses) => `
       							ak.author AS author,
       							ak.NORM_author AS NORM_author,
       							ak.misc AS misc,
-								(SELECT COUNT(pk_id_viewcount) AS viewcount FROM viewcount WHERE fk_id_kara = ak.kara_id) AS viewcount,
-								(SELECT COUNT(pk_id_request) AS request FROM request WHERE fk_id_kara = ak.kara_id) AS requested,
+								COUNT(vc.pk_id_viewcount) AS viewcount,
+								COUNT(rq.pk_id_request) AS requested,
 								ak.mediafile AS mediafile,
 								ak.karafile AS karafile,
       							ak.duration AS duration,
 								ak.gain AS gain,
-								(CASE WHEN $dejavu_time < (SELECT max(modified_at) FROM viewcount WHERE fk_id_kara = ak.kara_id)
+								(CASE WHEN $dejavu_time < max(vc.modified_at)
 	     							THEN 1
         							ELSE 0
       							END) AS flag_dejavu,
-								(SELECT max(vc.modified_at) FROM viewcount AS vc WHERE vc.fk_id_kara = ak.kara_id) AS lastplayed_at,
-								EXISTS(
-									SELECT 1 FROM playlist_content pc
-									JOIN playlist p ON pc.fk_id_playlist = p.pk_id_playlist
-									JOIN user u ON   u.pk_id_user = p.fk_id_user
-									WHERE pc.fk_id_kara = ak.kara_id
-										AND p.flag_favorites = 1
-										AND u.login = $username
-								) AS flag_favorites,
+								max(vc.modified_at) AS lastplayed_at,
+								(CASE WHEN cur_user_fav.fk_id_kara IS NULL THEN 0 ELSE 1 END) as flag_favorites,
 								ak.created_at AS created_at,
 								ak.modified_at as modified_at
 							FROM karasdb.all_karas AS ak
- 							WHERE ak.kara_id NOT IN (SELECT fk_id_kara FROM blacklist)
+							LEFT OUTER JOIN kara_serie AS ks_main ON ks_main.fk_id_kara = ak.kara_id
+							LEFT OUTER JOIN serie_lang AS sl_main ON sl_main.fk_id_serie = ks_main.fk_id_serie AND sl_main.lang = ${lang.main}
+							LEFT OUTER JOIN kara_serie AS ks_fall ON ks_fall.fk_id_kara = ak.kara_id
+							LEFT OUTER JOIN serie_lang AS sl_fall ON sl_fall.fk_id_serie = ks_fall.fk_id_serie AND sl_fall.lang = ${lang.fallback}
+							LEFT OUTER JOIN viewcount AS vc ON vc.fk_id_kara = ak.kara_id
+							LEFT OUTER JOIN request AS rq ON rq.fk_id_kara = ak.kara_id
+							LEFT OUTER JOIN user AS cur_user ON cur_user.login = $username
+							LEFT OUTER JOIN playlist AS cur_user_pl_fav ON cur_user.pk_id_user = cur_user_pl_fav.fk_id_user AND cur_user_pl_fav.flag_favorites = 1
+							LEFT OUTER JOIN playlist_content cur_user_fav ON cur_user_fav.fk_id_playlist = cur_user_pl_fav.fk_id_user AND cur_user_fav.fk_id_kara = pc.fk_id_kara
+							WHERE ak.kara_id NOT IN (SELECT fk_id_kara FROM blacklist)
 							 ${filterClauses.map(clause => 'AND (' + clause + ')').reduce((a, b) => (a + ' ' + b), '')}
 							 ${typeClauses}
 							 ORDER BY ${orderClauses} language, serie IS NULL, serie COLLATE NOCASE, songtype DESC, songorder, singer COLLATE NOCASE, title COLLATE NOCASE
