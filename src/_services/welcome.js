11bfdf1159fb0d90d384fbe6a3bd1686071611d9
Axel Terizaki <axel@teri-chan.net>
2018-11-05 21:18:23 +0100

    Fix initial admin setup due to new editUser security

--- src/_services/welcome.js
+++ src/_services/welcome.js
@@ -6,23 +6,26 @@ import opn from 'opn';
 function generateAdminPassword() {
 	const adminPassword = randomstring.generate(8);
 	editUser('admin',
-		{ 
+		{
 			password: adminPassword,
-			nickname: 'Dummy Plug System'
+			nickname: 'Dummy Plug System',
+			type: 1,
+			flag_admin: 1
 		},
 		null,
 		{
-			username: 'admin'
+			username: 'admin',
+			role: 'admin'
 		});
 	return adminPassword;
 }
 
-export async function welcomeToYoukousoKaraokeMugen(port) {	
+export async function welcomeToYoukousoKaraokeMugen(port) {
 	const conf = getConfig();
 	if (+conf.appFirstRun === 1) {
 		const adminPassword = generateAdminPassword();
 		if (!conf.isDemo && !conf.isTest) opn(`http://localhost:${port}/welcome?admpwd=${adminPassword}`);
-		console.log('\nAdmin password is : '+adminPassword+'\nPlease keep it in a safe place, it will not be displayed ever again.\nTo reset admin password, set appFirstRun to 1 in config.ini\n');			
+		console.log('\nAdmin password is : '+adminPassword+'\nPlease keep it in a safe place, it will not be displayed ever again.\nTo reset admin password, set appFirstRun to 1 in config.ini\n');
 	} else {
 		if (!conf.optNoBrowser && !conf.isDemo && !conf.isTest) {
 			opn(`http://localhost:${port}/welcome`);
