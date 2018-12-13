085bc843a35eab7efbc6f2e7a26a00e6cc5f9dab
Spokeek <spokeek@gmail.com>
2018-12-13 13:00:18 +0100

    done with ass file refacto

--- src/_common/utils/ass.js
+++ src/_common/utils/ass.js
@@ -1,9 +1,8 @@
 import {parse as assParser} from 'ass-compiler';
 
 export function ASSToLyrics(ass) {
-	let lyrics = [];
-	let script = assParser(ass);
-	script.events.dialogue.forEach( dialogue => lyrics.push(dialogue.Text.combined));
+	const script = assParser(ass);
+	const lyrics = script.events.dialogue.map((dialogue) => dialogue.Text.combined);
 	return lyrics;
 }
 
