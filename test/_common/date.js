5cf4f026d4fa098e0347c4f4445f7072c847ba4a
Spokeek <spokeek@gmail.com>
2018-12-13 23:53:48 +0100

    added test for timeToSeconds

--- test/_common/date.js
+++ test/_common/date.js
@@ -14,8 +14,8 @@ const testDate = () => it('Testing date()', () => {
 		.and.to.match(testRegex);
 	
 	const regexRes = testRegex.exec(returnedDate);
-	expect((regexRes || []).length)
-		.to.eql(4,`Impossible to match the 'day-month-year' pattern of ${testRegex} on '${returnedDate}' (wrong pattern matching).`);
+	expect(regexRes || [])
+		.to.have.lengthOf(4,`Impossible to match the 'day-month-year' pattern of ${testRegex} on '${returnedDate}' (wrong pattern matching)`);
 
 	const attributes = regexRes
 		.filter((_, i) => i > 0);
@@ -41,8 +41,8 @@ const testTime = () => it('Testing time()', () => {
 		.and.to.match(testRegex);
 	
 	const regexRes = testRegex.exec(returnedTime);
-	expect((regexRes || []).length)
-		.to.eql(4,`Impossible to match the 'hours-minutes-seconds' pattern of ${testRegex} on '${testRegex}' (wrong pattern matching).`);
+	expect(regexRes || [])
+		.to.have.lengthOf(4,`Impossible to match the 'hours-minutes-seconds' pattern of ${testRegex} on '${testRegex}' (wrong pattern matching)`);
 
 	const attributes = regexRes
 		.filter((_, i) => i > 0);
@@ -55,7 +55,23 @@ const testTime = () => it('Testing time()', () => {
 		.forEach((testData) => expect(testData[0], `The '${testData[1]}' value is not valid`).to.be.within(0, testData[2]));
 });
 
+const testTimeToSeconds = () => it('Testing time()', () => {
+
+	const testDataToThrow = [undefined, null, 0, -1, 1, 'blablabla', '00:00', 'AZ:AZ:AZ', '01:ZZ:03', '132:34:65', '-1:52:65'];
+	testDataToThrow
+		.forEach((testData) => expect(() => dateFunctions.timeToSeconds(testData), `Error with the parameter '${testData}'`).to.throw());
+	
+	const testDataToWork = [['23:32:09', 84729], ['12:34:65', 45305], ['00:34:65', 2105], ['00:52:65', 3185], ['0:52:65', 3185], ['0:0:0', 0]];
+	testDataToWork
+		.forEach((testData) => expect(() => dateFunctions.timeToSeconds(testData[0]))
+			.to.not.throw()
+			.and.to.be.a('number')
+			.and.equal(testData[1])
+		);
+});
+
 export default () => {
 	testDate();
 	testTime();
+	testTimeToSeconds();
 };
