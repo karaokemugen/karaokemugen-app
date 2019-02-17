1ee54aeec40b07b1a4b0782b9a416deb43c58654
Aeden <florentius.b@gmail.com>
2019-02-17 17:39:00 +0100

    fix online auth

--- src/_controllers/middlewares/auth.js
+++ src/_controllers/middlewares/auth.js
@@ -26,6 +26,7 @@ export async function checkValidUser(token, onlineToken) {
 				// Remote token does not exist, we're going to verify it and add it if it does work
 				try {
 					// Firing this first to avoid multiple triggers, will get canceled if auth is not OK.
+					onlineToken = remoteToken.token;
 					upsertRemoteToken(token.username, onlineToken);
 					if (await remoteCheckAuth(token.username.split('@')[1], onlineToken)){
 						fetchAndAddFavorites(token.username.split('@')[1], onlineToken, token.username, token.username);
