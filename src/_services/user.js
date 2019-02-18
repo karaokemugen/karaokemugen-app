cfcf2a10335db2265df440d66a3e098c176a90d4
Guillaume Lebigot <axel@teri-chan.net>
2019-02-18 10:27:04 +0100

    Replaced user avatar file detection by a regexp

--- src/_services/user.js
+++ src/_services/user.js
@@ -7,7 +7,7 @@ import {now} from '../_utils/date';
 import {resolve} from 'path';
 import logger from 'winston';
 import uuidV4 from 'uuid/v4';
-import {defaultGuestNames} from '../_services/constants';
+import {imageFileRegexp, defaultGuestNames} from '../_services/constants';
 import randomstring from 'randomstring';
 import {on} from '../_utils/pubsub';
 import {getSongCountForUser, getSongTimeSpentForUser} from '../_dao/kara';
@@ -266,11 +266,7 @@ async function replaceAvatar(oldImageFile,avatar) {
 	try {
 		const conf = getConfig();
 		const fileType = await detectFileType(avatar.path);
-		if (fileType !== 'jpg' &&
-				fileType !== 'gif' &&
-				fileType !== 'png') {
-			throw 'Wrong avatar file type';
-		}
+		if (!new RegExp(imageFileRegexp).test(fileType)) throw 'Wrong avatar file type';
 		// Construct the name of the new avatar file with its ID and filetype.
 		const newAvatarFile = `${uuidV4()}.${fileType}`;
 		const newAvatarPath = resolve(conf.PathAvatars,newAvatarFile);
