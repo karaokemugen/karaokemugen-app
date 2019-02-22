7de19fea412fe9784696a9a0848f52d208b3b0e9
Z♪assan <coc5@free.fr>
2019-02-22 20:49:17 +0100

    Fixed bug where we used settings before we had them

--- src/_webapp/ressources/js/karaokemugen.js
+++ src/_webapp/ressources/js/karaokemugen.js
@@ -2708,13 +2708,14 @@ var settingsNotUpdated;
 		} else {
 			$('.profileConvert').show();
 		}
-
-		if(!data.OnlineUsers && (Object.keys(settings).length == 0 || settings.OnlineUsers) && logInfos.username.includes('@')) {
-			setTimeout(function() {
-				displayMessage('warning',i18n.__('LOG_OFFLINE.TITLE') + '<br/>', i18n.__('LOG_OFFLINE.MESSAGE'), 8000);
-			}, 500)
-		}
-	}
+		settingsUpdating.done(function () {
+			if(!data.OnlineUsers && (Object.keys(settings).length == 0 || settings.OnlineUsers) && logInfos.username.includes('@')) {
+				setTimeout(function() {
+					displayMessage('warning',i18n.__('LOG_OFFLINE.TITLE') + '<br/>', i18n.__('LOG_OFFLINE.MESSAGE'), 8000);
+				}, 500);
+			}
+		});
+	};
 
 	/* socket part */
 
@@ -2822,8 +2823,7 @@ var settingsNotUpdated;
 		});
 
 		socket.on('favoritesUpdated', function(){
-            var side = sideOfPlaylist(-5);
-            console.log('rest');
+			var side = sideOfPlaylist(-5);
 			if(side && $('#playlist' + side + '.lyricsKara:visible').length == 0) {
 				playlistContentUpdating = fillPlaylist(side);
 			}
