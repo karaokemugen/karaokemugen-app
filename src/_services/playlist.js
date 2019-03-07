221192b8430dfb4e5cf7ee1f5c9b2d207a021992
Guillaume Lebigot <axel@teri-chan.net>
2019-03-07 16:15:12 +0100

    Fix fix previous commit teehee.

--- src/_services/playlist.js
+++ src/_services/playlist.js
@@ -769,27 +769,27 @@ export async function importPlaylist(playlist, username, playlist_id) {
 		if (playlist.Header.version > 4) throw `Cannot import this version (${playlist.Header.version})`;
 		if (!playlist.PlaylistContents) throw 'No PlaylistContents section';
 		if (!playlist.PlaylistInformation) throw 'No PlaylistInformation section';
-		if (!Date.parse(playlist.PlaylistInformation.created_at)) throw 'Creation time is not valid';
-		if (!Date.parse(playlist.PlaylistInformation.modified_at)) throw 'Modification time is not valid';
+		if (isNaN(playlist.PlaylistInformation.created_at) && !Date.parse(playlist.PlaylistInformation.created_at)) throw 'Creation time is not valid';
+		if (isNaN(playlist.PlaylistInformation.modified_at) && !Date.parse(playlist.PlaylistInformation.modified_at)) throw 'Modification time is not valid';
 		if (playlist.PlaylistInformation.flag_visible !== true &&
 		playlist.PlaylistInformation.flag_visible !== false) throw 'Visible flag must be boolean';
 		if (!playlist.PlaylistInformation.name) throw 'Playlist name must not be empty';
 		// Convert unix timestamps to JS Dates
-		if (!isNaN(playlist.PlaylistInformation.created_at)) playlist.PlaylistInformation.created_at = new Date(playlist.PlaylistInformation.created_at * 1000);
-		if (!isNaN(playlist.PlaylistInformation.modified_at)) playlist.PlaylistInformation.modified_at = new Date(playlist.PlaylistInformation.modified_at * 1000);
+		if (!isNaN(playlist.PlaylistInformation.created_at)) playlist.PlaylistInformation.created_at = new Date(+playlist.PlaylistInformation.created_at * 1000);
+		if (!isNaN(playlist.PlaylistInformation.modified_at)) playlist.PlaylistInformation.modified_at = new Date(+playlist.PlaylistInformation.modified_at * 1000);
 		let flag_playingDetected = false;
 		if (playlist.PlaylistContents) {
 			for (const index in playlist.PlaylistContents) {
 				const kara = playlist.PlaylistContents[index];
 				if (!(new RegExp(uuidRegexp).test(kara.kid))) throw 'KID is not a valid UUID!';
-				if (!Date.parse(kara.created_at)) throw 'Karaoke added time is not a number';
+				if (isNaN(kara.created_at) && !Date.parse(kara.created_at)) throw 'Karaoke added time is not a valid date';
 				if (kara.flag_playing === true) {
 					if (flag_playingDetected) throw 'Playlist contains more than one currently playing marker';
 					flag_playingDetected = true;
 					playingKara.kid = kara.kid;
 					playingKara.user = kara.username;
 				}
-				if (!isNaN(kara.created_at)) playlist.PlaylistContents[index].created_at = new Date(kara.created_at * 1000);
+				if (!isNaN(kara.created_at)) playlist.PlaylistContents[index].created_at = new Date(+kara.created_at * 1000);
 				if (isNaN(kara.pos)) throw 'Position must be a number';
 				if (!kara.nickname) throw 'All karaokes must have a nickname associated with them';
 				const user = await findUserByName(kara.username);
