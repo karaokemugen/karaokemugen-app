e68eb39690ba3ee0d3a2cb86459554d5ad32b06d
Guillaume Lebigot <axel@teri-chan.net>
2019-03-19 12:24:54 +0100

    Fix migrations (type was not a good idea) (#339)

--- migrations/sqls/20190226223300-createDownloadTable-up.sql
+++ migrations/sqls/20190226223300-createDownloadTable-up.sql
@@ -1,10 +1,9 @@
-CREATE TYPE dlStatus AS ENUM ('DL_PLANNED', 'DL_RUNNING', 'DL_FAILED', 'DL_DONE');
 CREATE TABLE download (
 	pk_uuid    uuid NOT NULL PRIMARY KEY,
 	name	character varying NOT NULL,
 	urls	jsonb NOT NULL,
 	size	integer NOT NULL DEFAULT 0,
-	status	dlStatus NOT NULL DEFAULT 'DL_PLANNED',
+	status	character VARYING NOT NULL DEFAULT 'DL_PLANNED',
 	started_at timestamp NOT NULL DEFAULT now()
 );
 
