c9cf318eb1fe0cec757c9fd7b345bc4535ce3ed4
Guillaume Lebigot <axel@teri-chan.net>
2019-03-22 14:32:35 +0100

    Do not open a browser window even if appfirstrun when nobrowser is specified

--- src/_services/welcome.js
+++ src/_services/welcome.js
@@ -22,7 +22,7 @@ export async function welcomeToYoukousoKaraokeMugen(port) {
 	const conf = getConfig();
 	if (+conf.appFirstRun === 1) {
 		const adminPassword = generateAdminPassword();
-		if (!conf.isDemo && !conf.isTest) opn(`http://localhost:${port}/welcome?admpwd=${adminPassword}`);
+		if (!conf.optNoBrowser && !conf.isDemo && !conf.isTest) opn(`http://localhost:${port}/welcome?admpwd=${adminPassword}`);
 		console.log(`\nAdmin password is : ${adminPassword}\nPlease keep it in a safe place, it will not be displayed ever again.\nTo reset admin password, set appFirstRun to 1 in config.ini\n`);
 	} else {
 		if (!conf.optNoBrowser && !conf.isDemo && !conf.isTest) {
