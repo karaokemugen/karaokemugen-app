d9a220f2cf7403d3327a6c5258db20b6ea940f74
Guillaume Lebigot <axel@teri-chan.net>
2019-03-27 09:31:01 +0100

    I'm a baka.

--- src/_services/welcome.js
+++ src/_services/welcome.js
@@ -1,7 +1,7 @@
 import {getConfig} from '../_utils/config';
 import {editUser} from '../_services/user';
 import randomstring from 'randomstring';
-import opn from 'opn';
+import open from 'open';
 
 function generateAdminPassword() {
 	// Resets admin's password when appFirstRun is set to 1.
@@ -22,7 +22,7 @@ export async function welcomeToYoukousoKaraokeMugen(port) {
 	const conf = getConfig();
 	if (+conf.appFirstRun === 1) {
 		const adminPassword = generateAdminPassword();
-		if (!conf.isDemo && !conf.isTest) opn(`http://localhost:${port}/welcome?admpwd=${adminPassword}`);
+		if (!conf.isDemo && !conf.isTest) open(`http://localhost:${port}/welcome?admpwd=${adminPassword}`);
 		console.log(`\nAdmin password is : ${adminPassword}\nPlease keep it in a safe place, it will not be displayed ever again.\nTo reset admin password, set appFirstRun to 1 in config.ini\n`);
 	} else {
 		if (!conf.optNoBrowser && !conf.isDemo && !conf.isTest) {
