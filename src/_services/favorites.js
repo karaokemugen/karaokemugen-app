8e1fb07d843ecb32ac3562cc6f68a02320ce212b
Guillaume Lebigot <axel@teri-chan.net>
2019-04-17 14:32:01 +0200

    Fix favorites getting when sending them to remote

--- src/_services/favorites.js
+++ src/_services/favorites.js
@@ -62,7 +62,7 @@ export async function convertToRemoteFavorites(username) {
 	// This is called when converting a local account to a remote one
 	// We thus know no favorites exist remotely.
 	if (!getConfig().Online.Users) return true;
-	const favorites = await getFavorites({username: username});
+	const favorites = await getFavorites(username);
 	const addFavorites = [];
 	if (favorites.content.length > 0) {
 		for (const favorite of favorites) {
