a83f102426448d50292be24f2e934c525ba069c9
Aeden <florentius.b@gmail.com>
2019-06-21 20:26:48 +0200

    add call to public/karas/suggest #422

--- frontend/ressources/js/karaokemugen.js
+++ frontend/ressources/js/karaokemugen.js
@@ -350,20 +350,20 @@ var settingsNotUpdated;
 
 
 		$('.playlist-main').on('click','li.karaSuggestion', function() {
-            var search = $('#searchPlaylist1').val();
-            settingsUpdating.done( function() {
-                displayModal('prompt', i18n.__('KARA_SUGGESTION_NAME'), '', function(text) {
-                    var adress = 'mailto:' + (settings && settings.App ? settings.App.karaSuggestionMail : 'err');
-                    var subject = i18n.__('KARA_SUGGESTION_SUBJECT') + text;
-                    var body = i18n.__('KARA_SUGGESTION_BODY') + '%0D%0A %0D%0A ' + logInfos.username;
-                    setTimeout(function() {
-                        displayMessage('info', i18n.__('KARA_SUGGESTION_INFO'),
-                            i18n.__('KARA_SUGGESTION_LINK', 'https://lab.shelter.moe/karaokemugen/karaokebase/issues/', 'console'), '30000');
-                    }, 200);
-    
-                    window.open(adress + '?' + 'body=' + body + '&subject=' + subject,'_blank');
-                }, search);
-            });
+        	var search = $('#searchPlaylist1').val();
+			displayModal('prompt', i18n.__('KARA_SUGGESTION_NAME'), '', function(text) {
+				$.ajax({
+					type: 'POST',
+					url: 'public/karas/suggest',
+					data: {karaName : text}
+				})
+					.done(function (response) {
+						setTimeout(function() {
+							displayMessage('info', i18n.__('KARA_SUGGESTION_INFO'),
+								i18n.__('KARA_SUGGESTION_LINK', response.issueURL, 'console'), '30000');
+						}, 200);
+					})
+			}, search);
 		
 		});
 
@@ -1501,7 +1501,7 @@ var settingsNotUpdated;
 
 
 					/* adding artificial last line */
-					if(idPlaylist === -1 && count === response.infos.from + data.length) {
+					if(settings.Gitlab.Enabled && idPlaylist === -1 && count === response.infos.from + data.length) {
 						// count++;
 						htmlContent +=	karaSuggestionHtml;
 					}
