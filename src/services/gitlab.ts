a50321cea30cfbf068099dbc523f3fdc7955f03f
Aeden <florentius.b@gmail.com>
2019-06-21 20:31:04 +0200

    fix PostSuggestionToKaraBase

--- src/services/gitlab.ts
+++ src/services/gitlab.ts
@@ -5,9 +5,9 @@ import { findUserByName } from './user';
 
 export async function PostSuggestionToKaraBase(karaName: string, username: string): Promise<string> {
 	const conf = getConfig();
-	let title = conf.Gitlab.IssueTemplate.Suggestion.Title || 'New kara suggestion: $kara';
+	let title = conf.Gitlab.IssueTemplate.Suggestion.Title ? conf.Gitlab.IssueTemplate.Suggestion.Title : 'New kara suggestion: $kara';
 	title = title.replace('$kara', karaName);
-	let desc = conf.Gitlab.IssueTemplate.Suggestion.Description || '';
+	let desc = conf.Gitlab.IssueTemplate.Suggestion.Description ? conf.Gitlab.IssueTemplate.Suggestion.Description : '';
 	const user = await findUserByName(username);
 	desc = desc.replace('$username', user.nickname)
 	try {
