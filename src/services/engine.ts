2e555c5cc53343dec2efb56de1a3a8d008e5b5de
Axel Terizaki <axel@teri-chan.net>
2019-07-20 11:10:09 +0200

    Fix last commit

--- src/services/engine.ts
+++ src/services/engine.ts
@@ -22,7 +22,6 @@ import {validateV3} from '../lib/dao/karafile';
 import { DBStats } from '../types/database/database';
 import { createVideoPreviews } from '../lib/utils/previews';
 import { getAllKaras } from './kara';
-import { selectTagByNameAndType } from '../dao/tag';
 
 export async function initEngine() {
 	profile('Init');
