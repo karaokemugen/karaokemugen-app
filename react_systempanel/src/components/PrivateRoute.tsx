9ca8c2a85f7a53c23bce860b75e0ce383aff5587
Mathieu Delestre <ldbglobe@gmail.com>
2019-07-22 21:48:19 +0200

    fix "login>home" redirection on page reload

--- react_systempanel/src/components/PrivateRoute.tsx
+++ react_systempanel/src/components/PrivateRoute.tsx
@@ -2,25 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { RouteProps } from 'react-router';
 import { Redirect, Route } from 'react-router-dom';
+import { AuthentifactionApi } from '../api/authentication.api';
 
 interface PrivateRouteProps extends RouteProps {
   isAuthenticated: boolean;
 }
 
-class PrivateRoute extends Component<PrivateRouteProps, {}> {
+interface PrivateRouteState {
+  logged: boolean; // Make sure to display the form only if you don't have already credential (F5 issue)
+}
+
+class PrivateRoute extends Component<PrivateRouteProps, PrivateRouteState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged: null
+    };
+  }
+
+  componentDidMount() {
+    AuthentifactionApi.isAuthenticated()
+      .then(() => this.setState({logged:true}))
+      .catch(() => this.setState({logged:false}));
+  }
 
   render() {
+    if(this.state.logged===null)
+      return <div>Loading</div>
+
     const LoggedRoute = <Route {...this.props}></Route>
     const NotLoggedRoute = <Redirect to='/system/login'/>
-    const NextRoute = this.props.isAuthenticated ? LoggedRoute : NotLoggedRoute;
+    const NextRoute = this.state.logged ? LoggedRoute : NotLoggedRoute;
     return (
       NextRoute
     );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => {
+  console.log(state)
+  return ({
   isAuthenticated: state.auth.isAuthenticated
-});
+})};
 
 export default connect(mapStateToProps)(PrivateRoute);
