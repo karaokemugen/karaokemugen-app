bee7fea30a9c48b7813e90686d1b68faa551ae01
Guillaume Lebigot <axel@teri-chan.net>
2019-07-30 10:21:05 +0200

    Simplified gitlab calls

--- src/services/gitlab.ts
+++ src/services/gitlab.ts
@@ -4,14 +4,18 @@ import logger from '../lib/utils/logger';
 import { findUserByName } from './user';
 
 export async function postSuggestionToKaraBase(karaName: string, username: string): Promise<string> {
-	const conf = getConfig();
-	let title = conf.Gitlab.IssueTemplate && conf.Gitlab.IssueTemplate.Suggestion && conf.Gitlab.IssueTemplate.Suggestion.Title ? conf.Gitlab.IssueTemplate.Suggestion.Title : 'New kara suggestion: $kara';
+	const conf = getConfig().Gitlab.IssueTemplate;
+	let title = conf && conf.Suggestion && conf.Suggestion.Title
+		? conf.Suggestion.Title
+		: 'New kara suggestion: $kara';
 	title = title.replace('$kara', karaName);
-	let desc = conf.Gitlab.IssueTemplate && conf.Gitlab.IssueTemplate.Suggestion && conf.Gitlab.IssueTemplate.Suggestion.Description ? conf.Gitlab.IssueTemplate.Suggestion.Description : '';
+	let desc = conf && conf.Suggestion && conf.Suggestion.Description
+		? conf.Suggestion.Description
+		: '';
 	const user = await findUserByName(username);
 	desc = desc.replace('$username', user.nickname)
 	try {
-		return await gitlabPostNewIssue(title, desc, conf.Gitlab.IssueTemplate.Suggestion.Labels);
+		return await gitlabPostNewIssue(title, desc, conf.Suggestion.Labels);
 	} catch(err) {
 		logger.error(`[Gitlab] Call to Gitlab API failed : ${err}`);
 	}
