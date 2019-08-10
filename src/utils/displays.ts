a12a072963cedfbab484fdd41ec44d60b4bc8005
Aeden <florentius.b@gmail.com>
2019-08-10 10:53:04 +0200

    fix displays regexp

--- src/utils/displays.ts
+++ src/utils/displays.ts
@@ -6,10 +6,8 @@ export async function getDisplays() {
     //Get list of monitors to allow users to select one for the player
     const data = await graphics();
     logger.debug('[Webapp] Displays detected : ' + JSON.stringify(data.displays));
-    return data.displays
-		.filter(d => d.resolutionx > 0)
-		.map(d => {
-		  d.model = d.model.replace('�', 'e');
+    return data.displays.filter(d => d.resolutionx > 0).map(d => {
+		  d.model = d.model.replace(/�/g, 'e');
 	  	  return d;
 		});
   }
