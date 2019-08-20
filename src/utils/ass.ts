398d0fa5f2e9e455bd1f0387712870ba27bbba40
Guillaume Lebigot <axel@teri-chan.net>
2019-08-20 09:52:35 +0200

    ASS compiler has types now, yay!

--- src/utils/ass.ts
+++ src/utils/ass.ts
@@ -3,6 +3,6 @@ import {parse as assParser} from 'ass-compiler';
 /** Parse ASS data and return lyrics */
 export function ASSToLyrics(ass: string): string[] {
 	const script = assParser(ass);
-	return script.events.dialogue.map((dialogue: any) => dialogue.Text.combined);
+	return script.events.dialogue.map(dialogue => dialogue.Text.combined);
 }
 
