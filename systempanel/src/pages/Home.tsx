4a6b147e43e233540737a1cae64cabbb5ecc34a0
Guillaume Lebigot <axel@teri-chan.net>
2019-10-30 10:38:19 +0100

    Detail welcome screen a bit

--- systempanel/src/pages/Home.tsx
+++ systempanel/src/pages/Home.tsx
@@ -9,9 +9,29 @@ interface HomeState {}
 class Home extends Component<HomeProps, HomeState> {
 	render() {
 		return (
-			<Layout.Content style={{ padding: '25px 50px', textAlign: 'center' }}>
-				<h1>Welcome to Karaoke Mugen's control panel</h1>
-				<p>You can manage users, create .kara files, update your base or regenerate the database from here.</p>
+			<Layout.Content style={{ padding: '25px 50px', textAlign: 'left' }}>
+				<h1>Welcome to Karaoke Mugen's system panel</h1>
+				<p>Here are the different menus you can use :</p>
+				<ul>
+					<li>
+						<b>System : </b> See logs, change configuration options, and manage karaoke session datata
+					</li>
+					<li>
+						<b>Karas : </b> View, edit, download, and create karaoke data files. You can also edit the download blacklist and view most requested, most played, and karaoke history
+					</li>
+					<li>
+						<b>Series : </b> Manage series from here
+					</li>
+					<li>
+						<b>Tags : </b> Manage tags (singers, songwriters, creators, song types, etc.)
+					</li>
+					<li>
+						<b>Database : </b> Backup/restore your database, trigger a manual generation or update your media files (if you are using a karaokebase under git)
+					</li>
+					<li>
+						<b>Users : </b> Create/edit/view users, change passwords, make new guests,
+					</li>
+				</ul>
 			</Layout.Content>
 		);
 	}
