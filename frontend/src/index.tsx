c3a23af33be97a79d951904c1b0fc8bf5f7eba90
Aeden <florentius.b@gmail.com>
2020-01-04 15:45:09 +0100

    fix frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import App from "./app";
 import ReactDOM from 'react-dom';
+import React from 'react';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
