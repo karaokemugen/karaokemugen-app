7a5b45faaa5ed48236ac311df3039fac458cfb46
Guillaume Lebigot <axel@teri-chan.net>
2020-01-07 11:39:50 +0100

    Cosmetic change to line separator

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ async function main() {
 	console.log(chalk.blue(logo));
 	console.log('Karaoke Player & Manager - http://karaokes.moe');
 	console.log(`Version ${chalk.bold.green(state.version.number)} (${chalk.bold.green(state.version.name)})`);
-	console.log('================================================================');
+	console.log('================================================================================');
 	await configureLogger(dataPath, !!argv.debug, true);
 	await initConfig(argv);
 	let config = getConfig();
