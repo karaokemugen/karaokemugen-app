f2fda7b5d4d77b79ab40ed9e3fdf7bc90146ebf1
Guillaume Lebigot <axel@teri-chan.net>
2020-02-09 10:30:26 +0100

    Updated deps + fix multer File type

--- src/services/user.ts
+++ src/services/user.ts
@@ -628,7 +628,6 @@ async function updateGuestAvatar(user: User) {
 			filename: null,
 			buffer: null,
 			size: null,
-			stream: null,
 		}, 'admin', {
 			renameUser: false,
 			editRemote: false
