c8fd5d5b9f5e1d381739479fabfe543a8ab753ad
Guillaume Lebigot <axel@teri-chan.net>
2020-02-26 14:11:09 +0100

    Use worker threads to avoid blocking main process when doing git updates/clones
    Let's welcome our worker threads overlords!

---
+++ src/utils/git.js
@@ -0,0 +1,95 @@
+// KM Imports
+const internet = require('internet-available');
+
+// Node modules
+const fs = require('fs');
+const {mkdirpSync, removeSync} = require('fs-extra');
+const { plugins, pull, clone}  = require('isomorphic-git');
+const { parentPort, workerData } = require('worker_threads');
+
+plugins.set('fs', fs);
+
+function logger(msg) {
+	parentPort.postMessage({
+		type: 'log',
+		data: msg
+	});
+}
+
+async function gitUpdate(o) {
+	try {
+		if (!fs.existsSync(o.gitDir) || !fs.existsSync(o.gitDir + '/.git')) {
+			logger(`[${o.type}] Downloading...`);
+			try {
+				await internet();
+			} catch (err) {
+				throw 'Internet not available';
+			}
+			// Git clone
+			try {
+				try {
+					mkdirpSync(o.gitDir);
+				} catch(err) {
+					// Non-fatal
+				}
+				await clone({
+					dir: o.gitDir,
+					url: o.gitURL
+				});
+				logger(`[${o.type}] Finished downloading`);
+				return o.localDirs;
+			} catch(err) {
+				throw err;
+			}
+		} else {
+			try {
+				await internet();
+			} catch (err) {
+				throw 'Internet not available';
+			}
+			logger(`[${o.type}] Updating...`);
+			try {
+				await pull({
+					dir: o.gitDir
+				});
+			} catch(err) {
+				//Pull failed, trying a clone
+				//Issue is reported here :
+				//https://github.com/isomorphic-git/isomorphic-git/issues/963
+				logger(`[${o.type}] Updating failed, trying wipe and reclone`);
+				try {
+					removeSync(o.gitDir);
+					return await gitUpdate({
+						gitDir: o.gitDir,
+						gitURL: o.gitURL,
+						type: o.type,
+						localDirs: o.localDirs,
+						logger: null
+					});
+				} catch(err) {
+					//Clone also failed, error might be elsewhere
+					throw 'git Error even after a clone';
+				}
+			} finally {
+				logger(`[${o.type}] Finished updating`);
+				return null;
+			}
+		}
+	} catch(err) {
+		throw Error(err);
+	}
+}
+
+gitUpdate(workerData.options)
+	.then(localDirs => {
+		parentPort.postMessage({
+			type: 'status-success',
+			data: localDirs
+		});
+	})
+	.catch(err => {
+		parentPort.postMessage({
+			type: 'status-failed',
+			data: err
+		});
+	});
