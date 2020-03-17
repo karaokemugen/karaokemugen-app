c3d203d25f13ca4e4ed2294a3433bf4aeb28cab5
Aeden <florentius.b@gmail.com>
2020-03-17 21:47:41 +0100

    Translate loading #607

--- systempanel/src/components/Loading.tsx
+++ systempanel/src/components/Loading.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import {Alert, Spin} from 'antd';
+import i18next from 'i18next';
 
 export default function Loading() {
 	return (
-		<Spin tip="Loading...">
+		<Spin tip={i18next.t('LOADING')}>
 			<Alert
 				message="Loading"
 				description="Please wait..."
