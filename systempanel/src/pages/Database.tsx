0c15c626b490a27e116dba757f1c0e65312fb485
Aeden <florentius.b@gmail.com>
2020-03-17 19:37:03 +0100

    Delete old progress bar in system panel

--- systempanel/src/pages/Database.tsx
+++ systempanel/src/pages/Database.tsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { Progress, Modal, Button, Layout, Row, Col } from 'antd';
-import openSocket from 'socket.io-client';
+import { Modal, Button, Layout, Row, Col } from 'antd';
 
 import { loading, infoMessage, errorMessage, warnMessage } from '../actions/navigation';
 import { ReduxMappedProps } from '../react-app-env';
@@ -12,25 +11,7 @@ interface DatabaseProps extends ReduxMappedProps { }
 
 interface DatabaseState {
 	updateModal: boolean,
-	renameModal: boolean,
-	generationProgress: {
-		text: string,
-		value: number,
-		total: number,
-		percentage: number,
-	},
-	downloadProgress: {
-		text: string,
-		value: number,
-		total: number,
-		percentage: number,
-	},
-	downloadBatchProgress: {
-		text: string,
-		value: number,
-		total: number,
-		percentage: number,
-	}
+	renameModal: boolean
 }
 
 class Database extends Component<DatabaseProps, DatabaseState> {
@@ -39,75 +20,9 @@ class Database extends Component<DatabaseProps, DatabaseState> {
 		super(props);
 		this.state = {
 			updateModal: false,
-			renameModal: false,
-			generationProgress: {
-				text: undefined,
-				value: 0,
-				total: 100,
-				percentage: 0, // Added percentage for reference, rather than computing again with Math.floor
-			},
-			downloadProgress: {
-				text: undefined,
-				value: 0,
-				total: 100,
-				percentage: 0
-			},
-			downloadBatchProgress: {
-				text: undefined,
-				value: 0,
-				total: 100,
-				percentage: 0
-			}
+			renameModal: false
 		};
 	}
-
-	componentDidMount() {
-		let url = window.location.port === '3000' ? `${window.location.protocol}//${window.location.hostname}:1337` : window.location.origin;
-		const socket = openSocket(url);
-		socket.on('generationProgress', (data) => {
-			if (!data.text) data.text = this.state.generationProgress.text;
-			const percentage = Math.floor((data.value / data.total) * 100);
-			if (data.text !== this.state.generationProgress.text || percentage !== this.state.generationProgress.percentage) {
-				this.setState({
-					generationProgress: {
-						text: data.text,
-						value: data.value,
-						total: data.total,
-						percentage
-					}
-				});
-			}
-		});
-		socket.on('downloadProgress', (data) => {
-			if (!data.text) data.text = this.state.downloadProgress.text;
-			const percentage = Math.floor((data.value / data.total) * 100);
-			if (data.text !== this.state.downloadProgress.text || percentage !== this.state.downloadProgress.percentage) {
-				this.setState({
-					downloadProgress: {
-						text: data.text,
-						value: data.value,
-						total: data.total,
-						percentage
-					}
-				});
-			}
-		});
-		socket.on('downloadBatchProgress', (data) => {
-			if (!data.text) data.text = this.state.downloadBatchProgress.text;
-			const percentage = Math.floor((data.value / data.total) * 100);
-			if (data.text !== this.state.downloadBatchProgress.text || percentage !== this.state.downloadBatchProgress.percentage) {
-				this.setState({
-					downloadBatchProgress: {
-						text: data.text,
-						value: data.value,
-						total: data.total,
-						percentage
-					}
-				});
-			}
-		});
-	}
-
 
 	dbregen = () => {
 		axios.post('/api/db/generate')
@@ -229,16 +144,6 @@ class Database extends Component<DatabaseProps, DatabaseState> {
 					<p>{i18next.t('DATABASE.UPDATE_MESSAGE_DELETED')}</p>
 					<p>{i18next.t('CONFIRM_SURE')}</p>
 				</Modal>
-				<h1 style={{ marginTop: '30px' }}>{i18next.t('DATABASE.PROGRESS')}</h1>
-
-				<h3>{i18next.t('DATABASE.GENERATION')}</h3>
-				{this.state.generationProgress.text}<br />
-				<Progress percent={this.state.generationProgress.percentage} />
-				<h3>{i18next.t('DATABASE.BASE_UPDATE')}</h3>
-				{this.state.downloadProgress.text}<br />
-				<Progress percent={this.state.downloadProgress.percentage} />
-				{this.state.downloadBatchProgress.text}<br />
-				<Progress percent={this.state.downloadBatchProgress.percentage} />
 			</Layout.Content>
 		);
 	}
