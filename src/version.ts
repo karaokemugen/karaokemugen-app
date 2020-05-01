606afb8085d7fe33c2a440b2fe5ba1e0a7ce270a
Kmeuh <clemalbo@cegetel.net>
2020-05-01 03:51:41 +0200

    Change background for 3.2.1(.0)

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 export const version = {
-	number: '3.2.0-master',
-	name: 'Nadia Naturiste',
-	image: 'NadiaNaturiste.jpg',
+	number: '3.2.1',
+	name: 'Nadia Nostalgique',
+	image: 'NadiaNostalgique.jpg',
 	sha: null
 }
