28b4c8dbee96730430fe3584676a37e44b099dfa
Aeden <florentius.b@gmail.com>
2020-05-18 18:14:47 +0200

    Remove dead code

--- systempanel/src/reducers/reducers.ts
+++ systempanel/src/reducers/reducers.ts
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import AuthReducer from './auth';
 import NavigationReducer from './navigation';
-// import KarasReducer from './navigation';
 
 export function createRootReducer(history) {
   return combineReducers({
     router: connectRouter(history),
     auth: AuthReducer,
     navigation: NavigationReducer,
-    // karas: KarasReducer
   })
 }
