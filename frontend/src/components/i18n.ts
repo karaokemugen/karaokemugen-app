9cea8606d159d020db2f2ee765d6bc4eb6b83e2d
Aeden <florentius.b@gmail.com>
2020-05-19 15:15:59 +0200

    i18next-xhr-backend is now deprecated and seem useless for our case, remove it

--- frontend/src/components/i18n.ts
+++ frontend/src/components/i18n.ts
@@ -1,14 +1,10 @@
 import i18n from 'i18next';
 
-import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 const en = require('./locales/en.json');
 const fr = require('./locales/fr.json');
 
 i18n
-// load translation using xhr -> see /public/locales
-// learn more: https://github.com/i18next/i18next-xhr-backend
-	.use(Backend)
 // detect user language
 // learn more: https://github.com/i18next/i18next-browser-languageDetector
 	.use(LanguageDetector)
