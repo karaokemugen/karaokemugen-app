60a82fc69179a0435da612ca30827fcb0176128a
Leon <me@leonekmi.fr>
2020-05-29 17:10:06 +0200

    :mouse_three_button: "faut que le lien soit cliquable"

--- initpage/initpage.js
+++ initpage/initpage.js
@@ -1,3 +1,39 @@
+// https://github.com/bryanwoods/autolink-js
+(function() {
+	var autoLink,
+		slice = [].slice;
+
+	autoLink = function() {
+		var callback, k, linkAttributes, option, options, pattern, v;
+		options = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+		pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
+		if (!(options.length > 0)) {
+			return this.replace(pattern, "$1<a href='$2'>$2</a>");
+		}
+		option = options[0];
+		callback = option["callback"];
+		linkAttributes = ((function() {
+			var results;
+			results = [];
+			for (k in option) {
+				v = option[k];
+				if (k !== 'callback') {
+					results.push(" " + k + "='" + v + "'");
+				}
+			}
+			return results;
+		})()).join('');
+		return this.replace(pattern, function(match, space, url) {
+			var link;
+			link = (typeof callback === "function" ? callback(url) : void 0) || ("<a href='" + url + "'" + linkAttributes + ">" + url + "</a>");
+			return "" + space + link;
+		});
+	};
+
+	String.prototype['autoLink'] = autoLink;
+
+}).call(this);
+
 const ipcRenderer = require('electron').ipcRenderer
 ipcRenderer.on('initStep', (event, data) => {
 	let message = document.querySelector('.ip--message');
@@ -28,7 +64,7 @@ ipcRenderer.on('error', (event, data) => {
 })
 ipcRenderer.on('tip', (event, data) => {
 	let tipbox = document.querySelector('.ip--protip');
-	tipbox.textContent = data.message;
+	tipbox.innerHTML = data.message.autoLink({target: '_blank'});
 });
 
 let buttonLogsStatus = false;
