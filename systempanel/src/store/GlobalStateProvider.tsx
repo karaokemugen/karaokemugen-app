d97ed47139b21f44f8de11d3d23d2f3b2f88b85b
Aeden <florentius.b@gmail.com>
2020-05-31 17:07:30 +0200

    Fix GlobalStateProvider for compile with react-scripts

--- systempanel/src/store/GlobalStateProvider.tsx
+++ systempanel/src/store/GlobalStateProvider.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import GlobalContext from './context';
 import useGlobalState from './useGlobalState';
 
-const GlobalStateProvider = ({children}) => {
-	return (
-		<GlobalContext.Provider value={useGlobalState()}>{children}</GlobalContext.Provider>
-	);
-}
+const GlobalStateProvider = ({children}) =>	<GlobalContext.Provider value={useGlobalState()}>{children}</GlobalContext.Provider>
 
 export default GlobalStateProvider;
