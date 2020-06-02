2f843b58426a21a58cd7a677af1b35324daf03df
Guillaume Lebigot <axel@teri-chan.net>
2020-06-02 19:54:42 +0200

    Sentry: include source maps with errors

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,19 @@ import cloneDeep from 'lodash.clonedeep';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+
+// This allows TypeScript to detect our global value
+declare global {
+	namespace NodeJS {
+	  interface Global {
+		__rootdir__: string;
+	  }
+	}
+  }
+
+global.__rootdir__ = __dirname || process.cwd();
+
 initSentry(app);
 
 process.on('uncaughtException', exception => {
