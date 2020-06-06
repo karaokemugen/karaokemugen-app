f78afd73a151a52b55372c5ddb8c904d536bbce8
Guillaume Lebigot <axel@teri-chan.net>
2020-06-06 22:56:36 +0200

    Discord: fix activity too long

--- src/utils/discordRPC.ts
+++ src/utils/discordRPC.ts
@@ -3,6 +3,7 @@ import i18next from 'i18next';
 import sample from 'lodash.sample';
 
 import { getConfig } from '../lib/utils/config';
+import { sentryError } from '../lib/utils/sentry';
 import { version } from '../version';
 
 const clientId = '718211141033263145';
@@ -12,27 +13,32 @@ let rpc: any;
 discordRPC.register(clientId);
 
 export function setDiscordActivity(activityType: 'song' | 'idle', activityData?: any) {
-	if (!getConfig().Online.Discord.DisplayActivity || !rpc) return;
-	const startTimestamp = new Date();
-	let activity: string;
-	let activityDetail = 'Zzz...';
-	if (activityType === 'idle') {
-		activity = sample(i18next.t('DISCORD.IDLING', {returnObjects: true}));
+	try {
+		if (!getConfig().Online.Discord.DisplayActivity || !rpc) return;
+		const startTimestamp = new Date();
+		let activity: string;
+		let activityDetail = 'Zzz...';
+		if (activityType === 'idle') {
+			activity = sample(i18next.t('DISCORD.IDLING', {returnObjects: true}));
+		}
+		if (activityType === 'song') {
+			activity = activityData.title,
+			activityDetail = activityData.singer;
+		}
+		rpc.setActivity({
+			details: activity.substring(0, 128),
+			state: activityDetail.substring(0, 128),
+			startTimestamp,
+			largeImageKey: 'nanami-smile',
+			largeImageText: 'Karaoke Mugen',
+			smallImageKey: activityType === 'song' ? 'nanami-xd' : 'nanami-hehe2',
+			smallImageText: `Version ${version.number} - ${version.name}`,
+			instance: false,
+		});
+	} catch(err) {
+		sentryError(err);
+		throw err;
 	}
-	if (activityType === 'song') {
-		activity = activityData.title,
-		activityDetail = activityData.singer;
-	}
-	rpc.setActivity({
-		details: activity,
-		state: activityDetail,
-		startTimestamp,
-		largeImageKey: 'nanami-smile',
-		largeImageText: 'Karaoke Mugen',
-		smallImageKey: activityType === 'song' ? 'nanami-xd' : 'nanami-hehe2',
-		smallImageText: `Version ${version.number} - ${version.name}`,
-		instance: false,
-	});
 }
 
 export function stopDiscordRPC() {
