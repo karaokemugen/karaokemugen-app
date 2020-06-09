28d6281b7bb5336bb345a141b9525023960c45b5
Guillaume Lebigot <axel@teri-chan.net>
2020-06-09 16:50:24 +0200

    chore: new release 4.0.2-next (fixes CI 4th)

--- util/electronBuilderGetVersion.sh
+++ util/electronBuilderGetVersion.sh
@@ -1,7 +1,7 @@
 # Detects if we're running on a master or next release or tag release.
 # if not master or next, it's tag
 
-if [ "$CI_COMMIT_REF_NAME" = "master" ] && [ "$CI_COMMIT_REF_NAME" = "next" ]
+if [ "$CI_COMMIT_REF_NAME" = "master" ] || [ "$CI_COMMIT_REF_NAME" = "next" ]
 then
 	export BUILDVERSION=`cat package.json | grep version\": | awk -F\" {'print $4'}`
 	export RELEASE=$CI_COMMIT_REF_NAME
