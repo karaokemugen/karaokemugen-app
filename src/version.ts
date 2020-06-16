9c04ba38a9c5a84a009a3d7aba78245eb1464997
Guillaume Lebigot <axel@teri-chan.net>
2020-06-16 21:07:15 +0200

    I want crepes.

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,5 @@
 export const version = {
-<<<<<<< HEAD
-	number: '4.0.5 4-next',
-=======
-	number: '4.0.5 4-next',
->>>>>>> 6966cd0dc0be462a45d38e190881304b05b1b240
+	number: '4.0.4-next',
 	name: 'Ôgi Obscure',
 	image: 'OgiObscure.jpg',
 	sha: null
