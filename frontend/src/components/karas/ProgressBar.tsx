c93f1c583c78aea9461ea6740e7689862726a64e
Leon <me@leonekmi.fr>
2020-06-22 16:30:26 +0200

    :bug: Doing the XHR only when necessary

--- frontend/src/components/karas/ProgressBar.tsx
+++ frontend/src/components/karas/ProgressBar.tsx
@@ -82,56 +82,53 @@ class ProgressBar extends Component<IProps,IState> {
     * refresh the player infos
     */
     refreshPlayerInfos = async (data:PublicState) => {
-    	if (this.state.oldState != data) {
+		let element = document.getElementById('karaInfo');
+		if (element) {
+			let newWidth = element.offsetWidth *
+				10000 * (data.timePosition + this.state.refreshTime / 1000) / this.state.length / 10000 + 'px';
 
-			let element = document.getElementById('karaInfo');
-			if (element) {
-				let newWidth = element.offsetWidth *
-					10000 * (data.timePosition + this.state.refreshTime / 1000) / this.state.length / 10000 + 'px';
-
-				if (!this.state.oldState || data.timePosition != this.state.oldState.timePosition && this.state.length != 0) {
-					this.setState({width: newWidth});
-				}
+			if (!this.state.oldState || data.timePosition != this.state.oldState.timePosition && this.state.length != 0) {
+				this.setState({width: newWidth});
 			}
-    		if (!this.state.oldState || (this.state.oldState.status != data.status || this.state.oldState.playerStatus != data.playerStatus)) {
-    			status = data.playerStatus;
-    			if( status === 'stop') {
-    				this.setState({width: '0'});
-    			}
-    			this.setState({status: status});
-    		}
+		}
+		if (!this.state.oldState || (this.state.oldState.status != data.status || this.state.oldState.playerStatus != data.playerStatus)) {
+			status = data.playerStatus;
+			if( status === 'stop') {
+				this.setState({width: '0'});
+			}
+			this.setState({status: status});
+		}
 
-    		if (!this.state.oldState || data.currentlyPlaying !== this.state.oldState.currentlyPlaying) {
-    			this.setState({width: '0'});
-    		}
-
-    		if (data.currentlyPlaying === null) {
-    			this.setState({karaInfoText: i18next.t('KARA_PAUSED_WAITING'), length: -1});
-    		} else if (data.currentlyPlaying === 'Jingles') {
-    			this.setState({karaInfoText: i18next.t('JINGLE_TIME'), length: -1});
-    		} else if (data.currentlyPlaying === 'Intros') {
+		if (!this.state.oldState || data.currentlyPlaying !== this.state.oldState.currentlyPlaying) {
+			this.setState({width: '0'});
+			if (data.currentlyPlaying === null) {
+				this.setState({karaInfoText: i18next.t('KARA_PAUSED_WAITING'), length: -1});
+			} else if (data.currentlyPlaying === 'Jingles') {
+				this.setState({karaInfoText: i18next.t('JINGLE_TIME'), length: -1});
+			} else if (data.currentlyPlaying === 'Intros') {
 				this.setState({karaInfoText: i18next.t('INTRO_TIME'), length: -1});
 			} else if (data.currentlyPlaying === 'Outros') {
 				this.setState({karaInfoText: i18next.t('OUTRO_TIME'), length: -1});
 			} else if (data.currentlyPlaying === 'Encores') {
-    			this.setState({karaInfoText: i18next.t('ENCORES_TIME'), length: -1});
-    		} else if (data.currentlyPlaying === 'Sponsors') {
-    			this.setState({karaInfoText: i18next.t('SPONSOR_TIME'), length: -1});
-    		} else if (store.getLogInfos()) {
-    			let response = await axios.get('/karas/' + data.currentlyPlaying);
-    			let kara = response.data;
-    			let karaInfoText;
-    			if (this.props.lyrics || (this.props.scope === 'public' && this.props.webappMode == 1)) {
-    				let text = data.subText;
-    				if (text) text = text.indexOf('\n') == -1 ? text : text.substring(0, text.indexOf('\n'));
-    				karaInfoText = text;
-    			} else {
-    				karaInfoText = buildKaraTitle(kara, true);
-    			}
-    			this.setState({karaInfoText: karaInfoText, length: kara.duration});
-    		}
-    		this.setState({oldState: data});
-    	}
+				this.setState({karaInfoText: i18next.t('ENCORES_TIME'), length: -1});
+			} else if (data.currentlyPlaying === 'Sponsors') {
+				this.setState({karaInfoText: i18next.t('SPONSOR_TIME'), length: -1});
+			} else if (store.getLogInfos()) {
+				let response = await axios.get('/karas/' + data.currentlyPlaying);
+				let kara = response.data;
+				let karaInfoText;
+				if (this.props.lyrics || (this.props.scope === 'public' && this.props.webappMode == 1)) {
+					let text = data.subText;
+					if (text) text = text.indexOf('\n') == -1 ? text : text.substring(0, text.indexOf('\n'));
+					karaInfoText = text;
+				} else {
+					karaInfoText = buildKaraTitle(kara, true);
+				}
+				this.setState({karaInfoText: karaInfoText, length: kara.duration});
+			}
+		}
+
+		this.setState({oldState: data});
     };
 
     render() {
