ccb5b3fe028255e4acbfbf38fb52863b03e75ee8
Aeden <florentius.b@gmail.com>
2020-06-22 22:58:41 +0200

    Add sentry react to system panel and frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
-import App from "./app";
 import ReactDOM from 'react-dom';
 import React from 'react';
+import * as Sentry from '@sentry/browser';
+
+import App from "./app";
 import './electron';
+
+Sentry.init({dsn: "https://464814b9419a4880a2197b1df7e1d0ed@o399537.ingest.sentry.io/5256806"});
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
