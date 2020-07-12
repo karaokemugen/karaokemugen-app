baf1981d3b8b3507dd86f2eb9e07c2c22f20e4cb
Aeden <florentius.b@gmail.com>
2020-07-12 20:26:55 +0200

    Remove button addkara in current playlist in public page

--- frontend/src/components/karas/ActionsButtons.tsx
+++ frontend/src/components/karas/ActionsButtons.tsx
@@ -40,7 +40,8 @@ class ActionsButtons extends Component<IProps, unknown> {
 					<button title={i18next.t('TOOLTIP_DELETEKARA')}
 						className={`${classValue} karaLineButton`} onClick={this.props.deleteKara}><i className="fas fa-minus"></i></button> : null}
 				{((this.props.scope === 'admin' && this.props.idPlaylistTo !== -1) ||
-					(this.props.scope === 'public' && this.props.idPlaylist !== store.getState().publicPlaylistID))
+					(this.props.scope === 'public' && this.props.idPlaylist !== store.getState().publicPlaylistID
+					&& this.props.idPlaylist !== store.getState().currentPlaylistID))
 					&& this.props.idPlaylistTo !== -5 ?
 					<button
 						title={`${this.props.kara?.flag_inplaylist && this.props.scope !== 'admin' ? i18next.t('TOOLTIP_UPVOTE') :
