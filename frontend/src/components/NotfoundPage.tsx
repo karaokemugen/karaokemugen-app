199c764613438e1c58ef9a05f4c22cd20a1c79fb
Leon <me@leonekmi.fr>
2020-07-14 22:39:10 +0200

    :sparkles: Fancy new 404

--- frontend/src/components/NotfoundPage.tsx
+++ frontend/src/components/NotfoundPage.tsx
@@ -1,19 +1,19 @@
 import i18next from 'i18next';
 import React, { Component } from 'react';
 
-import image404 from '../assets/nanami_.jpg';
+import image404 from '../assets/nanami-surpris.png';
 
 class NotfoundPage extends Component {
 
 	render() {
 		return (
-			<React.Fragment>
-				<p>{location.pathname}</p>
+			<div className="page404-lost">
 				<h1>{i18next.t('404')}</h1>
 				<h3>{i18next.t('404_2')}</h3>
-				<strong><pre>    * &lt;----- {i18next.t('404_3')}</pre></strong>
-				<img height="500" src={image404} />
-			</React.Fragment>
+				<div className="you-are-here">{location.pathname} &lt;----- {i18next.t('404_3')}</div>
+				<a href="/" className="page404-btn">{i18next.t('404_4')}</a><br/>
+				<img height="150" src={image404} />
+			</div>
 		);
 	}
 }
