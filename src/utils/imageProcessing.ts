c0145a5d015cd520783e45803175481bef918664
Leon <me@leonekmi.fr>
2020-07-15 21:08:45 +0200

    :wrench: Delete the optimized image once consumed

--- src/utils/imageProcessing.ts
+++ src/utils/imageProcessing.ts
@@ -4,7 +4,7 @@ import circle from '@jimp/plugin-circle';
 import png from '@jimp/png';
 
 import { convertAvatar } from '../lib/utils/ffmpeg';
-import { replaceExt } from '../lib/utils/files';
+import { replaceExt, asyncUnlink } from '../lib/utils/files';
 import sentry from '../utils/sentry';
 
 const j = configure({
@@ -18,6 +18,8 @@ export async function createCircleAvatar(file: string) {
 		// Load the png converted file
 		const image = await j.read(convertedFile);
 		await image.circle().writeAsync(replaceExt(file, '.circle.png'));
+		// Delete the converted file
+		await asyncUnlink(convertedFile);
 	} catch(err) {
 		sentry.error(err);
 		throw err;
