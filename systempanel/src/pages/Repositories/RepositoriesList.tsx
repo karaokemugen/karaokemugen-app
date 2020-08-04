54a0338dbe844575f0acfbbe5483f0546f52a2a7
Aeden <florentius.b@gmail.com>
2020-08-04 13:31:38 +0200

    [Lint] Lint Repositories files

--- systempanel/src/pages/Repositories/RepositoriesList.tsx
+++ systempanel/src/pages/Repositories/RepositoriesList.tsx
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
-import { Button, Layout, Table, Divider, Checkbox, Tooltip } from 'antd';
-import {Link} from 'react-router-dom';
-import i18next from 'i18next';
-import { Repository } from '../../../../src/lib/types/repo';
-import { QuestionCircleOutlined, PlusOutlined, DeleteOutlined, ArrowUpOutlined, ArrowDownOutlined, EditOutlined } from '@ant-design/icons';
+import { ArrowDownOutlined, ArrowUpOutlined, DeleteOutlined, EditOutlined,PlusOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Divider, Layout, Table, Tooltip } from 'antd';
 import Axios from 'axios';
+import i18next from 'i18next';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Repository } from '../../../../src/lib/types/repo';
 import { getAxiosInstance } from '../../axiosInterceptor';
 
 interface RepositoryListState {
@@ -12,8 +13,8 @@ interface RepositoryListState {
 	repository?: Repository
 }
 
-let timer:any;
-class RepositoryList extends Component<{}, RepositoryListState> {
+let timer: any;
+class RepositoryList extends Component<unknown, RepositoryListState> {
 
 	constructor(props) {
 		super(props);
@@ -27,8 +28,8 @@ class RepositoryList extends Component<{}, RepositoryListState> {
 	}
 
 	refresh = async () => {
-		let res = await Axios.get('/repos');
-		this.setState({repositories: res.data});
+		const res = await Axios.get('/repos');
+		this.setState({ repositories: res.data });
 	}
 
 	deleteRepository = async (repository: Repository) => {
@@ -36,14 +37,14 @@ class RepositoryList extends Component<{}, RepositoryListState> {
 		this.refresh();
 	}
 
-	move = async (index: number, change:number) => {
-		let repositories = this.state.repositories;
-		let firstRepos =  repositories[index];
-		let secondRepos = repositories[index + change];
+	move = async (index: number, change: number) => {
+		const repositories = this.state.repositories;
+		const firstRepos = repositories[index];
+		const secondRepos = repositories[index + change];
 		repositories[index + change] = firstRepos;
 		repositories[index] = secondRepos;
 		await Axios.put('/settings', {
-			setting: {System:{Repositories: repositories}}
+			setting: { System: { Repositories: repositories } }
 		});
 		this.refresh();
 		if (timer) clearTimeout(timer);
@@ -54,10 +55,10 @@ class RepositoryList extends Component<{}, RepositoryListState> {
 
 	render() {
 		return (
-            <Layout.Content style={{ padding: '25px 50px', textAlign: 'center' }}>
+			<Layout.Content style={{ padding: '25px 50px', textAlign: 'center' }}>
 				<Layout>
 					<Layout.Header>
-					<span><Link to={`/system/km/repositories/new`}>{i18next.t('REPOSITORIES.NEW_REPOSITORY')}<PlusOutlined /></Link></span>
+						<span><Link to={'/system/km/repositories/new'}>{i18next.t('REPOSITORIES.NEW_REPOSITORY')}<PlusOutlined /></Link></span>
 					</Layout.Header>
 					<Layout.Content>
 						<Table
@@ -68,7 +69,7 @@ class RepositoryList extends Component<{}, RepositoryListState> {
 					</Layout.Content>
 				</Layout>
 			</Layout.Content>
-        );
+		);
 	}
 
 	columns = [{
@@ -79,27 +80,37 @@ class RepositoryList extends Component<{}, RepositoryListState> {
 		title: i18next.t('REPOSITORIES.PATH_KARAS'),
 		dataIndex: 'Path.Karas',
 		key: 'path_karas',
-		render: (text, record:Repository) => (record.Path.Karas.map(item => {return <div className="pathFolders" key={item}>{item}</div>}))
+		render: (text, record: Repository) => (record.Path.Karas.map(item => {
+			return <div className="pathFolders" key={item}>{item}</div>;
+		}))
 	}, {
 		title: i18next.t('REPOSITORIES.PATH_LYRICS'),
 		dataIndex: 'Path.Lyrics',
 		key: 'path_lyrics',
-		render: (text, record:Repository) => (record.Path.Lyrics.map(item => {return <div className="pathFolders" key={item}>{item}</div>}))
+		render: (text, record: Repository) => (record.Path.Lyrics.map(item => {
+			return <div className="pathFolders" key={item}>{item}</div>;
+		}))
 	}, {
 		title: i18next.t('REPOSITORIES.PATH_MEDIAS'),
 		dataIndex: 'Path.Medias',
 		key: 'path_medias',
-		render: (text, record:Repository) => (record.Path.Medias.map(item => {return <div className="pathFolders" key={item}>{item}</div>}))
+		render: (text, record: Repository) => (record.Path.Medias.map(item => {
+			return <div className="pathFolders" key={item}>{item}</div>;
+		}))
 	}, {
 		title: i18next.t('REPOSITORIES.PATH_SERIES'),
 		dataIndex: 'Path.Series',
 		key: 'path_series',
-		render: (text, record:Repository) => (record.Path.Series.map(item => {return <div className="pathFolders" key={item}>{item}</div>}))
+		render: (text, record: Repository) => (record.Path.Series.map(item => {
+			return <div className="pathFolders" key={item}>{item}</div>;
+		}))
 	}, {
 		title: i18next.t('REPOSITORIES.PATH_TAGS'),
 		dataIndex: 'Path.Tags',
 		key: 'path_tags',
-		render: (text, record:Repository) => (record.Path.Tags.map(item => {return <div key={item}>{item}</div>}))
+		render: (text, record: Repository) => (record.Path.Tags.map(item => {
+			return <div key={item}>{item}</div>;
+		}))
 	}, {
 		title: i18next.t('REPOSITORIES.ONLINE'),
 		dataIndex: 'Online',
@@ -115,28 +126,28 @@ class RepositoryList extends Component<{}, RepositoryListState> {
 			<Tooltip title={i18next.t('REPOSITORIES.MOVE_TOOLTIP')}>
 				<QuestionCircleOutlined />
 			</Tooltip>
-		</span> ,
+		</span>,
 		key: 'move',
 		render: (text, record, index) => {
 			return (
-                <React.Fragment>
-                        {index > 0 ? 
-                            <Button type="default" icon={<ArrowUpOutlined />} onClick={() => this.move(index, -1)}></Button> : null}
-                        {index < this.state.repositories.length-1 ?  
-                            <Button type="default" icon={<ArrowDownOutlined />} onClick={() => this.move(index, +1)}></Button> : null}
-                    </React.Fragment>
-            );
+				<React.Fragment>
+					{index > 0 ?
+						<Button type="default" icon={<ArrowUpOutlined />} onClick={() => this.move(index, -1)}></Button> : null}
+					{index < this.state.repositories.length - 1 ?
+						<Button type="default" icon={<ArrowDownOutlined />} onClick={() => this.move(index, +1)}></Button> : null}
+				</React.Fragment>
+			);
 		}
 	}, {
 		title: i18next.t('ACTION'),
 		key: 'action',
-		render: (text, record:Repository) => (
+		render: (text, record: Repository) => (
 			<span>
 				<Link to={`/system/km/repositories/${record.Name}`}><EditOutlined /></Link>
 				{this.state.repositories.length > 1 ?
 					<React.Fragment>
-						<Divider type="vertical"/>
-						<Button type="primary" danger icon={<DeleteOutlined />} 
+						<Divider type="vertical" />
+						<Button type="primary" danger icon={<DeleteOutlined />}
 							onClick={() => this.deleteRepository(record)}></Button>
 					</React.Fragment> : null
 				}
