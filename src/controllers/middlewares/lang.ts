f41f311f5d0cea171322c342669d152177fb5401
Guillaume Lebigot <axel@teri-chan.net>
2020-08-28 09:58:53 +0200

    chore: some code cleanup for #758

--- src/controllers/middlewares/lang.ts
+++ src/controllers/middlewares/lang.ts
@@ -1,5 +1,5 @@
-export const getLang = (req: any, _res: any, next: any) => {
+export function getLang(req: any, _res: any, next: any) {
 	const langs = req.get('accept-language');
 	if (langs) req.lang = langs.split(',')[0].substring(0,2);
 	next();
-};
+}
