d1ba239db4e75389f26ed1bc0841365707c9ef53
Leon <me@leonekmi.fr>
2020-10-02 17:53:18 +0200

    :green_heart: Fix sentry task

--- util/sentryUpdateReleases.ts
+++ util/sentryUpdateReleases.ts
@@ -1,25 +1,25 @@
 import execa from 'execa';
 
-import {version} from '../src/version';
+import {version} from '../package.json';
 
 // Create the release if it doesn't exists
-execa.commandSync(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app new ${version.number}`,
+execa.commandSync(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app new ${version}`,
 	{stdout: 'inherit', stderr: 'inherit'});
 
-execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version.number} upload-sourcemaps --no-rewrite --url-prefix app:///dist/ dist/`,
+execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version} upload-sourcemaps --no-rewrite --url-prefix app:///dist/ dist/`,
 	{stdout: 'inherit', stderr: 'inherit'});
 
-execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version.number} upload-sourcemaps --no-rewrite --url-prefix ~/ frontend/build/`,
+execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version} upload-sourcemaps --no-rewrite --url-prefix ~/ frontend/build/`,
 	{stdout: 'inherit', stderr: 'inherit'});
 
-execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version.number} upload-sourcemaps --no-rewrite --url-prefix ~/system/ systempanel/build/`,
+execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version} upload-sourcemaps --no-rewrite --url-prefix ~/system/ systempanel/build/`,
 	{stdout: 'inherit', stderr: 'inherit'});
 
-execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app set-commits --commit Karaoke\\ Mugen\\ /\\ Karaoke\\ Mugen\\ Application@${process.env.CI_COMMIT_SHA} ${version.number}`,
+execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app set-commits --commit Karaoke\\ Mugen\\ /\\ Karaoke\\ Mugen\\ Application@${process.env.CI_COMMIT_SHA} ${version}`,
 	{stdout: 'inherit', stderr: 'inherit'});
 
 // If tagged, deploy release
 if (process.env.CI_COMMIT_TAG) {
-	execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app deploys ${version.number} new -e release`,
+	execa.command(`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app deploys ${version} new -e release`,
 		{stdout: 'inherit', stderr: 'inherit'});
 }
