5e16246bb06b00bb229a21cf00f250698369b77a
Guillaume Lebigot <axel@teri-chan.net>
2020-10-05 11:45:03 +0200

    IAmAnIdiot: Fix preview creation calls

--- src/services/karaManagement.ts
+++ src/services/karaManagement.ts
@@ -244,7 +244,7 @@ export async function integrateKaraFile(file: string) {
 		await createKaraInDB(karaData, { refresh: false });
 	}
 	// Do not create image previews if running this from the command line.
-	if (!getState().opt.generateDB && getConfig().Frontend.GeneratePreviews) createImagePreviews(await getKaras({mode: 'kid', modeValue: karaData.kid, token: {username: 'admin', role: 'admin'}}), false, 'single');
+	if (!getState().opt.generateDB && getConfig().Frontend.GeneratePreviews) createImagePreviews(await getKaras({mode: 'kid', modeValue: karaData.kid, token: {username: 'admin', role: 'admin'}}), 'single');
 	saveSetting('baseChecksum', getStoreChecksum());
 }
 
