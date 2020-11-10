4c992cc5fadbf8613b3fede2ca6de326c50102bb
Leon <me@leonekmi.fr>
2020-11-10 23:55:39 +0100

    :white_check_mark: mpv is never launched, don't test currentSong

--- test/player.ts
+++ test/player.ts
@@ -12,7 +12,6 @@ describe('Player', () => {
 		const data = await commandBackend(token, 'getPlayerStatus');
 		expect(data.currentRequester).to.satisfy((e:any) => typeof e === 'string' || e === null);
 		expect(data.currentSessionID).to.be.a('string').and.match(new RegExp(uuidRegexp));
-		expect(data.currentSong).to.satisfy((e:any) => typeof e === 'object' || e === null);
 		expect(data.defaultLocale).to.be.a('string').and.have.lengthOf(2);
 		expect(data.mediaType).to.be.equal('background');
 		expect(data.onTop).to.be.a('boolean');
