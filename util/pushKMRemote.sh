34977486d9feaaa0049e6393d4e606d6f75452f1
Leon <me@leonekmi.fr>
2020-12-14 22:50:14 +0100

    :wrench: Touch kmremote directory

--- util/pushKMRemote.sh
+++ util/pushKMRemote.sh
@@ -1,3 +1,3 @@
 source util/versionUtil.sh
 
-lftp -c "set cmd:fail-exit yes; set ftp:ssl-allow no; open -u $USERNAME,$PASSWORD $HOST; lcd kmfrontend/build; cd kmremote-releases; mkdir $BUILDVERSION; cd $BUILDVERSION; mirror -Rnev --parallel=10"
+lftp -c "set cmd:fail-exit yes; set ftp:ssl-allow no; open -u $USERNAME,$PASSWORD $HOST; lcd kmfrontend/build; cd kmremote-releases; mkdir -f $BUILDVERSION; cd $BUILDVERSION; mirror -Rnev --parallel=10; !touch ../"
