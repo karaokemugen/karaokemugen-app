75fd7a775b1b89bb8059449f778cf29bef472d1b
Aeden <florentius.b@gmail.com>
2020-12-28 16:44:19 +0100

    fix KM-APP-GR

--- kmfrontend/src/systempanel/pages/UnusedList.tsx
+++ kmfrontend/src/systempanel/pages/UnusedList.tsx
@@ -1,6 +1,6 @@
-import {Col, Layout, Radio, Row, Select,Table} from 'antd';
+import { Col, Layout, Radio, Row, Select, Table } from 'antd';
 import i18next from 'i18next';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import { commandBackend } from '../../utils/socket';
 import { getTagTypeName } from '../../utils/tagTypes';
@@ -27,21 +27,25 @@ class SessionList extends Component<unknown, SessionListState> {
 
 	refresh = async () => {
 		const res = await commandBackend('getRepos');
-		this.setState({repository:res[0].Name, repositories: res.map(value => value.Name)});
+		this.setState({ repository: res[0].Name, repositories: res.map(value => value.Name) });
 	}
 
 	getTags = async () => {
-		const res = await commandBackend('getUnusedTags', {name: this.state.repository}, undefined, 60000);
-		this.setState({unused: res.map(value => {
-			return {name: value.name, types: value.types, file: value.tagfile};
-		})});
+		const res = await commandBackend('getUnusedTags', { name: this.state.repository }, undefined, 60000);
+		this.setState({
+			unused: res ? res.map(value => {
+				return { name: value.name, types: value.types, file: value.tagfile };
+			}) : []
+		});
 	}
 
 	getMedias = async () => {
-		const res = await commandBackend('getUnusedMedias', {name: this.state.repository}, undefined, 60000);
-		this.setState({unused: res.map(value => {
-			return {file: value};
-		})});
+		const res = await commandBackend('getUnusedMedias', { name: this.state.repository }, undefined, 60000);
+		this.setState({
+			unused: res ? res.map(value => {
+				return { file: value };
+			}) : []
+		});
 	}
 
 	render() {
@@ -52,11 +56,11 @@ class SessionList extends Component<unknown, SessionListState> {
 					<div className='description'>{i18next.t('HEADERS.UNUSED_FILES.DESCRIPTION')}</div>
 				</Layout.Header>
 				<Layout.Content>
-					<Row style={{margin: '0.5em'}}>
+					<Row style={{ margin: '0.5em' }}>
 						{this.state.repositories && this.state.repository ?
-							<Col style={{ paddingRight: '100px'}}>
-								<label style={{ paddingRight: '15px'}}>{i18next.t('UNUSED_FILES.REPOSITORY')}</label>
-								<Select  style={{ width: 150 }} defaultValue={this.state.repository}>
+							<Col style={{ paddingRight: '100px' }}>
+								<label style={{ paddingRight: '15px' }}>{i18next.t('UNUSED_FILES.REPOSITORY')}</label>
+								<Select style={{ width: 150 }} defaultValue={this.state.repository}>
 									{this.state.repositories.map(repo => {
 										return <Select.Option key={repo} value={repo}>{repo}</Select.Option>;
 									})
@@ -64,16 +68,16 @@ class SessionList extends Component<unknown, SessionListState> {
 								</Select>
 							</Col> : null
 						}
-						<Col style={{ paddingTop: '5px'}}>
-							<label style={{ paddingRight: '15px'}}>{i18next.t('MENU.UNUSED_FILES')}</label>
+						<Col style={{ paddingTop: '5px' }}>
+							<label style={{ paddingRight: '15px' }}>{i18next.t('MENU.UNUSED_FILES')}</label>
 							<Radio checked={this.state.type === 'tags'}
 								onChange={async () => {
-									await this.setState({type: 'tags'});
+									await this.setState({ type: 'tags' });
 									this.getTags();
 								}}>{i18next.t('UNUSED_FILES.TAGS')}</Radio>
 							<Radio checked={this.state.type === 'medias'}
 								onChange={async () => {
-									await this.setState({type: 'medias'});
+									await this.setState({ type: 'medias' });
 									this.getMedias();
 								}}>{i18next.t('UNUSED_FILES.MEDIAS')}</Radio>
 						</Col>
