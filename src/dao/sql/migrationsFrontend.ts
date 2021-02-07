aa6c4ea73973ecac849d3688810e695baaab3e0b
Leon <me@leonekmi.fr>
2021-02-07 14:53:44 +0100

    :wrench: Mark all frontend migrations as done for firstRun

--- src/dao/sql/migrationsFrontend.ts
+++ src/dao/sql/migrationsFrontend.ts
@@ -1,3 +1,5 @@
 export const sqlSelectAllMigrations = 'SELECT name, flag_done FROM migrations_frontend';
 
 export const sqlUpdateMigrations = 'UPDATE migrations_frontend SET flag_done = $2 WHERE name = $1';
+
+export const sqlMarkAllMigrationsAsDone = 'UPDATE migrations_frontend SET flag_done = true';
