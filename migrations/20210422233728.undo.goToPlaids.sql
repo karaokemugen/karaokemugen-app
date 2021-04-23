1f7e90ccdc42f79b52f5a27e83b975cb9ab61cc9
Leon <me@leonekmi.fr>
2021-04-23 03:16:24 +0200

    🐛 Fix down migration [skip ci]

--- migrations/20210422233728.undo.goToPlaids.sql
+++ migrations/20210422233728.undo.goToPlaids.sql
@@ -14,13 +14,13 @@ alter table playlist_content
         foreign key (fk_id_playlist) references playlist
             on update cascade on delete cascade;
 
-update playlist set pk_id_playlist = nextval(playlist_pk_id_playlist_seq)::text;
+update playlist set pk_id_playlist = nextval('playlist_pk_id_playlist_seq')::text;
 
 alter table playlist_content
     drop constraint playlist_content_fk_id_playlist_fkey;
 
 alter table playlist
-    alter column pk_id_playlist set default nextval(playlist_pk_id_playlist_seq);
+    alter column pk_id_playlist set default nextval('playlist_pk_id_playlist_seq');
 
 alter table playlist
     alter column pk_id_playlist type int using pk_id_playlist::int;
