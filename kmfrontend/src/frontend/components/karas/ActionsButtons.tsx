3fc354fd68925acedc4b31651983ec3e67abcefe
Florent Berthelot <florentius.b@gmail.com>
2021-07-24 18:52:26 +0200

    :bug: Fix missing delete button

--- kmfrontend/src/frontend/components/karas/ActionsButtons.tsx
+++ kmfrontend/src/frontend/components/karas/ActionsButtons.tsx
@@ -62,7 +62,7 @@ class ActionsButtons extends Component<IProps, unknown> {
 
 				{this.props.plaid !== nonStandardPlaylists.favorites
 					&& ((this.props.scope === 'admin' && this.props.plaid !== nonStandardPlaylists.library
-						&& !(this.props.isHeader && nonStandardPlaylists.blacklist)
+						&& !(this.props.isHeader && this.props.plaid === nonStandardPlaylists.blacklist)
 						&& !(this.props.plaid === this.context.globalState.settings.data.state.publicPlaid
 							&& this.props.plaidTo === this.context.globalState.settings.data.state.currentPlaid))
 						|| (this.props.scope !== 'admin' && !this.props.kara?.flag_dejavu && !this.props.kara?.flag_playing
