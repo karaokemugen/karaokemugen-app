c7ca22a3d4f423021afc24bfbb80e2f42618922f
Guillaume Lebigot <axel@teri-chan.net>
2021-08-16 23:22:25 +0200

    🗃 🚑 Fixes to database migrations

--- migrations/20210723000000.do.switchToJSONPath.sql
+++ migrations/20210723000000.do.switchToJSONPath.sql
@@ -1,7 +1,7 @@
 DROP TABLE all_karas;
 CREATE TABLE all_karas AS
 SELECT k.*,
-	 CASE WHEN MIN(kt.pk_tid::text) IS NULL THEN null ELSE jsonb_agg(DISTINCT json_build_object('tid', kt.pk_tid, 'short', kt.short, 'name', kt.name, 'problematic', kt.problematic, 'aliases', kt.aliases, 'i18n', kt.i18n, 'priority', kt.priority, 'type_in_kara', ka.type, 'karafile_tag', kt.karafile_tag)::jsonb) END as tags,
+	 CASE WHEN MIN(kt.pk_tid::text) IS NULL THEN null ELSE jsonb_agg(DISTINCT json_build_object('tid', kt.pk_tid, 'short', kt.short, 'name', kt.name, 'problematic', kt.problematic, 'aliases', kt.aliases, 'i18n', kt.i18n, 'priority', kt.priority, 'type_in_kara', ka.type)::jsonb) END as tags,
 	 tsvector_agg(kt.tag_search_vector) || k.title_search_vector AS search_vector,
 	 CASE WHEN MIN(kt.pk_tid::text) IS NULL THEN ARRAY[]::text[] ELSE array_agg(DISTINCT kt.pk_tid::text || '~' || ka.type::text) END AS tid,
   string_agg(DISTINCT lower(unaccent(tlang.name)), ', ' ORDER BY lower(unaccent(tlang.name))) AS languages_sortable,
