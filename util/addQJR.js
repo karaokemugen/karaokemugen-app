dff543a5aa01ce8b6fa607f94b9ffa62cdbd4f76
Guillaume Lebigot <axel@teri-chan.net>
2021-09-12 19:09:42 +0200

    Fix stuff on addQJR [skip-ci]

--- util/addQJR.js
+++ util/addQJR.js
@@ -8,7 +8,7 @@ async function main() {
 		if (!file.endsWith('.tag.json')) continue;
 		const tagData = await fs.readFile(path.resolve(tpath, file), 'utf-8');
 		const tag = JSON.parse(tagData);
-		if (tag.tag.i18n.jpn && !tag.tag.types.includes('langs')) {
+		if (!tag.tag.i18n.qjr && tag.tag.i18n.jpn && !tag.tag.types.includes('langs')) {
 			tag.tag.i18n.qjr = tag.tag.name;
 			await fs.writeFile(path.resolve(tpath, file), JSON.stringify(tag, null, 2), 'utf-8');
 		}
