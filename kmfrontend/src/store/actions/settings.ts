d404b9ff316625b48ed615b0c5636c6714561329
Florent Berthelot <florentius.b@gmail.com>
2021-11-21 17:14:52 +0100

    🥅 Ignore "no space left on device" errors in Sentry

--- kmfrontend/src/store/actions/settings.ts
+++ kmfrontend/src/store/actions/settings.ts
@@ -50,7 +50,13 @@ function setSentry(environment: string, version: Version, config: Config, user: 
 			dsn: 'https://464814b9419a4880a2197b1df7e1d0ed@o399537.ingest.sentry.io/5256806',
 			environment: environment || 'release',
 			release: version.number,
-			ignoreErrors: ['Network Error', 'Request failed with status code', 'Request aborted', 'ResizeObserver loop limit exceeded']
+			ignoreErrors: [
+				'Network Error',
+				'Request failed with status code',
+				'Request aborted',
+				'ResizeObserver loop limit exceeded',
+				/.*[n|N]o space left on device.*/
+			]
 		});
 		Sentry.configureScope((scope) => {
 			scope.setUser({
