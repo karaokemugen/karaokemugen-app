88e75db4234d978b6e9119fe73f294fa59dd7366
Guillaume Lebigot <axel@teri-chan.net>
2022-01-05 13:29:36 +0100

    Fix release with Erin

--- util/pushKMRemote.sh
+++ util/pushKMRemote.sh
@@ -1,3 +1,3 @@
 source util/versionUtil.sh
 
-lftp -c "set cmd:fail-exit yes; set ftp:ssl-allow no; open -u $USERNAME,$PASSWORD $HOST; lcd kmfrontend/build; cd kmremote-releases; set cmd:fail-exit no; mkdir -f $BUILDVERSION; set cmd:fail-exit yes; cd $BUILDVERSION; mirror -Rnev --parallel=10"
+lftp -c "set cmd:fail-exit yes; set ftp:ssl-allow no; open -u $USERNAME,$PASSWORD $HOST; lcd kmfrontend/build; cd srv/kmremote-releases; set cmd:fail-exit no; mkdir -f $BUILDVERSION; set cmd:fail-exit yes; cd $BUILDVERSION; mirror -Rnev --parallel=10"
