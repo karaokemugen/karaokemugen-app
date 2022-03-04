8b1a45b85f1134318e115df81df0273adb20e464
Leon <me@leonekmi.fr>
2022-03-04 23:45:34 +0100

    ♻️ Rework sentryUpdateReleases.js with @sentry/cli package.

--- util/sentryUpdateReleases.js
+++ util/sentryUpdateReleases.js
@@ -1,33 +1,32 @@
-import { execaCommand, execaSync } from 'execa';
 import { promises as fs } from 'node:fs';
+import SentryCli from '@sentry/cli';
 
-const { version } = JSON.parse(await fs.readFile('package.json'));
+const { version } = JSON.parse(await fs.readFile('package.json', 'utf-8'));
 
-// Create the release if it doesn't exists
-execaSync(
-	`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app new ${version}`,
-	{ stdout: 'inherit', stderr: 'inherit' }
-);
+const sentry = new SentryCli(null, {
+	authToken: process.env.SENTRYTOKEN,
+	org: 'karaoke-mugen',
+	dist: process.env.CI_JOB_ID,
+});
 
-execaCommand(
-	`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version} upload-sourcemaps --no-rewrite --url-prefix app:///dist/ dist/`,
-	{ stdout: 'inherit', stderr: 'inherit' }
-);
+await sentry.releases.new(version, { projects: ['km-app'] });
+await sentry.releases.uploadSourceMaps(version, {
+	rewrite: false,
+	urlPrefix: 'app:///dist/',
+	include: ['dist/'],
+});
+await sentry.releases.uploadSourceMaps(version, {
+	rewrite: false,
+	urlPrefix: '~/static/js',
+	include: ['kmfrontend/build/static/js'],
+});
+await sentry.releases.setCommits(version, {
+	repo: 'Karaoke Mugen / Karaoke Mugen Application',
+	commit: process.env.CI_COMMIT_SHA,
+});
 
-execaCommand(
-	`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app files ${version} upload-sourcemaps --no-rewrite --url-prefix ~/static/js kmfrontend/build/static/js`,
-	{ stdout: 'inherit', stderr: 'inherit' }
-);
-
-execaCommand(
-	`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app set-commits --commit Karaoke\\ Mugen\\ /\\ Karaoke\\ Mugen\\ Application@${process.env.CI_COMMIT_SHA} ${version}`,
-	{ stdout: 'inherit', stderr: 'inherit' }
-);
-
-// If tagged, deploy release
 if (process.env.CI_COMMIT_TAG) {
-	execaCommand(
-		`yarn sentry-cli --auth-token ${process.env.SENTRYTOKEN} releases --org karaoke-mugen -p km-app deploys ${version} new -e release`,
-		{ stdout: 'inherit', stderr: 'inherit' }
-	);
+	await sentry.releases.newDeploy(version, {
+		env: 'release',
+	});
 }
