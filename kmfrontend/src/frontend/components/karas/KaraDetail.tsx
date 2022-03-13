566da38638cf1bc6aaee8ac65538e9361107c787
Florent Berthelot <florentius.b@gmail.com>
2022-03-13 13:45:52 +0100

    🐛 Fix KM-APP-3AA

--- kmfrontend/src/frontend/components/karas/KaraDetail.tsx
+++ kmfrontend/src/frontend/components/karas/KaraDetail.tsx
@@ -276,7 +276,7 @@ export default function KaraDetail(props: IProps) {
 				</button>
 			);
 
-		const modifyKaraokeButton = context.globalState.settings.data.config?.System?.Repositories.filter(
+		const modifyKaraokeButton = context.globalState.settings.data.config?.System?.Repositories?.filter(
 			value => value.Name === kara.repository
 		)[0].MaintainerMode ? (
 			<a href={`/system/karas/${kara.kid}`}>
