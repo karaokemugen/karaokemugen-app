fa913b26996917205d74b74b2936b98f6e62066a
Guillaume Lebigot <axel@teri-chan.net>
2022-03-28 15:06:07 +0200

    :ambulance: A ; is missing and everything breaks.

--- migrations/20220328000000.do.removeDuplicateCriterias.sql
+++ migrations/20220328000000.do.removeDuplicateCriterias.sql
@@ -3,6 +3,6 @@ DELETE FROM playlist_criteria T1
 WHERE   T1.ctid    < T2.ctid --yes, it works, this is an internal, hidden column
     AND T1.fk_id_playlist < T2.fk_id_playlist
     AND T1.value = T2.value
-    AND T1.type = T2.type
+    AND T1.type = T2.type;
 
-CREATE UNIQUE INDEX ON playlist_criterias USING btree(fk_id_playlist, type, value);
+CREATE UNIQUE INDEX ON playlist_criteria USING btree(fk_id_playlist, type, value);
