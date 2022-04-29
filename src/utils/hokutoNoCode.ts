4d558d61892a0cd87619f678b1a2c690b3a652cf
Guillaume Lebigot <axel@teri-chan.net>
2022-04-29 13:54:07 +0200

    🐛 Fix whitelist migration (duh)

--- src/utils/hokutoNoCode.ts
+++ src/utils/hokutoNoCode.ts
@@ -250,7 +250,7 @@ export async function migrateBLWLToSmartPLs() {
 				plaid,
 				username: 'admin',
 				nickname: 'Dummy Plug System',
-				kid: s.kid,
+				kid: s.fk_kid,
 				added_at: new Date(),
 				pos,
 				criteria: null,
@@ -282,9 +282,9 @@ export async function migrateBLWLToSmartPLs() {
 	}
 	await updateAllSmartPlaylists();
 	try {
-		await db().query('DROP TABLE whitelist');
-		await db().query('DROP TABLE blacklist_criteria');
-		await db().query('DROP TABLE blacklist_criteria_set');
+		await db().query('DROP TABLE IF EXISTS whitelist');
+		await db().query('DROP TABLE IF EXISTS blacklist_criteria');
+		await db().query('DROP TABLE IF EXISTS blacklist_criteria_set');
 	} catch (err) {
 		// Everything is daijokay
 	}
