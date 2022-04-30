6cc4449478bc9d181a30b709fff3dbd6fe3f995c
Guillaume Lebigot <axel@teri-chan.net>
2022-04-30 13:32:27 +0200

    🐛 Fix migration constraints

--- migrations/20220429000000.do.removeGuests.sql
+++ migrations/20220429000000.do.removeGuests.sql
@@ -1,3 +1,15 @@
+ALTER TABLE favorites DROP CONSTRAINT favorites_login_fkey;
+ALTER TABLE playlist_content DROP CONSTRAINT plc_login_fkey;
+ALTER TABLE requested DROP CONSTRAINT requested_login_fkey;
+ALTER TABLE upvote DROP CONSTRAINT upvote_login_fkey;
+
+ALTER TABLE playlist_content ALTER COLUMN fk_login SET DEFAULT 'admin';
+
+ALTER TABLE favorites ADD CONSTRAINT favorites_login_fkey FOREIGN KEY(fk_login) REFERENCES users(pk_login) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE playlist_content ADD CONSTRAINT plc_login_fkey FOREIGN KEY(fk_login) REFERENCES users(pk_login) ON UPDATE CASCADE ON DELETE SET DEFAULT;
+ALTER TABLE requested ADD CONSTRAINT requested_login_fkey FOREIGN KEY(fk_login) REFERENCES users(pk_login) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE upvote ADD CONSTRAINT upvote_login_fkey FOREIGN KEY(fk_login) REFERENCES users(pk_login) ON DELETE CASCADE ON UPDATE CASCADE;
+
 DELETE FROM users WHERE nickname = 'Jean-Michel Normal';
 DELETE FROM users WHERE nickname = 'Alex Teriyaki';
 DELETE FROM users WHERE nickname = 'Beauf-kun';
