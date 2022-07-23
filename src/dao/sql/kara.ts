635c7115f70df4502535ce0c1dd35fb6081c36ee
Damien HERBERT <dherbert98@gmail.com>
2022-07-23 11:45:48 +0200

    🎨 Use CTE instead of inner SELECT

--- src/dao/sql/kara.ts
+++ src/dao/sql/kara.ts
@@ -44,7 +44,83 @@ export const sqlgetAllKaras = (
 	withCTE: string[],
 	blacklistClauses: string
 ) => `
-WITH ${withCTE.join(', \n')}
+WITH ${withCTE.join(', \n')},
+	k AS (
+		SELECT
+		  ak.tags AS tags,
+		  ak.pk_kid AS kid,
+		  ak.titles AS titles,
+		  ak.titles_aliases AS titles_aliases,
+		  ak.titles_default_language AS titles_default_language,
+		  ak.songorder AS songorder,
+		  ak.subfile AS subfile,
+		  ak.year AS year,
+		  ak.mediafile AS mediafile,
+		  ak.karafile AS karafile,
+		  ak.duration AS duration,
+		  ak.gain AS gain,
+		  ak.loudnorm AS loudnorm,
+		  ak.created_at AS created_at,
+		  ak.modified_at AS modified_at,
+		  ak.mediasize AS mediasize,
+		  ak.download_status AS download_status,
+		  ak.comment AS comment,
+		  ak.ignore_hooks AS ignoreHooks,
+		  COUNT(p.*)::integer AS played,
+		  ${selectRequested}
+		  (CASE WHEN :dejavu_time < MAX(p.played_at)
+				THEN TRUE
+				ELSE FALSE
+		  END) AS flag_dejavu,
+		  MAX(p.played_at) AS lastplayed_at,
+		  NOW() - MAX(p.played_at) AS lastplayed_ago,
+		  (CASE WHEN f.fk_kid IS NULL
+				THEN FALSE
+				ELSE TRUE
+		  END) as flag_favorites,
+		  ak.repository as repository,
+		  ak.tid AS tid,
+		  array_remove(array_agg(DISTINCT pc.pk_id_plcontent), null) AS public_plc_id,
+		  (CASE WHEN COUNT(up.*) > 0 THEN TRUE ELSE FALSE END) as flag_upvoted,
+		  array_remove(array_agg(DISTINCT pc_self.pk_id_plcontent), null) AS my_public_plc_id,
+		  count(ak.pk_kid) OVER()::integer AS count,
+		  array_remove(array_agg(DISTINCT krc.fk_kid_parent), null) AS parents,
+		  array_remove(array_agg(DISTINCT krp.fk_kid_child), null) AS children,
+		  array_remove((SELECT array_agg(DISTINCT fk_kid_child) FROM kara_relation WHERE fk_kid_parent = ANY (array_remove(array_agg(DISTINCT krc.fk_kid_parent), null))), ak.pk_kid) AS siblings
+		FROM all_karas AS ak
+		LEFT OUTER JOIN kara k ON k.pk_kid = ak.pk_kid
+		LEFT OUTER JOIN kara_relation krp ON krp.fk_kid_parent = ak.pk_kid ${
+			blacklistClauses ? ' AND krp.fk_kid_child NOT IN (SELECT * FROM blacklist)' : ''
+		}
+		LEFT OUTER JOIN kara_relation krc ON krc.fk_kid_child = ak.pk_kid ${
+			blacklistClauses ? ' AND krc.fk_kid_parent NOT IN (SELECT * FROM blacklist)' : ''
+		}
+		LEFT OUTER JOIN played AS p ON p.fk_kid = ak.pk_kid
+		LEFT OUTER JOIN playlist_content AS pc ON pc.fk_kid = ak.pk_kid AND pc.fk_id_playlist = :publicPlaylist_id
+		LEFT OUTER JOIN playlist_content AS pc_self on pc_self.fk_kid = ak.pk_kid AND pc_self.fk_id_playlist = :publicPlaylist_id AND pc_self.fk_login = :username
+		LEFT OUTER JOIN upvote up ON up.fk_id_plcontent = pc.pk_id_plcontent AND up.fk_login = :username
+		LEFT OUTER JOIN favorites AS f ON f.fk_login = :username AND f.fk_kid = ak.pk_kid
+		${joinClauses.join('')}
+		${additionalFrom.join('')}
+		WHERE true
+		${
+			collectionClauses.length > 0
+				? `AND ((${collectionClauses
+						.map(clause => `(${clause})`)
+						.join(
+							' OR '
+						)}) OR jsonb_array_length(jsonb_path_query_array( tags, '$[*] ? (@.type_in_kara == 16)')) = 0)`
+				: ''
+		}
+		${filterClauses.map(clause => `AND (${clause})`).reduce((a, b) => `${a} ${b}`, '')}
+		${whereClauses}
+		${blacklistClauses}
+		GROUP BY ${groupClauses} ak.pk_kid, pc.fk_kid, ak.titles, ak.titles_aliases, ak.titles_default_language, ak.comment, ak.songorder, ak.serie_singer_sortable, ak.subfile, ak.year, ak.tags, ak.mediafile, ak.karafile, ak.duration, ak.gain, ak.loudnorm, ak.created_at, ak.modified_at, ak.mediasize, ak.repository, ak.songtypes_sortable, f.fk_kid, ak.tid, ak.languages_sortable, ak.download_status, ak.ignore_hooks, ak.titles_sortable ${groupClauseEnd}
+		${havingClause}
+		ORDER BY ${orderClauses} ak.serie_singer_sortable, ak.songtypes_sortable DESC, ak.songorder, ak.languages_sortable, ak.titles_sortable
+		${limitClause}
+		${offsetClause}
+	)
 SELECT
   *,
   jsonb_path_query_array( tags, '$[*] ? (@.type_in_kara == 2)') AS singers,
@@ -63,82 +139,7 @@ SELECT
   jsonb_path_query_array( tags, '$[*] ? (@.type_in_kara == 14)') AS versions,
   jsonb_path_query_array( tags, '$[*] ? (@.type_in_kara == 15)') AS warnings,
   jsonb_path_query_array( tags, '$[*] ? (@.type_in_kara == 16)') AS collections
-FROM (
-	SELECT
-	  ak.tags AS tags,
-	  ak.pk_kid AS kid,
-	  ak.titles AS titles,
-	  ak.titles_aliases AS titles_aliases,
-	  ak.titles_default_language AS titles_default_language,
-	  ak.songorder AS songorder,
-	  ak.subfile AS subfile,
-	  ak.year AS year,
-	  ak.mediafile AS mediafile,
-	  ak.karafile AS karafile,
-	  ak.duration AS duration,
-	  ak.gain AS gain,
-	  ak.loudnorm AS loudnorm,
-	  ak.created_at AS created_at,
-	  ak.modified_at AS modified_at,
-	  ak.mediasize AS mediasize,
-	  ak.download_status AS download_status,
-	  ak.comment AS comment,
-	  ak.ignore_hooks AS ignoreHooks,
-	  COUNT(p.*)::integer AS played,
-	  ${selectRequested}
-	  (CASE WHEN :dejavu_time < MAX(p.played_at)
-			THEN TRUE
-			ELSE FALSE
-	  END) AS flag_dejavu,
-	  MAX(p.played_at) AS lastplayed_at,
-	  NOW() - MAX(p.played_at) AS lastplayed_ago,
-	  (CASE WHEN f.fk_kid IS NULL
-			THEN FALSE
-			ELSE TRUE
-	  END) as flag_favorites,
-	  ak.repository as repository,
-	  ak.tid AS tid,
-	  array_remove(array_agg(DISTINCT pc.pk_id_plcontent), null) AS public_plc_id,
-	  (CASE WHEN COUNT(up.*) > 0 THEN TRUE ELSE FALSE END) as flag_upvoted,
-	  array_remove(array_agg(DISTINCT pc_self.pk_id_plcontent), null) AS my_public_plc_id,
-	  count(ak.pk_kid) OVER()::integer AS count,
-	  array_remove(array_agg(DISTINCT krc.fk_kid_parent), null) AS parents,
-	  array_remove(array_agg(DISTINCT krp.fk_kid_child), null) AS children,
-	  array_remove((SELECT array_agg(DISTINCT fk_kid_child) FROM kara_relation WHERE fk_kid_parent = ANY (array_remove(array_agg(DISTINCT krc.fk_kid_parent), null))), ak.pk_kid) AS siblings
-	FROM all_karas AS ak
-	LEFT OUTER JOIN kara k ON k.pk_kid = ak.pk_kid
-	LEFT OUTER JOIN kara_relation krp ON krp.fk_kid_parent = ak.pk_kid ${
-		blacklistClauses ? ' AND krp.fk_kid_child NOT IN (SELECT * FROM blacklist)' : ''
-	}
-	LEFT OUTER JOIN kara_relation krc ON krc.fk_kid_child = ak.pk_kid ${
-		blacklistClauses ? ' AND krc.fk_kid_parent NOT IN (SELECT * FROM blacklist)' : ''
-	}
-	LEFT OUTER JOIN played AS p ON p.fk_kid = ak.pk_kid
-	LEFT OUTER JOIN playlist_content AS pc ON pc.fk_kid = ak.pk_kid AND pc.fk_id_playlist = :publicPlaylist_id
-	LEFT OUTER JOIN playlist_content AS pc_self on pc_self.fk_kid = ak.pk_kid AND pc_self.fk_id_playlist = :publicPlaylist_id AND pc_self.fk_login = :username
-	LEFT OUTER JOIN upvote up ON up.fk_id_plcontent = pc.pk_id_plcontent AND up.fk_login = :username
-	LEFT OUTER JOIN favorites AS f ON f.fk_login = :username AND f.fk_kid = ak.pk_kid
-	${joinClauses.join('')}
-	${additionalFrom.join('')}
-	WHERE true
-	${
-		collectionClauses.length > 0
-			? `AND ((${collectionClauses
-					.map(clause => `(${clause})`)
-					.join(
-						' OR '
-					)}) OR jsonb_array_length(jsonb_path_query_array( tags, '$[*] ? (@.type_in_kara == 16)')) = 0)`
-			: ''
-	}
-	${filterClauses.map(clause => `AND (${clause})`).reduce((a, b) => `${a} ${b}`, '')}
-	${whereClauses}
-	${blacklistClauses}
-	GROUP BY ${groupClauses} ak.pk_kid, pc.fk_kid, ak.titles, ak.titles_aliases, ak.titles_default_language, ak.comment, ak.songorder, ak.serie_singer_sortable, ak.subfile, ak.year, ak.tags, ak.mediafile, ak.karafile, ak.duration, ak.gain, ak.loudnorm, ak.created_at, ak.modified_at, ak.mediasize, ak.repository, ak.songtypes_sortable, f.fk_kid, ak.tid, ak.languages_sortable, ak.download_status, ak.ignore_hooks, ak.titles_sortable ${groupClauseEnd}
-	${havingClause}
-	ORDER BY ${orderClauses} ak.serie_singer_sortable, ak.songtypes_sortable DESC, ak.songorder, ak.languages_sortable, ak.titles_sortable
-	${limitClause}
-	${offsetClause}
-) k
+FROM k
 `;
 
 export const sqlgetAllKarasMicro = (
