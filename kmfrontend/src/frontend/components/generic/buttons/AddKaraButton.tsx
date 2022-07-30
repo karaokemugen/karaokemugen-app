dffb5eca5bc42d34aa59c221fdef3cf1343726e9
Florent Berthelot <florentius.b@gmail.com>
2022-07-30 12:57:10 +0200

    ♻️ Use PLCCallback function in AddKaraButton component

--- kmfrontend/src/frontend/components/generic/buttons/AddKaraButton.tsx
+++ kmfrontend/src/frontend/components/generic/buttons/AddKaraButton.tsx
@@ -1,13 +1,9 @@
 import i18next from 'i18next';
 import { useContext } from 'react';
-import { toast } from 'react-toastify';
 
-import nanamiSingPng from '../../../../assets/nanami-sing.png';
-import nanamiSingWebP from '../../../../assets/nanami-sing.webp';
 import GlobalContext from '../../../../store/context';
 import { commandBackend } from '../../../../utils/socket';
-import { displayMessage, secondsTimeSpanToHMS } from '../../../../utils/tools';
-import { getTitleInLocale } from '../../../../utils/kara';
+import { PLCCallback } from '../../../../utils/tools';
 import { DBKara } from '../../../../../../src/lib/types/database/kara';
 
 interface Props {
@@ -27,76 +23,7 @@ export default function AddKaraButton(props: Props) {
 		} catch (e) {
 			// already display
 		}
-		if (response && response.code && response.data?.plc) {
-			let message;
-			if (response.data?.plc.time_before_play) {
-				const playTime = new Date(Date.now() + response.data.plc.time_before_play * 1000);
-				const playTimeDate = playTime.getHours() + 'h' + ('0' + playTime.getMinutes()).slice(-2);
-				const beforePlayTime = secondsTimeSpanToHMS(response.data.plc.time_before_play, 'hm');
-				message = (
-					<>
-						{i18next.t(`SUCCESS_CODES.${response.code}`, {
-							song: getTitleInLocale(
-								context.globalState.settings.data,
-								props.kara.titles,
-								props.kara.titles_default_language
-							),
-						})}
-						<br />
-						{i18next.t('KARA_DETAIL.TIME_BEFORE_PLAY', {
-							time: beforePlayTime,
-							date: playTimeDate,
-						})}
-					</>
-				);
-			} else {
-				message = (
-					<>
-						{i18next.t(`SUCCESS_CODES.${response.code}`, {
-							song: getTitleInLocale(
-								context.globalState.settings.data,
-								props.kara.titles,
-								props.kara.titles_default_language
-							),
-						})}
-					</>
-				);
-			}
-			displayMessage(
-				'success',
-				<div className="toast-with-img">
-					<picture>
-						<source type="image/webp" srcSet={nanamiSingWebP} />
-						<source type="image/png" srcSet={nanamiSingPng} />
-						<img src={nanamiSingPng} alt="Nanami is singing!" />
-					</picture>
-					<span>
-						{message}
-						<br />
-						<button
-							className="btn"
-							onClick={e => {
-								e.preventDefault();
-								e.stopPropagation();
-								commandBackend('deleteKaraFromPlaylist', { plc_ids: [response.data.plc.plcid] })
-									.then(() => {
-										toast.dismiss(response.data.plc.plcid);
-										displayMessage('success', i18next.t('SUCCESS_CODES.KARA_DELETED'));
-									})
-									.catch(() => {
-										toast.dismiss(response.data.plc.plcid);
-									});
-							}}
-						>
-							{i18next.t('CANCEL')}
-						</button>
-					</span>
-				</div>,
-				10000,
-				'top-left',
-				response.data.plc.plcid
-			);
-		}
+		PLCCallback(response, context, props.kara);
 	};
 
 	return (
