ce475fa56a9f6c8f90428554cc365da9c28f91ce
Guillaume Lebigot <axel@teri-chan.net>
2022-10-17 11:35:51 +0200

    :truck: Move closeIssue to lib

--- src/utils/gitlab.ts
+++ src/utils/gitlab.ts
@@ -49,27 +49,3 @@ export async function getUserID(repoName: string) {
 		throw err;
 	}
 }
-
-/** Close an issue */
-export async function closeIssue(issue: number, repoName: string) {
-	let repo = getRepo(repoName);
-	const params = {
-		state_event: 'close',
-	};
-	if (!repo.MaintainerMode) throw 'Maintainer mode is not enabled for this repository';
-	const url = new URL(repo.Git.URL);
-	if (!repo.Git.ProjectID) {
-		// Editing the repo should trigger
-		await editRepo(repo.Name, repo);
-		repo = getRepo(repoName) as RepositoryMaintainerSettings;
-	}
-	const closeIssueURL = `${url.protocol}//${url.hostname}/api/v4/projects/${repo.Git.ProjectID}/issues/${issue}`;
-	logger.debug(`Close Issue URL: ${closeIssueURL}`, { service });
-	await HTTP.put(closeIssueURL, params, {
-		headers: {
-			'PRIVATE-TOKEN': repo.Git.Password,
-			'Content-Type': 'application/json',
-		},
-		timeout: 25000,
-	});
-}
