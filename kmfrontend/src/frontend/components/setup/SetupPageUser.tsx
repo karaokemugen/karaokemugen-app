3e6a81279a3d5ddda53a808856d706c06b87c7d2
Florent Berthelot <florentius.b@gmail.com>
2022-10-19 22:47:35 +0200

    🐛 Fix errors display in setup page

--- kmfrontend/src/frontend/components/setup/SetupPageUser.tsx
+++ kmfrontend/src/frontend/components/setup/SetupPageUser.tsx
@@ -47,7 +47,9 @@ function SetupPageUser() {
 			setError(undefined);
 			loginCall();
 		} catch (err: any) {
-			const error = err?.message ? i18next.t(`ERROR_CODES.${err.message.code}`) : JSON.stringify(err);
+			const error = err?.message
+				? i18next.t(`ERROR_CODES.${JSON.parse(err.message).message.code}`)
+				: JSON.stringify(err);
 			setError(error);
 		}
 	};
