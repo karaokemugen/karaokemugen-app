655a136634e322e2e65b1fd2d759314e5c9742b5
Guillaume Lebigot <axel@teri-chan.net>
2022-10-30 16:49:51 +0100

    🐛 Fix public My Anime page showing all songs

--- src/controllers/frontend/user.ts
+++ src/controllers/frontend/user.ts
@@ -252,6 +252,8 @@ export default function userController(router: SocketIOApp) {
 				from: +req.body?.from || 0,
 				size: +req.body?.size || 9999999,
 				order: req.body?.order,
+				parentsOnly: req.body?.parentsOnly,
+				blacklist: req.body?.blacklist,
 			});
 		} catch (err) {
 			const code = 'ANIME_LIST_VIEW_ERROR';
