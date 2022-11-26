18765489d2d8d50dc6e74026eb4b799aaca02772
Guillaume Lebigot <axel@teri-chan.net>
2022-11-26 16:48:44 +0100

    🐛 Fix kara.moe auto-updates for exisiting installs since <7.0

--- src/components/init.ts
+++ src/components/init.ts
@@ -18,6 +18,7 @@ import { Config } from '../types/config';
 import { initConfig } from '../utils/config';
 import { logo } from '../utils/constants';
 import { defaultRepositories } from '../utils/defaultSettings';
+import { reenableKaraMoeAutoUpdates } from '../utils/hokutoNoCode';
 import Sentry from '../utils/sentry';
 import { getState, setState } from '../utils/state';
 import { parseArgs, setupFromCommandLineArgs } from './args';
@@ -71,6 +72,8 @@ export async function preInit() {
 	const conf = getConfig();
 	if (conf.App.FirstRun && conf.System.Repositories.length === 0) {
 		setConfig({ System: { Repositories: [...defaultRepositories] } });
+	} else {
+		reenableKaraMoeAutoUpdates();
 	}
 	// Test if network ports are available
 	await verifyOpenPort(getConfig().System.FrontendPort, getConfig().App.FirstRun);
