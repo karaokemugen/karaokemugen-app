dafb670d3436a2eb08002b9ddb01fda4addb96bd
Florent Berthelot <florentius.b@gmail.com>
2022-12-04 22:31:40 +0100

    🔥 Delete migrations for 4.5 and 5.0 versions

--- src/dao/migrations.ts
+++ src/dao/migrations.ts
@@ -23,22 +23,9 @@ export async function postMigrationTasks(migrations: Postgrator.Migration[], did
 	for (const migration of migrations) {
 		let breakFromLoop = false;
 		switch (migration.name) {
-			// 4.0 migrations
 			case 'initial':
 				// Initial migration will likely trigger generation anyway.
 				breakFromLoop = true;
-				break;
-			case 'addPlaylistTriggers':
-				await dialog.showMessageBox(win, {
-					type: 'info',
-					title: i18next.t('NO_KARAOKE_MODE_ANYMORE.TITLE'),
-					message: i18next.t('NO_KARAOKE_MODE_ANYMORE.MESSAGE'),
-				});
-				break;
-			// 5.0 migrations
-			case 'addPriorityToTags':
-				if (!didGeneration) doGenerate = true;
-				logger.info('Migration adding priority to tags detected, forcing generation', { service });
 				break;
 			// 6.0 migrations
 			case 'addTitlesToKara':
