a515edcc9ed408a6ef02514cd9d4ba51fb865c1a
Guillaume Lebigot <axel@teri-chan.net>
2023-01-01 19:44:26 +0000

    Revert "Make wget silent"
    This reverts commit f62a5febe0b08b53b511bbef6efa913c04af91e8

--- util/updateFlatpak.sh
+++ util/updateFlatpak.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-wget --silent -N https://mugen.karaokes.moe/downloads/$DIST_LINUX_X64
+wget -N https://mugen.karaokes.moe/downloads/$DIST_LINUX_X64
 DIST_X64_SHA=$(sha256sum "$DIST_LINUX_X64" | awk -F\  {'print $1'})
 
-wget --silent -N https://mugen.karaokes.moe/downloads/$DIST_LINUX_ARM64
+wget -N https://mugen.karaokes.moe/downloads/$DIST_LINUX_ARM64
 DIST_ARM64_SHA=$(sha256sum "$DIST_LINUX_ARM64" | awk -F\  {'print $1'})
 
 SENTRYCLI_VERSION=$(grep @sentry/cli\": package.json | awk -F\" {'print $4'} | sed -e 's/\^//g')
 
-wget --silent -N https://downloads.sentry-cdn.com/sentry-cli/$SENTRYCLI_VERSION/sentry-cli-Linux-x86_64
+wget -N https://downloads.sentry-cdn.com/sentry-cli/$SENTRYCLI_VERSION/sentry-cli-Linux-x86_64
 
-wget --silent -N https://downloads.sentry-cdn.com/sentry-cli/$SENTRYCLI_VERSION/sentry-cli-Linux-aarch64
+wget -N https://downloads.sentry-cdn.com/sentry-cli/$SENTRYCLI_VERSION/sentry-cli-Linux-aarch64
 
 SENTRYCLI_X64_SHA=$(sha256sum sentry-cli-Linux-x86_64 | awk -F\  {'print $1'})
 
