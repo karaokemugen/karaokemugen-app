37f9d98678933ec37f38c3047d26f2546af1db83
Guillaume Lebigot <axel@teri-chan.net>
2023-07-11 12:11:46 +0200

    :sparkles: Added "All Users" to possible users for faves and anilists on automixes. Closes #1464

--- kmfrontend/src/frontend/components/modals/AutoMixModal.tsx
+++ kmfrontend/src/frontend/components/modals/AutoMixModal.tsx
@@ -26,6 +26,10 @@ function AutoMixModal(props: IProps) {
 	const [limitType, setLimitType] = useState<PlaylistLimit>('duration');
 	const [limitNumber, setLimitNumber] = useState(0);
 	const memoizedUsers = useMemo(() => {
+		props.userList.push({
+			nickname: i18next.t('ALL_LOGGED_USERS'),
+			login: '*',
+		});
 		return props.userList.map(el => {
 			return {
 				label: el.nickname || el.login,
