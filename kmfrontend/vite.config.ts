77dc122e5ef2d96e1e5f383ce80c056b1cc27560
Florent Berthelot <florentius.b@gmail.com>
2023-09-27 20:01:43 +0200

    🔧 Add sourcemap to vite build

--- kmfrontend/vite.config.ts
+++ kmfrontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+	build: {
+		sourcemap: true,
+	},
 	plugins: [nodePolyfills(), react()],
 	server: {
 		port: 3000,
