4ccd381c310ce4eda64145612349f2c43eacd25d
Ismaël Bussière <bussiereismael@gmail.com>
2024-01-14 14:50:44 +0000

    💄 When the news on the home is already at its maximum size, it doesn't need to be hidden

--- kmfrontend/src/frontend/components/WelcomePageArticle.tsx
+++ kmfrontend/src/frontend/components/WelcomePageArticle.tsx
@@ -1,7 +1,7 @@
 import '../styles/start/WelcomePageArticle.scss';
 
 import DOMPurify from 'dompurify';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { News } from '../types/news';
 
@@ -11,11 +11,26 @@ interface IProps {
 
 function WelcomePageArticle(props: IProps) {
 	const [open, setOpen] = useState(false);
+	const [containerHeight, setContainerHeight] = useState(0);
+	const [bodyHeight, setBodyHeight] = useState(0);
+
+	// useRef doen't work, see https://stackoverflow.com/a/67906087
+	const containerRef = useCallback((node: HTMLDivElement | null) => {
+		if (node !== null) {
+			setContainerHeight(node.getBoundingClientRect().height);
+		}
+	}, []);
+	const bodyRef = useCallback((node: HTMLDivElement | null) => {
+		if (node !== null) {
+			setBodyHeight(node.getBoundingClientRect().height);
+		}
+	}, []);
+
+	const canBeExpanded = () => bodyHeight > containerHeight;
 
 	return (
 		<article
 			className="article-wrapper"
-			key={Math.random()}
 			data-open={open ? 1 : 0}
 			data-type={props.article.type}
 			onClick={() => setOpen(!open)}
@@ -24,8 +39,9 @@ function WelcomePageArticle(props: IProps) {
 				<b>{props.article.title}</b>
 				<a href={props.article.link}>{props.article.dateStr}</a>
 			</div>
-			<div className="article-body">
-				<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.article.html) }} />
+			<div className="article-body" ref={containerRef}>
+				<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.article.html) }} ref={bodyRef} />
+				{!open && canBeExpanded() ? <div className="expandShadow"></div> : ''}
 			</div>
 		</article>
 	);
