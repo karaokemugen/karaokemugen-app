1ecab865a59d0c79d6ba8cb78a5829a3958e6ad2
Guillaume Lebigot <axel@teri-chan.net>
2024-05-30 09:08:49 +0200

    💚 Removed sharp and libvips from flatpak

--- util/updateFlatpak.sh
+++ util/updateFlatpak.sh
@@ -10,18 +10,4 @@ SENTRYCLI_X64_SHA=$(sha256sum sentry-cli-Linux-x86_64 | awk -F\  {'print $1'})
 
 SENTRYCLI_ARM64_SHA=$(sha256sum sentry-cli-Linux-aarch64 | awk -F\  {'print $1'})
 
-###
-
-SHARP_VERSION=$(grep version\": node_modules/sharp/package.json | awk -F\" {'print $4'} | sed -e 's/\^//g')
-
-wget --quiet -N https://github.com/lovell/sharp/releases/download/v$SHARP_VERSION/sharp-v$SHARP_VERSION-napi-v7-linux-x64.tar.gz
-
-SHARP_SHA=$(sha256sum sharp-v$SHARP_VERSION-napi-v7-linux-x64.tar.gz | awk -F\  {'print $1'})
-
-LIBVIPS_VERSION=$(grep libvips\": node_modules/sharp/package.json | awk -F\" {'print $4'} | sed -e 's/\^//g')
-
-wget --quiet -N https://github.com/lovell/sharp-libvips/releases/download/v$LIBVIPS_VERSION/libvips-$LIBVIPS_VERSION-linux-x64.tar.br
-
-LIBVIPS_SHA=$(sha256sum libvips-$LIBVIPS_VERSION-linux-x64.tar.br | awk -F\  {'print $1'})
-
-node util/updateFlatpak.cjs "$SENTRYCLI_VERSION" "$SENTRYCLI_X64_SHA" "$SENTRYCLI_ARM64_SHA" "$SHARP_VERSION" "$SHARP_SHA" "$LIBVIPS_VERSION" "$LIBVIPS_SHA"
+node util/updateFlatpak.cjs "$SENTRYCLI_VERSION" "$SENTRYCLI_X64_SHA" "$SENTRYCLI_ARM64_SHA"
