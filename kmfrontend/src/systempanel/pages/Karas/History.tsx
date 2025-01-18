9bac28e553fc20a50df4274d65f4054bf0f681f8
Florent Berthelot <florentius.b@gmail.com>
2025-01-18 15:11:32 +0100

    ♻️ Migrate History to function component

--- kmfrontend/src/systempanel/pages/Karas/History.tsx
+++ kmfrontend/src/systempanel/pages/Karas/History.tsx
@@ -1,6 +1,5 @@
-import { Button, Layout, Table, TableColumnProps } from 'antd';
+import { Button, Layout, Table } from 'antd';
 import i18next from 'i18next';
-import { Component } from 'react';
 
 import { DBKara } from '../../../../../src/lib/types/database/kara';
 import GlobalContext from '../../../store/context';
@@ -8,82 +7,49 @@ import { getSerieOrSingerGroupsOrSingers, getTagInLocaleList, getTitleInLocale }
 import { commandBackend } from '../../../utils/socket';
 import Title from '../../components/Title';
 import dayjs from 'dayjs';
+import { SortOrder } from 'antd/es/table/interface';
+import { useContext, useEffect, useState } from 'react';
 
-interface KaraListState {
-	karas: DBKara[];
-	i18n: any[];
-}
+function KaraHistory() {
+	const context = useContext(GlobalContext);
 
-class KaraList extends Component<unknown, KaraListState> {
-	static contextType = GlobalContext;
-	context: React.ContextType<typeof GlobalContext>;
+	const [karas, setKaras] = useState<DBKara[]>([]);
+	const [i18n, setI18n] = useState([]);
 
-	state = {
-		karas: [],
-		i18n: [],
-	};
+	useEffect(() => {
+		refresh();
+	}, []);
 
-	componentDidMount() {
-		this.refresh();
-	}
-
-	refresh = async () => {
+	const refresh = async () => {
 		try {
 			const res = await commandBackend('getKaras', { order: 'history', ignoreCollections: true });
-			this.setState({ karas: res.content, i18n: res.i18n });
+			setKaras(res.content);
+			setI18n(res.i18n);
 		} catch (_) {
 			// already display
 		}
 	};
 
-	render() {
-		return (
-			<>
-				<Title
-					title={i18next.t('HEADERS.HISTORY.TITLE')}
-					description={i18next.t('HEADERS.HISTORY.DESCRIPTION')}
-				/>
-				<Layout.Content>
-					<Button style={{ margin: '1em' }} type="primary" onClick={this.refresh}>
-						{i18next.t('REFRESH')}
-					</Button>
-					<Table
-						dataSource={this.state.karas}
-						columns={this.columns}
-						rowKey="lastplayed_at"
-						scroll={{
-							x: true,
-						}}
-						expandable={{
-							showExpandColumn: false,
-						}}
-					/>
-				</Layout.Content>
-			</>
-		);
-	}
-
-	columns: TableColumnProps<any>[] = [
+	const columns = [
 		{
 			title: i18next.t('TAG_TYPES.LANGS_other'),
 			dataIndex: 'langs',
 			key: 'langs',
-			render: langs =>
-				getTagInLocaleList(this.context.globalState.settings.data, langs, this.state.i18n).join(', '),
+			render: langs => getTagInLocaleList(context.globalState.settings.data, langs, i18n).join(', '),
 		},
 		{
 			title: i18next.t('KARA.FROM_DISPLAY_TYPE_COLUMN'),
 			dataIndex: 'series',
 			key: 'series',
 			render: (_series, record: DBKara) =>
-				getSerieOrSingerGroupsOrSingers(this.context.globalState.settings.data, record, this.state.i18n),
+				getSerieOrSingerGroupsOrSingers(context.globalState.settings.data, record, i18n),
 		},
 		{
 			title: i18next.t('TAG_TYPES.SONGTYPES_other'),
 			dataIndex: 'songtypes',
 			key: 'songtypes',
 			render: (songtypes, record) =>
-				getTagInLocaleList(this.context.globalState.settings.data, songtypes, this.state.i18n).join(', ') +
+				getTagInLocaleList(context.globalState.settings.data, songtypes, i18n).join(', ') +
 				' ' +
 				(record.songorder || ''),
 		},
@@ -92,14 +58,13 @@ class KaraList extends Component<unknown, KaraListState> {
 			dataIndex: 'titles',
 			key: 'titles',
 			render: (titles, record) =>
-				getTitleInLocale(this.context.globalState.settings.data, titles, record.titles_default_language),
+				getTitleInLocale(context.globalState.settings.data, titles, record.titles_default_language),
 		},
 		{
 			title: i18next.t('TAG_TYPES.VERSIONS_other'),
 			dataIndex: 'versions',
 			key: 'versions',
-			render: versions =>
-				getTagInLocaleList(this.context.globalState.settings.data, versions, this.state.i18n).join(', '),
+			render: versions => getTagInLocaleList(context.globalState.settings.data, versions, i18n).join(', '),
 		},
 		{
 			title: i18next.t('KARA.PLAYED'),
@@ -112,10 +77,32 @@ class KaraList extends Component<unknown, KaraListState> {
 			dataIndex: 'lastplayed_at',
 			key: 'lastplayed_at',
 			render: played_at => dayjs(played_at).format('L LTS'),
-			defaultSortOrder: 'descend',
+			defaultSortOrder: 'descend' as SortOrder,
 			sorter: (a, b) => a.lastplayed_at - b.lastplayed_at,
 		},
 	];
+
+	return (
+		<>
+			<Title title={i18next.t('HEADERS.HISTORY.TITLE')} description={i18next.t('HEADERS.HISTORY.DESCRIPTION')} />
+			<Layout.Content>
+				<Button style={{ margin: '1em' }} type="primary" onClick={refresh}>
+					{i18next.t('REFRESH')}
+				</Button>
+				<Table
+					dataSource={karas}
+					columns={columns}
+					rowKey="lastplayed_at"
+					scroll={{
+						x: true,
+					}}
+					expandable={{
+						showExpandColumn: false,
+					}}
+				/>
+			</Layout.Content>
+		</>
+	);
 }
 
-export default KaraList;
+export default KaraHistory;
