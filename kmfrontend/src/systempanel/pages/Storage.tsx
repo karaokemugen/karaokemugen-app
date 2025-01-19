ad5b9650f90c0c943dc37660392a6053abd546c9
Florent Berthelot <florentius.b@gmail.com>
2025-01-19 18:23:40 +0100

    ♻️ Migrate Storage to function component

--- kmfrontend/src/systempanel/pages/Storage.tsx
+++ kmfrontend/src/systempanel/pages/Storage.tsx
@@ -2,27 +2,21 @@ import { ClearOutlined, DeleteOutlined, FolderOpenOutlined } from '@ant-design/i
 import { Alert, Button, Layout, Table } from 'antd';
 import i18next from 'i18next';
 import prettyBytes from 'pretty-bytes';
-import { Component } from 'react';
 
 import { commandBackend } from '../../utils/socket';
 import Title from '../components/Title';
+import { useEffect, useState } from 'react';
 
-interface StorageState {
-	repositories: { name: string; freeSpace: number }[];
-}
+function Storage() {
+	const [repositories, setRepositories] = useState<{ name: string; freeSpace: number }[]>([]);
 
-class Storage extends Component<unknown, StorageState> {
-	state = {
-		repositories: [],
-	};
+	useEffect(() => {
+		getRepos();
+	}, []);
 
-	componentDidMount() {
-		this.getRepos();
-	}
-
-	getRepos = async () => {
+	const getRepos = async () => {
 		const res = await commandBackend('getRepos');
-		const repositories: { name: string; freeSpace: number }[] = await Promise.all(
+		const repos: { name: string; freeSpace: number }[] = await Promise.all(
 			res
 				.filter(repo => repo.Online)
 				.map(async repo => {
@@ -35,10 +29,10 @@ class Storage extends Component<unknown, StorageState> {
 					return { name: repo.Name, freeSpace: freeSpace == null ? 'N/A' : prettyBytes(freeSpace) };
 				})
 		);
-		this.setState({ repositories });
+		setRepositories(repos);
 	};
 
-	openMediaFolder = async (name: string) => {
+	const openMediaFolder = async (name: string) => {
 		try {
 			await commandBackend('openMediaFolder', { name }, true, 300000);
 		} catch (_) {
@@ -46,7 +40,7 @@ class Storage extends Component<unknown, StorageState> {
 		}
 	};
 
-	deleteOldRepoMedias = async (name: string) => {
+	const deleteOldRepoMedias = async (name: string) => {
 		try {
 			await commandBackend('deleteOldRepoMedias', { name }, true, 300000);
 		} catch (_) {
@@ -54,7 +48,7 @@ class Storage extends Component<unknown, StorageState> {
 		}
 	};
 
-	deleteAllRepoMedias = async (name: string) => {
+	const deleteAllRepoMedias = async (name: string) => {
 		try {
 			await commandBackend('deleteAllRepoMedias', { name }, true, 300000);
 		} catch (_) {
@@ -62,31 +56,7 @@ class Storage extends Component<unknown, StorageState> {
 		}
 	};
 
-	render() {
-		return (
-			<>
-				<Title
-					title={i18next.t('HEADERS.STORAGE.TITLE')}
-					description={i18next.t('HEADERS.STORAGE.DESCRIPTION')}
-				/>
-				<Layout.Content>
-					<Table dataSource={this.state.repositories} columns={this.columns} rowKey="name" />
-					<Alert
-						type="info"
-						message={i18next.t('REPOSITORIES.STORAGE_INFO')}
-						description={
-							<ul>
-								<li>{i18next.t('REPOSITORIES.STORAGE_INFO_DELETE')}</li>
-								<li>{i18next.t('REPOSITORIES.STORAGE_INFO_MOVE')}</li>
-							</ul>
-						}
-					/>
-				</Layout.Content>
-			</>
-		);
-	}
-
-	columns = [
+	const columns = [
 		{
 			title: i18next.t('REPOSITORIES.NAME'),
 			dataIndex: 'name',
@@ -101,7 +71,7 @@ class Storage extends Component<unknown, StorageState> {
 			key: 'openFolder',
 			align: 'center' as const,
 			render: (_, record) => (
-				<Button type="primary" icon={<FolderOpenOutlined />} onClick={() => this.openMediaFolder(record.name)}>
+				<Button type="primary" icon={<FolderOpenOutlined />} onClick={() => openMediaFolder(record.name)}>
 					{i18next.t('REPOSITORIES.OPEN_MEDIA_FOLDER')}
 				</Button>
 			),
@@ -110,12 +80,7 @@ class Storage extends Component<unknown, StorageState> {
 			key: 'deleteOldRepoMedias',
 			align: 'center' as const,
 			render: (_, record) => (
-				<Button
-					type="primary"
-					danger
-					icon={<ClearOutlined />}
-					onClick={() => this.deleteOldRepoMedias(record.name)}
-				>
+				<Button type="primary" danger icon={<ClearOutlined />} onClick={() => deleteOldRepoMedias(record.name)}>
 					{i18next.t('REPOSITORIES.DELETE_OLD_MEDIAS')}
 				</Button>
 			),
@@ -128,13 +93,32 @@ class Storage extends Component<unknown, StorageState> {
 					type="primary"
 					danger
 					icon={<DeleteOutlined />}
-					onClick={() => this.deleteAllRepoMedias(record.name)}
+					onClick={() => deleteAllRepoMedias(record.name)}
 				>
 					{i18next.t('REPOSITORIES.DELETE_ALL_MEDIAS')}
 				</Button>
 			),
 		},
 	];
+
+	return (
+		<>
+			<Title title={i18next.t('HEADERS.STORAGE.TITLE')} description={i18next.t('HEADERS.STORAGE.DESCRIPTION')} />
+			<Layout.Content>
+				<Table dataSource={repositories} columns={columns} rowKey="name" />
+				<Alert
+					type="info"
+					message={i18next.t('REPOSITORIES.STORAGE_INFO')}
+					description={
+						<ul>
+							<li>{i18next.t('REPOSITORIES.STORAGE_INFO_DELETE')}</li>
+							<li>{i18next.t('REPOSITORIES.STORAGE_INFO_MOVE')}</li>
+						</ul>
+					}
+				/>
+			</Layout.Content>
+		</>
+	);
 }
 
 export default Storage;
