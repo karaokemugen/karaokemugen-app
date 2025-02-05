38cac960e28e42f1f24ac60890ef5373449800a0
Guillaume Lebigot <axel@teri-chan.net>
2025-02-05 12:01:18 +0000

    :bug: Allow env var SKIP_NOTARIZE when building on macOS

--- build/notarize.cjs
+++ build/notarize.cjs
@@ -1,19 +1,19 @@
 const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
+	const { electronPlatformName, appOutDir } = context;
+	if (electronPlatformName !== 'darwin' || process.env.SKIP_NOTARIZE) {
+		return;
+	}
 
-  const appName = context.packager.appInfo.productFilename;
+	const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    appBundleId: 'moe.karaokes.mugen',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-	tool: 'notarytool',
-	teamId: process.env.TEAMID,
-  });
+	return await notarize({
+		appBundleId: 'moe.karaokes.mugen',
+		appPath: `${appOutDir}/${appName}.app`,
+		appleId: process.env.APPLEID,
+		appleIdPassword: process.env.APPLEIDPASS,
+		tool: 'notarytool',
+		teamId: process.env.TEAMID,
+	});
 };
