f0c7d93be07c22493a4c2d448bfe6f5f87903de1
Guillaume Lebigot <axel@teri-chan.net>
2025-02-26 19:12:51 +0100

    👷 Added -e to zip to overwrite files

--- util/pushKMRemote.sh
+++ util/pushKMRemote.sh
@@ -6,4 +6,4 @@ cd kmfrontend/dist
 
 zip -r $BUILDVERSION.zip .
 
-lftp -c "set cmd:fail-exit yes; set ftp:ssl-allow no; open -u $USERNAME,$PASSWORD $KARAOKESMOE; cd www/mugen.karaokes.moe/frontends; set cmd:fail-exit yes; put $BUILDVERSION.zip"
+lftp -c "set cmd:fail-exit yes; set ftp:ssl-allow no; open -u $USERNAME,$PASSWORD $KARAOKESMOE; cd www/mugen.karaokes.moe/frontends; set cmd:fail-exit yes; put -e $BUILDVERSION.zip"
