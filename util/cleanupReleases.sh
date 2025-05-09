77edf09fd7ab9e3912265bac89b72079439fac87
Guillaume Lebigot <axel@teri-chan.net>
2025-05-09 13:44:55 +0200

    💚 Fix releases cleanup (it's ugly, please don't hit me) [skip-ci]

--- util/cleanupReleases.sh
+++ util/cleanupReleases.sh
@@ -2,9 +2,17 @@ source util/versionUtil.sh
 
 lftp -u $USERNAME,$PASSWORD $KARAOKESMOE <<__CMD__
   set cmd:fail-exit yes
-  cls www/mugen.karaokes.moe/downloads/ | grep -- "-$RELEASE" | grep -v -- "$BUILDVERSION" | xargs -I{} echo rm "{}" > rm_list.txt
+  cls www/mugen.karaokes.moe/downloads/ | grep -- "-$RELEASE" | grep -v -- "$BUILDVERSION" | sed -e 's/^/\"/g' | sed -e 's/$/\"/g' | xargs -0 -I{} echo "{}" | sed 's/^/rm\ /g' >> rm_list.txt
+__CMD__
+
+# Remove last line
+sed -i '$ d' rm_list.txt
+
+cat rm_list.txt
+
+lftp -u $USERNAME,$PASSWORD $KARAOKESMOE <<__CMD__
+  set cmd:fail-exit yes
   source rm_list.txt
 __CMD__
 
-echo rm_list.txt
 rm rm_list.txt
