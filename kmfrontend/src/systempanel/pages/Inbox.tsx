862b570cdfdbb75eb2ec1f2d42dd8cffc51a3cb6
Guillaume Lebigot <axel@teri-chan.net>
2025-06-10 21:14:04 +0200

    🚸 Allow maintainers to unassign themselves from inbox items

--- kmfrontend/src/systempanel/pages/Inbox.tsx
+++ kmfrontend/src/systempanel/pages/Inbox.tsx
@@ -1,4 +1,4 @@
-import { DeleteOutlined, DownloadOutlined, UserOutlined } from '@ant-design/icons';
+import { DeleteOutlined, DownloadOutlined, RollbackOutlined, UserOutlined } from '@ant-design/icons';
 import { EditOutlined, PlayCircleOutlined } from '@ant-design/icons';
 import { Alert, Button, Layout, Modal, Table } from 'antd';
 import i18next from 'i18next';
@@ -43,6 +43,15 @@ export default function Inbox() {
 	const downloadKaraFromInbox = async (inid: string) => {
 		try {
 			await commandBackend(WS_CMD.DOWNLOAD_KARA_FROM_INBOX, { repoName: instance.Name, inid });
+		} catch (_) {
+			// already display
+		}
+		getInbox();
+	};
+
+	const unassignKaraFromInbox = async (inid: string) => {
+		try {
+			await commandBackend(WS_CMD.UNASSIGN_KARA_FROM_INBOX, { repoName: instance.Name, inid });
 		} catch (_) {
 			// already display
 		}
@@ -265,8 +274,17 @@ export default function Inbox() {
 					<Button
 						type="primary"
 						danger
+						onClick={() => unassignKaraFromInbox(record.inid)}
+						style={{ marginLeft: '1em' }}
+						title={i18next.t('INBOX.UNASSIGN_FROM_SONG')}
+						icon={<RollbackOutlined />}
+					/>
+					<Button
+						type="primary"
+						danger
 						onClick={() => deleteKaraFromInbox(record.inid)}
 						style={{ marginLeft: '1em' }}
+						title={i18next.t('INBOX.DELETE_SONG')}
 						icon={<DeleteOutlined />}
 					/>
 				</div>
--- src/controllers/frontend/inbox.ts
+++ src/controllers/frontend/inbox.ts
@@ -1,7 +1,12 @@
 import { WS_CMD } from '../../../kmfrontend/src/utils/ws.js';
 import { APIMessage } from '../../lib/services/frontend.js';
 import { SocketIOApp } from '../../lib/utils/ws.js';
-import { deleteKaraInInbox, downloadKaraFromInbox, getInbox } from '../../services/inbox.js';
+import {
+	deleteKaraInInbox,
+	downloadKaraFromInbox,
+	getInbox,
+	markKaraAsUnassignedInInbox,
+} from '../../services/inbox.js';
 import { runChecklist } from '../middlewares.js';
 
 export default function inboxController(router: SocketIOApp) {
@@ -25,6 +30,14 @@ export default function inboxController(router: SocketIOApp) {
 		await runChecklist(socket, req, 'user', 'closed');
 		try {
 			return await deleteKaraInInbox(req.body.inid, req.body.repoName, req.onlineAuthorization);
+		} catch (err) {
+			throw { code: err.code || 500, message: APIMessage(err.message) };
+		}
+	});
+	router.route(WS_CMD.UNASSIGN_KARA_FROM_INBOX, async (socket, req) => {
+		await runChecklist(socket, req, 'user', 'closed');
+		try {
+			return await markKaraAsUnassignedInInbox(req.body.inid, req.body.repoName, req.onlineAuthorization);
 		} catch (err) {
 			throw { code: err.code || 500, message: APIMessage(err.message) };
 		}
